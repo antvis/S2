911d971c33a8219cf46dc3b733d5d5c629424281
yingying.xwy <yingying.xwy@antgroup.com>
2021-01-12 16:36:35 +0800

    chore: :wrench:  core init

---
+++ packages/s2-core/webpack-dev.config.js
@@ -0,0 +1,11 @@
+const webpackConfig = require('./webpack.config');
+const merge = require('webpack-merge');
+
+module.exports = merge(webpackConfig, {
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+    ignored: ['node_modules/**'],
+  },
+});
