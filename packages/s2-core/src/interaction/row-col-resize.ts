91d95184c7f0a6dfbab3e47a0621ef41ca8e565f
brucetoo <tuyong.ty@alibaba-inc.com>
2021-05-06 18:14:28 +0800

    fix: adjust some logic

--- packages/s2-core/src/interaction/row-col-resize.ts
+++ packages/s2-core/src/interaction/row-col-resize.ts
@@ -194,7 +194,6 @@ export class RowColResize extends BaseInteraction {
                 break;
             }
           }
-          this.spreadsheet.needUseCacheMeta = true;
           this.spreadsheet.emit(eventType, config);
           this.spreadsheet.setOptions(
             merge({}, this.spreadsheet.options, { style: config }),
