53fbc0d760238f18bb8520bb06829755f3ddb521
yingying.xwy <yingying.xwy@antgroup.com>
2021-05-21 11:16:31 +0800

    feat: :sparkles: draw the background of merged cells

--- packages/s2-core/src/common/interface/interaction.ts
+++ packages/s2-core/src/common/interface/interaction.ts
@@ -1,8 +1,18 @@
 import { DataCell, ColCell, CornerCell, RowCell } from 'src/cell';
+import { ViewMeta } from './index';
 
-export type S2CellTypes = DataCell | ColCell | CornerCell | RowCell;
+export type S2CellType = DataCell | ColCell | CornerCell | RowCell;
 
+export interface Cell {
+  cell: S2CellType;
+  meta: ViewMeta;
+}
 export interface SelectedState {
   stateName: string;
-  cells: S2CellTypes[];
+  cells: Cell[];
+}
+
+export interface Posision {
+  x: number;
+  y: number;
 }
---
+++ packages/s2-core/src/utils/interactions/merge-cell.ts
@@ -0,0 +1,114 @@
+import { find, isEqual, forEach } from 'lodash';
+import { BaseSpreadSheet } from 'src';
+import { SelectedStateName } from 'src/common/constant/interatcion';
+import { SelectedState, Cell } from 'src/common/interface/interaction';
+
+/**
+ *  according to the coordinates of the starting point of the rectangle,
+ * return the four sides of the rectangle in a clockwise direction.
+ * [TopLeft] --- [TopRight]
+ *    |               |
+ * [BottoLeft] -[BottomRight]
+ * @param x
+ * @param y
+ * @param width
+ * @param height
+ */
+const getRectangleEdges = (
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+) => {
+  const topLeft = [x, y];
+
+  const topRight = [x + width, y];
+
+  const bottomRight = [x + width, y + height];
+
+  const bottomLeft = [x, y + height];
+
+  return [
+    [topLeft, topRight],
+    [topRight, bottomRight],
+    [bottomRight, bottomLeft],
+    [bottomLeft, topLeft],
+  ];
+};
+
+/**
+ * return the edges without overlapping edges
+ * @param edges the collection of edges
+ */
+const unique = (edges: number[][]) => {
+  const result = [];
+  forEach(edges, (edge) => {
+    const reverseEdge = [edge[1], edge[0]];
+    if (!JSON.stringify(edges).includes(JSON.stringify(reverseEdge))) {
+      result.push(edge);
+    }
+  });
+  return result;
+};
+
+/**
+ * return the edge according to the  coordinate of current dedge
+ * eg: curEdge: [[0,0], [100,0]] then the next edge: [[100, 0 ], [100, 100]]
+ * @param curEdge the  coordinate of current edge
+ * @param edges the collection of edges
+ */
+const getNextEdge = (curEdge: number[], edges: number[][]) => {
+  const result = find(edges, (edge) => isEqual(edge[0], curEdge[1]));
+  return result;
+};
+
+/**
+ * return all the points of the polygon
+ * @param cells the collection of information of cells which needed be merged
+ */
+const getPolygonPoints = (cells: Cell[]) => {
+  let allEdges = [];
+
+  cells.forEach((cell) => {
+    const meta = cell?.meta;
+    const { x, y, width, height } = meta;
+    allEdges = allEdges.concat(getRectangleEdges(x, y, width, height));
+  });
+  allEdges = unique(allEdges);
+
+  let allPoints = [];
+
+  const startEdge = allEdges[0];
+  let curEdge = startEdge;
+  let nextEdge = [];
+
+  while (!isEqual(startEdge, nextEdge)) {
+    allPoints = allPoints.concat(curEdge);
+    nextEdge = getNextEdge(curEdge, allEdges);
+    curEdge = nextEdge;
+  }
+};
+
+/**
+ * draw the background of the merged cell
+ * @param sheet the base sheet instance
+ * @param curSelectedState
+ */
+export const drawMergeCellBg = (
+  sheet: BaseSpreadSheet,
+  curSelectedState: SelectedState,
+) => {
+  const { stateName, cells } = curSelectedState;
+  const allPoints = getPolygonPoints(cells);
+  if (stateName === SelectedStateName.SELECTED) {
+    const cellTheme = sheet.theme.view.cell;
+    sheet.facet.panelGroup.addShape('polygon', {
+      attrs: {
+        points: allPoints,
+        stroke: cellTheme.borderColor[0],
+        fill: cellTheme.backgroundColor,
+        lineWidth: cellTheme.borderWidth[0],
+      },
+    });
+  }
+};
