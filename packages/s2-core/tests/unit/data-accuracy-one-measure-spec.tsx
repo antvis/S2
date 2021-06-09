570b28d0644f52e4aff18865075ef46cd020c287
wjgogogo <906626481@qq.com>
2021-06-09 18:49:24 +0800

    chore: refine test structure

--- packages/s2-core/tests/unit/data-accuracy-one-measure-spec.tsx
+++ packages/s2-core/tests/unit/data-accuracy-one-measure-spec.tsx
@@ -8,7 +8,7 @@ import {
   SheetComponent,
   SpreadSheet,
 } from '../../src';
-import { getContainer } from '../helpers';
+import { getContainer } from '../util/helpers';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {
@@ -18,7 +18,7 @@ import {
   data4,
   data5,
   data6,
-} from '../datasets/data-accuracy';
+} from '../data/data-accuracy';
 
 let spreadsheet1: SpreadSheet;
 const setSpreadSheet = (
