82784134786fae4d2bd1ac7220f7a42ce70a395b
yangzhanmei <15809244435@163.com>
2021-07-14 19:53:13 +0800

    refactor: optimize sort action function

--- packages/s2-core/src/utils/sort-action.ts
+++ packages/s2-core/src/utils/sort-action.ts
@@ -1,4 +1,3 @@
-import { isNumber } from 'lodash';
 import { SortMethod } from 'src/common/interface';
 import { DataType } from 'src/data-set/interface';
 
@@ -6,32 +5,39 @@ import { DataType } from 'src/data-set/interface';
  * 执行排序
  * @param list - 待排序数组
  * @param sortMethod - 升、降序
- * @param key - 根据key数值排序
+ * @param key - 根据key数值排序，如果有key代表根据维度值排序，故按数字排，如果没有按照字典排
  */
 export const sortAction = (
-  list: Array<string | DataType>,
+  list: Array<string | number | DataType>,
   sortMethod?: SortMethod,
   key?: string,
 ) => {
   const sort = sortMethod === 'ASC' ? 1 : -1;
-  return list?.sort((pre: string | DataType, next: string | DataType) => {
-    let a = pre;
-    let b = next;
-    if (key) {
-      a = pre[key];
-      b = next[key];
-    }
-    if (isNumber(a) && isNumber(b)) {
-      return (a - b) * sort;
-    }
-    if (Number(a) || Number(b)) {
-      // 数值比较，解决 '2' > '11' 场景
-      return (Number(a) - Number(b)) * sort;
-    }
-    if (a && b) {
-      // 数据健全兼容，用户数据不全时，能够展示.
-      return a.toString().localeCompare(b.toString(), 'zh') * sort;
-    }
-    return sort;
-  });
+  const specialValues = ['-', undefined];
+  return list?.sort(
+    (pre: string | number | DataType, next: string | number | DataType) => {
+      let a = pre;
+      let b = next;
+      if (key) {
+        a = pre[key];
+        b = next[key];
+        if (Number(a) && Number(b)) {
+          return (Number(a) - Number(b)) * sort;
+        }
+        // @ts-ignore
+        if (specialValues?.includes(a)) {
+          return -sort;
+        }
+        // @ts-ignore
+        if (Number(a) && specialValues?.includes(b)) {
+          return sort;
+        }
+      }
+      if (a && b) {
+        // 数据健全兼容，用户数据不全时，能够展示.
+        return a.toString().localeCompare(b.toString(), 'zh') * sort;
+      }
+      return -sort;
+    },
+  );
 };
