d323c6d60889bc3c7c9c3f3d88ce47873f02a827
yingying.xwy <yingying.xwy@antgroup.com>
2021-07-20 11:17:30 +0800

    refactor: update the theme interface

--- packages/s2-core/src/common/interface/theme.ts
+++ packages/s2-core/src/common/interface/theme.ts
@@ -1,110 +1,159 @@
-export interface Palette {
-  // brand colors
-  brandColors: string[];
-  // neutral colors
-  grayColors: string[];
-  // semantic colors
-  semanticColors?: {
-    red?: string;
-    green?: string;
-  };
-}
-
-export interface CellTheme {
-  // cell's border color [horizontal, vertical]
-  borderColor?: [string, string];
-  // cell's border width [horizontal, vertical]
-  borderWidth?: [number, number];
-  // 行按照奇偶数来绘制背景（偶数行颜色）
-  crossColor?: string;
-  // dataCell 单元格背景色
-  backgroundColor?: string;
-  // row cell 单元格背景色
-  rowBackgroundColor?: string;
-  // col cell 单元格背景色
-  colBackgroundColor?: string;
-  // corner header背景色
-  cornerBackgroundColor?: string;
-  // 单元格交互色
-  interactiveBgColor?: string;
-  // interactive bg opacity[default, selected]
-  // 交互色在选择和非选中时的颜色饱和度
-  interactiveFillOpacity?: [number, number];
-  // cell的padding = top,right,bottom,left
-  padding?: [number, number, number, number];
-  // 文本在树形行头的的缩进值
-  textIndent?: number;
-  // 矩形条件格式的高度
-  intervalBgHeight?: number;
-  [key: string]: any;
-}
-
-export interface TextTheme {
-  fontSize?: number;
-  fontFamily?: string;
-  fill?: string;
-  textBaseline?: string;
-  fontWeight?: string;
-  textAlign?: string;
-  // text opacity[default, selected]
-  fillOpacity?: [number, number];
-}
-
-export interface IconTheme {
-  radius?: number;
-}
-
-export interface ScrollBarTheme {
-  trackColor?: string;
-  thumbHoverColor?: string;
-  thumbColor?: string;
-  size?: number;
-}
-
-export interface HeaderTheme {
-  cell?: CellTheme;
-  text?: TextTheme;
-  bolderText?: TextTheme;
-  icon?: IconTheme;
-  seriesNumberWidth?: number;
-}
-
-export interface DerivedMeasureTextTheme {
-  mainUp?: string;
-  mainDown?: string;
-  minorUp?: string;
-  minorDown?: string;
-}
-
-export interface ViewTheme {
-  minorText?: TextTheme;
-  derivedMeasureText?: DerivedMeasureTextTheme;
-  text?: TextTheme;
-  bolderText?: TextTheme;
-  cell?: CellTheme;
-}
-
-export interface CenterTheme {
-  verticalBorderColor?: string;
-  verticalBorderWidth?: number;
-  horizontalBorderColor?: string;
-  horizontalBorderWidth?: number;
-  showCenterRightShadow?: boolean;
-  centerRightShadowWidth?: number;
-  [key: string]: any;
-}
-
-export interface SpreadSheetTheme {
-  fontFamily?: string;
-  header?: HeaderTheme;
-  view?: ViewTheme;
-  scrollBar?: ScrollBarTheme;
-  center?: CenterTheme;
-  [key: string]: any;
-}
+import { CornerCell } from '@/cell';
 
 // 文本内容的水平对齐方式, 默认 left
 export type TextAlign = 'left' | 'center' | 'right';
 
 // 绘制文本时的基线, 对应垂直方向对齐方式 默认 bottom
 export type TextBaseline = 'top' | 'middle' | 'bottom';
+export interface Palette {
+  /* brand colors */
+  brandColors: string[];
+  /* neutral colors */
+  grayColors: string[];
+  /* semantic colors */
+  semanticColors?: {
+    red?: string;
+    green?: string;
+  };
+}
+
+export interface Padding {
+  top?: number;
+  right?: number;
+  bottom?: number;
+  left?: number;
+}
+
+export interface Margin {
+  top?: number;
+  right?: number;
+  bottom?: number;
+  left?: number;
+}
+
+export interface TextTheme {
+  fontFamily?: string;
+  fontSize?: number;
+  fontWeight?: string;
+  fill?: string;
+  opacity?: number;
+  textAlign?: TextAlign;
+  textBaseline?: TextBaseline;
+}
+
+export interface CellTheme {
+  /* 偶数行单元格背景色 */
+  crossBackgroundColor?: string;
+  /* 单元格背景色 */
+  backgroundColor?: string;
+  /* 单元格背景色透明度 */
+  backgroundColorOpacity?: number;
+  /* hover 态单元格背景色 */
+  hoverBackgroundColor?: string;
+  /* hover 十字器背景色 */
+  hoverLinkageBackgroundColor?: string;
+  /* 选中态单元格背景色 */
+  selectedBackgroundColor?: string;
+  /* 单元格水平边线颜色 */
+  horizontalBorderColor?: string;
+  /* 单元格水平边线颜色透明度 */
+  horizontalBorderColorOpacity?: number;
+  /* 单元格垂直边线颜色 */
+  verticalBorderColor?: string;
+  /* 单元格垂直边线颜色透明度 */
+  verticalBorderColorOpacity?: number;
+  /* hover 态单元格边线颜色 */
+  hoverBorderColor?: string;
+  /* 选中态单元格边线颜色 */
+  selectedBorderColor?: string;
+  /* 预选中态单元格边线 */
+  prepareSelectBorderColor?: string;
+  /* 单元格水平边线宽度 */
+  horizontalBorderWidth?: number;
+  /* 单元格垂直边线宽度 */
+  verticalBorderWidth?: number;
+  /* hover 态单元格边线宽度 */
+  hoverBorderWidth?: number;
+  /* 选中态单元格边线宽度 */
+  selectedBorderWidth?: number;
+  /* 单元格内边距 */
+  padding: Padding;
+  /* 单元格内条件格式-迷你条形图高度 */
+  miniBarChartHeight?: number;
+  /* 额外属性字段 */
+  [key: string]: any;
+}
+
+export interface IconTheme {
+  /* icon 填充色 */
+  fill?: string;
+  /* icon 圆角 */
+  radius?: number;
+  /* icon  大小 */
+  size?: number;
+  /* icon 外边距 */
+  margin?: Margin;
+  /* icon 内边距 */
+  padding?: Padding;
+}
+
+export interface ScrollBarTheme {
+  /* 滚动条轨道颜色 */
+  trackColor?: string;
+  /* 滚动条 hover 态颜色 */
+  thumbHoverColor?: string;
+  /* 滚动条颜色 */
+  thumbColor?: string;
+  /* 滚动条尺寸 */
+  size?: number;
+  /* 滚动条 hover 态尺寸 */
+  hoverSize?: number;
+}
+
+export interface SplitLine {
+  /* 水平分割线颜色 */
+  horizontalBorderColor?: string;
+  /* 水平分割线颜色透明度 */
+  horizontalBorderColorOpacity?: number;
+  /* 水平分割线宽度 */
+  horizontalBorderWidth?: number;
+  /* 垂直分割线颜色 */
+  verticalBorderColor?: string;
+  /* 垂直分割线颜色透明度 */
+  verticalBorderColorOpacity?: number;
+  /* 垂直分割线宽度 */
+  verticalBorderWidth?: number;
+  /* 分割线是否显示右侧外阴影 */
+  showRightShadow?: boolean;
+  /* 阴影宽度 */
+  shadowWidth?: number;
+  /* 阴影线性渐变色，第一个为起始颜色，第二个为结束颜色 */
+  shadowColors?: [string, string];
+}
+export interface DefaultCellTheme {
+  /* 粗体文本样式 */
+  bolderText?: TextTheme;
+  /* 文本样式 */
+  text?: TextTheme;
+  /* 单元格样式 */
+  cell?: CellTheme;
+  /* 图标样式 */
+  icon?: IconTheme;
+}
+
+export interface SpreadSheetTheme {
+  /* 角头样式 */
+  corner?: DefaultCellTheme;
+  /* 行头样式 */
+  rowHeader?: DefaultCellTheme;
+  /* 列头样式 */
+  colHeader?: DefaultCellTheme;
+  /* 交叉单于格样式 */
+  dataCell?: DefaultCellTheme;
+  /* 滚动条样式 */
+  scrollBar?: ScrollBarTheme;
+  /* 分割线样式 */
+  splitLine?: SplitLine;
+  /* 额外属性字段 */
+  [key: string]: any;
+}
