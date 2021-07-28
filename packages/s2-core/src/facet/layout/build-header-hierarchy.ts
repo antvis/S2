24dfde91df25b6a45de6e3eb10a0d2193823ee40
brucetoo <tuyong.ty@alibaba-inc.com>
2021-07-28 11:41:15 +0800

    feat: optimize code logic

--- packages/s2-core/src/facet/layout/build-header-hierarchy.ts
+++ packages/s2-core/src/facet/layout/build-header-hierarchy.ts
@@ -8,6 +8,99 @@ import { buildRowTreeHierarchy } from 'src/facet/layout/build-row-tree-hierarchy
 import { buildGridHierarchy } from 'src/facet/layout/build-gird-hierarchy';
 import { PivotDataSet } from '@/data-set';
 import { buildRowCustomTreeHierarchy } from '@/facet/layout/build-row-custom-tree-hierarchy';
+import { SpreadSheet } from '@/sheet-type';
+import { SpreadSheetFacetCfg } from '@/common/interface';
+
+interface HeaderParams {
+  isValueInCols: boolean;
+  isPivotMode: boolean;
+  moreThanOneValue: boolean;
+  hierarchy: Hierarchy;
+  rootNode: Node;
+  spreadsheet: SpreadSheet;
+  facetCfg: SpreadSheetFacetCfg;
+  fields: string[];
+}
+
+const handleGridRowColHierarchy = (params: HeaderParams) => {
+  const {
+    isValueInCols,
+    moreThanOneValue,
+    rootNode,
+    facetCfg,
+    hierarchy,
+    fields,
+  } = params;
+  // row grid hierarchy
+  const addTotalMeasureInTotal = !isValueInCols && moreThanOneValue;
+  // value in rows and only has one value(or none)
+  const addMeasureInTotalQuery = !isValueInCols && !moreThanOneValue;
+  buildGridHierarchy({
+    addTotalMeasureInTotal,
+    addMeasureInTotalQuery,
+    parentNode: rootNode,
+    currentField: fields[0],
+    fields,
+    facetCfg,
+    hierarchy,
+  });
+};
+
+const handleCustomTreeRowHierarchy = (params: HeaderParams) => {
+  const { facetCfg, rootNode, hierarchy } = params;
+  const customTreeItems = facetCfg.dataSet.fields.customTreeItems;
+  // row custom tree header
+  buildRowCustomTreeHierarchy({
+    customTreeItems: customTreeItems,
+    facetCfg,
+    level: 0,
+    parentNode: rootNode,
+    hierarchy,
+  });
+};
+
+const handleTreeRowHierarchy = (params: HeaderParams) => {
+  const { facetCfg, rootNode, hierarchy } = params;
+  const { hierarchyType, rows, dataSet } = facetCfg;
+  if (hierarchyType === 'tree') {
+    // row tree hierarchy(value must stay in colHeader)
+    buildRowTreeHierarchy({
+      level: 0,
+      currentField: rows[0],
+      pivotMeta: (dataSet as PivotDataSet).rowPivotMeta,
+      facetCfg,
+      parentNode: rootNode,
+      hierarchy,
+    });
+  } else {
+    handleCustomTreeRowHierarchy(params);
+  }
+};
+
+const handleRowHeaderHierarchy = (params: HeaderParams) => {
+  const { isPivotMode, spreadsheet } = params;
+  if (isPivotMode) {
+    if (spreadsheet.isHierarchyTreeType()) {
+      handleTreeRowHierarchy(params);
+    } else {
+      handleGridRowColHierarchy(params);
+    }
+  } else {
+    // TODO 光生哥看下是否需要更改
+    throw new Error('There are not header hierarchy in table mode');
+  }
+};
+
+const handleColHeaderHierarchy = (params: HeaderParams) => {
+  const { isPivotMode } = params;
+  if (isPivotMode) {
+    handleGridRowColHierarchy(params);
+  } else {
+    // TODO 光生哥看下是否需要更改
+    throw new Error('There are not header hierarchy in table mode');
+  }
+};
+
 /**
  * Header Hierarchy
  * - row header
@@ -29,76 +122,36 @@ export const buildHeaderHierarchy = (
   params: BuildHeaderParams,
 ): BuildHeaderResult => {
   const { isRowHeader, facetCfg } = params;
-  const { rows, cols, values, spreadsheet, dataSet, hierarchyType } = facetCfg;
+  const { values, spreadsheet, rows, cols } = facetCfg;
   const isValueInCols = spreadsheet.dataCfg.fields.valueInCols;
   const isPivotMode = spreadsheet.isPivotMode();
   const moreThanOneValue = values.length > 1;
   const rootNode = Node.rootNode();
   const hierarchy = new Hierarchy();
+  const headParams = {
+    isValueInCols,
+    isPivotMode,
+    moreThanOneValue,
+    rootNode,
+    hierarchy,
+    spreadsheet,
+    facetCfg,
+    fields: isRowHeader ? rows : cols,
+  } as HeaderParams;
   if (isRowHeader) {
-    if (isPivotMode) {
-      if (spreadsheet.isHierarchyTreeType()) {
-        if (hierarchyType === 'tree') {
-          // row tree hierarchy(value must stay in colHeader)
-          buildRowTreeHierarchy({
-            level: 0,
-            currentField: rows[0],
-            pivotMeta: (dataSet as PivotDataSet).rowPivotMeta,
-            facetCfg,
-            parentNode: rootNode,
-            hierarchy,
-          });
-        } else {
-          const customTreeItems = facetCfg.dataSet.fields.customTreeItems;
-          // row custom tree header
-          buildRowCustomTreeHierarchy({
-            customTreeItems: customTreeItems,
-            facetCfg,
-            level: 0,
-            parentNode: rootNode,
-            hierarchy,
-          });
-        }
-        return {
-          hierarchy,
-          leafNodes: hierarchy.getNodes(),
-        } as BuildHeaderResult;
-      }
-      // row grid hierarchy
-      const addTotalMeasureInTotal = !isValueInCols && moreThanOneValue;
-      // value in rows and only has one value(or none)
-      const addMeasureInTotalQuery = !isValueInCols && !moreThanOneValue;
-      buildGridHierarchy({
-        addTotalMeasureInTotal,
-        addMeasureInTotalQuery,
-        parentNode: rootNode,
-        currentField: rows[0],
-        fields: rows,
-        facetCfg,
-        hierarchy,
-      });
-    } else {
-      throw new Error('There are not header hierarchy in table mode');
-    }
-  } else if (isPivotMode) {
-    // only has grid hierarchy
-    const addTotalMeasureInTotal = isValueInCols && moreThanOneValue;
-    // value in cols and only has one value(or none)
-    const addMeasureInTotalQuery = isValueInCols && !moreThanOneValue;
-    buildGridHierarchy({
-      addTotalMeasureInTotal,
-      addMeasureInTotalQuery,
-      parentNode: rootNode,
-      currentField: cols[0],
-      fields: cols,
-      facetCfg,
-      hierarchy,
-    });
+    handleRowHeaderHierarchy(headParams);
   } else {
-    throw new Error('There are not header hierarchy in table mode');
+    handleColHeaderHierarchy(headParams);
   }
+
+  const getLeafNodes = () => {
+    if (!isRowHeader) return hierarchy.getLeaves();
+    return spreadsheet.isHierarchyTreeType()
+      ? hierarchy.getNodes()
+      : hierarchy.getLeaves();
+  };
   return {
     hierarchy,
-    leafNodes: hierarchy.getLeaves(),
+    leafNodes: getLeafNodes(),
   } as BuildHeaderResult;
 };
