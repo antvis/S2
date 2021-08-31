df294d8de57dcc3c7295ab6124b84a70c74f49f3
baoming.qbm <baoming.qbm@antfin.com>
2021-08-31 20:47:31 +0800

    test(list-table): add list-table data process spec

--- packages/s2-core/tests/unit/core/data-process/list-table-spec.tsx
+++ packages/s2-core/tests/unit/core/data-process/list-table-spec.tsx
@@ -1,12 +1,20 @@
 /** 明细表核心数据流程 */
 import { flattenDeep, get } from 'lodash';
 import { SpreadSheet } from 'src/sheet-type';
-import { EXTRA_FIELD, VALUE_FIELD } from 'src/common/constant';
-import { PivotDataSet } from 'src/data-set/pivot-data-set';
-import STANDARD_SPREADSHEET_DATA from '../../../data/standard-spreadsheet-data';
+import { EXTRA_FIELD, VALUE_FIELD } from '@/common/constant';
+import { PivotDataSet } from '@/data-set/pivot-data-set';
+import STANDARD_SPREADSHEET_DATA from '../../../data/standard-spreadsheet-data.json';
 import { getContainer } from '../../../util/helpers';
 
 describe('List Table Core Data Process', () => {
+  const options = { width: 600, height: 400, mode: 'table' };
+  const dataCfg = {
+    fields: { columns: ['province', 'city', 'category', 'subCategory', 'price'] },
+    data: STANDARD_SPREADSHEET_DATA.data,
+  };
+  const ss = new SpreadSheet(getContainer(), dataCfg, options);
+  ss.render();
+
   describe('1、Transform indexes data', () => {
     
   });
