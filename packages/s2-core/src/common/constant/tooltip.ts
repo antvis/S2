b84bbca2b1f20cdb1788dbffc68086722ead7ec5
yingying.xwy <yingying.xwy@antgroup.com>
2021-09-01 18:22:58 +0800

    chore: :memo: add i18n for the group order

--- packages/s2-core/src/common/constant/tooltip.ts
+++ packages/s2-core/src/common/constant/tooltip.ts
@@ -1,5 +1,6 @@
 import { OrderOption } from '@/common/interface';
 import { S2_PREFIX_CLS } from '@/common/constant/classnames';
+import { i18n } from '@/common/i18n';
 
 export const TOOLTIP_PREFIX_CLS = `${S2_PREFIX_CLS}-tooltip`;
 
@@ -18,7 +19,7 @@ export const POSITION_X_OFFSET = 10;
 export const POSITION_Y_OFFSET = 10;
 
 export const ORDER_OPTIONS: OrderOption[] = [
-  { sortMethod: 'ASC', type: 'groupAsc', name: '组内升序' },
-  { sortMethod: 'DESC', type: 'groupDesc', name: '组内降序' },
-  { sortMethod: null, type: 'none', name: '不排序' },
+  { sortMethod: 'ASC', type: 'groupAsc', name: i18n('组内升序') },
+  { sortMethod: 'DESC', type: 'groupDesc', name: i18n('组内降序') },
+  { sortMethod: null, type: 'none', name: i18n('不排序') },
 ];
