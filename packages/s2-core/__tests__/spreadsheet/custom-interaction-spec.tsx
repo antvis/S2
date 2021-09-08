24089befee1eae6b29be0959383a4cb3e61a0322
wjgogogo <906626481@qq.com>
2021-09-08 10:28:05 +0800

    test: simplify interaction test

--- packages/s2-core/__tests__/spreadsheet/custom-interaction-spec.tsx
+++ packages/s2-core/__tests__/spreadsheet/custom-interaction-spec.tsx
@@ -1,120 +1,25 @@
-import { merge } from 'lodash';
-import { act } from 'react-dom/test-utils';
-import 'antd/dist/antd.min.css';
-import ReactDOM from 'react-dom';
+import { S2Options } from '@/common/interface';
 import React from 'react';
-import { Switch } from 'antd';
-import { getContainer, getMockData } from '../util/helpers';
-import { SheetComponent } from '../../src';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { getContainer } from '../util/helpers';
+import { SheetEntry } from '../util/sheet-entry';
 import { CustomHover } from './custom/custom-interaction';
 
-const data = getMockData('../data/tableau-supermarket.csv');
-
-const getDataCfg = () => {
-  return {
-    fields: {
-      // rows has value
-      rows: ['area', 'province', 'city'],
-      columns: ['type', 'sub_type'],
-      values: ['profit'],
+const options: Partial<S2Options> = {
+  customInteractions: [
+    {
+      key: 'spreadsheet:custom-hover',
+      interaction: CustomHover,
     },
-    meta: [
-      {
-        field: 'sale_amt',
-        name: '销售额',
-      },
-      {
-        field: 'count',
-        name: '销售个数',
-      },
-      {
-        field: 'discount',
-        name: '折扣',
-      },
-      {
-        field: 'profit',
-        name: '利润',
-      },
-    ],
-    data,
-    sortParams: [
-      {
-        sortFieldId: 'area',
-        sortMethod: 'ASC',
-      },
-      {
-        sortFieldId: 'province',
-        sortMethod: 'DESC',
-      },
-    ],
-  };
+  ],
+  tooltip: {
+    showTooltip: true,
+  },
 };
 
-const getOptions = () => {
-  return {
-    width: 1000,
-    height: 600,
-    hierarchyType: 'grid',
-    hierarchyCollapse: false,
-    showSeriesNumber: true,
-    freezeRowHeader: true,
-    mode: 'pivot',
-    valueInCols: true,
-    conditions: {
-      text: [],
-      interval: [],
-      background: [],
-      icon: [],
-    },
-    style: {
-      treeRowsWidth: 100,
-      collapsedRows: {},
-      colCfg: {
-        widthByFieldValue: {},
-        heightByField: {},
-        colWidthType: 'compact',
-      },
-      cellCfg: {
-        height: 32,
-      },
-      device: 'pc',
-    },
-    customInteractions: [
-      {
-        key: 'spreadsheet:custom-hover',
-        interaction: CustomHover,
-      },
-    ],
-    tooltip: {
-      showTooltip: true,
-    },
-  };
-};
-
-function MainLayout(props) {
-  const [options, setOptions] = React.useState(props.options);
-  const [dataCfg, setDataCfg] = React.useState(props.dataCfg);
-
-  const onCheckChanged = (checked) => {
-    setOptions(
-      merge({}, options, {
-        hierarchyType: checked ? 'tree' : 'grid',
-      }),
-    );
-  };
-  return (
-    <div>
-      <div style={{ display: 'inline-block' }}>
-        <Switch
-          checkedChildren="树形"
-          unCheckedChildren="平铺"
-          defaultChecked={false}
-          onChange={onCheckChanged}
-        />
-      </div>
-      <SheetComponent dataCfg={dataCfg} adaptive={false} options={options} />
-    </div>
-  );
+function MainLayout() {
+  return <SheetEntry dataCfg={{}} options={options} />;
 }
 
 describe('spreadsheet normal spec', () => {
@@ -123,9 +28,6 @@ describe('spreadsheet normal spec', () => {
   });
 
   act(() => {
-    ReactDOM.render(
-      <MainLayout dataCfg={getDataCfg()} options={getOptions()} />,
-      getContainer(),
-    );
+    ReactDOM.render(<MainLayout />, getContainer());
   });
 });
