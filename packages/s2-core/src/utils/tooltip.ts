d90a21f7f4bfeee34301caef57a6ceedef648c39
蒺藜 <zhanmei.yzm@alibaba-inc.com>
2021-09-09 19:57:18 +0800

    fix: float data's precision

--- packages/s2-core/src/utils/tooltip.ts
+++ packages/s2-core/src/utils/tooltip.ts
@@ -16,7 +16,7 @@ import {
   map,
   pick,
   some,
-  sumBy,
+  reduce,
   uniq,
   noop,
   mapKeys,
@@ -56,16 +56,44 @@ const isNotNumber = (v) => {
 };
 
 /**
+ * 浮点数加法，解决精度问题
+ * 因为乘法也有精度问题，故用字符串形式
+ * @param arg1
+ * @param arg2
+ */
+function accAdd(arg1: number, arg2: number) {
+  const [pre1, next1 = ''] = arg1?.toString()?.split('.');
+  const [pre2, next2 = ''] = arg2?.toString()?.split('.');
+  const r1 = next1?.length || 0;
+  const r2 = next2?.length || 0;
+  const m = Math.pow(10, Math.max(r1, r2));
+  const suffix = Array(Math.abs(r1 - r2))
+    .fill('0')
+    ?.join('');
+  const number1 = r2 > r1 ? `${pre1}${next1}${suffix}` : `${pre1}${next1}`;
+  const number2 = r1 > r2 ? `${pre2}${next2}${suffix}` : `${pre2}${next2}`;
+
+  return (Number.parseInt(number1) + Number.parseInt(number2)) / m;
+}
+
+/**
  * calculate sum value
  */
 export const getDataSumByField = (
   data: TooltipDataItem[],
   field: string,
 ): number => {
-  return sumBy(data, (datum) => {
-    const v = get(datum, field, 0);
-    return isNotNumber(v) ? 0 : Number.parseFloat(v);
-  });
+  const sum = reduce(
+    data,
+    (pre, next) => {
+      const fieldValue = get(next, field, 0);
+      const v = isNotNumber(fieldValue) ? 0 : Number.parseFloat(fieldValue);
+
+      return accAdd(pre, v);
+    },
+    0,
+  );
+  return sum;
 };
 
 /** whether the data of hover is selected */
@@ -379,7 +407,8 @@ export const getSummaries = (params: SummaryParam): TooltipSummaryOptions[] => {
     if (getShowValue) {
       value = getShowValue(selected, VALUE_FIELD);
     }
-    if (every(selected, (item) => isNotNumber(get(item, VALUE_FIELD)))) { // 如果选中的单元格都无数据，则显示"-"
+    if (every(selected, (item) => isNotNumber(get(item, VALUE_FIELD)))) {
+      // 如果选中的单元格都无数据，则显示"-"
       value = '-';
     } else {
       const dataSum = getDataSumByField(selected, VALUE_FIELD);
