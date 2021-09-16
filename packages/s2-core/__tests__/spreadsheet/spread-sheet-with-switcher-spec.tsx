9e905c912ac4b9556460e1461826bc6244efffd2
wjgogogo <906626481@qq.com>
2021-09-16 20:22:02 +0800

    test: fix test file typo

--- packages/s2-core/__tests__/spreadsheet/spread-sheet-with-switcher-spec.tsx
+++ packages/s2-core/__tests__/spreadsheet/spread-sheet-with-switcher-spec.tsx
@@ -3,47 +3,41 @@ import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { getContainer } from '../util/helpers';
 import { SheetEntry } from '../util/sheet-entry';
-import {
-  DimensionItem,
-  MeasureItem,
-} from '@/components/switcher/dimension/dimension';
 import { Switcher } from '@/components/switcher';
+import { Item } from '@/components/switcher/interface';
 
-const mockRows: DimensionItem[] = [
+const mockRows: Item[] = [
   { id: 'area', displayName: '区域' },
   { id: 'province', displayName: '省' },
   { id: 'city', displayName: '城市' },
 ];
-const mockCols: DimensionItem[] = [
-  { id: 'area', displayName: '区域' },
-  { id: 'province', displayName: '省' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
+const mockCols: Item[] = [
+  { id: 'county1', displayName: '一县' },
+  { id: 'county2', displayName: '二县' },
+  { id: 'county3', displayName: '三县' },
+  { id: 'county4', displayName: '四县' },
+  { id: 'county5', displayName: '五县' },
 ];
-const mockValues: MeasureItem[] = [
-  { id: 'area', displayName: '区域' },
-  { id: 'province', displayName: '省' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
-  { id: 'city', displayName: '城市' },
+const mockValues: Item[] = [
+  {
+    id: 'price',
+    displayName: '价格',
+    checked: true,
+    derivedValues: [
+      { id: 'price-rc', displayName: '环比' },
+      { id: 'price-ac', displayName: '同比' },
+    ],
+  },
+  {
+    id: 'cost',
+    displayName: '成本',
+    derivedValues: [
+      { id: 'cost-rc', displayName: '环比' },
+      { id: 'cost-ac', displayName: '同比' },
+    ],
+  },
+  { id: 'count', displayName: '数量' },
+  { id: 'rank', displayName: '等级' },
 ];
 
 function MainLayout() {
@@ -56,6 +50,10 @@ function MainLayout() {
           rows={mockRows}
           cols={mockCols}
           values={mockValues}
+          onSubmit={(result) => {
+            // eslint-disable-next-line no-console
+            console.log('result: ', result);
+          }}
         ></Switcher>
       </div>
       <SheetEntry
