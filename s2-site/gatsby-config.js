43a6006d942c9dcd5a98bbb29e92988be9736a39
yangzhanmei <15809244435@163.com>
2021-09-28 17:20:28 +0800

    fix: config repeat optimize

--- s2-site/gatsby-config.js
+++ s2-site/gatsby-config.js
@@ -86,16 +86,8 @@ module.exports = {
           zh: '小计总计',
           en: 'Total',
         },
-        order: 4,
-      },
-      {
-        slug: 'manual/total',
-        title: {
-          zh: '小计总计',
-          en: 'Total',
-        },
-        order: 4,
-      },
+        order: 5,
+      }
     ],
     // demo配置
     examples: [
