28bfb3d80ed02d5d7b7c75e2585394ea55038e4e
baoming.qbm <baoming.qbm@antfin.com>
2021-09-28 10:09:53 +0800

    fix(pivot-dataset): fix lint error

--- packages/s2-core/src/utils/dataset/pivot-data-set.ts
+++ packages/s2-core/src/utils/dataset/pivot-data-set.ts
@@ -1,5 +1,5 @@
-import { DataType } from '@/data-set/interface';
 import { set, map, isUndefined } from 'lodash';
+import { DataType } from '@/data-set/interface';
 import {
   DataPathParams,
   PivotMeta,
@@ -14,76 +14,6 @@ interface Param {
   rowPivotMeta?: PivotMeta;
   colPivotMeta?: PivotMeta;
 }
-/**
- * 转换原始数据为二维数组数据
- * @param rows 
- * @param columns 
- * @param originData
- * @param totalData
- */
-export function transformIndexesData(params: Param) {
-  const { rows, columns, originData, totalData, sortedDimensionValues,
-    rowPivotMeta, colPivotMeta } = params;
-  const paths = [];
-  const indexesData = [];
-  for (const data of [...originData, ...totalData]) {
-    const rowDimensionValues = transformDimensionsValues(data, rows, sortedDimensionValues);
-    const colDimensionValues = transformDimensionsValues(data, columns, sortedDimensionValues);
-    const path = getDataPath({
-      rowDimensionValues,
-      colDimensionValues,
-      rowPivotMeta,
-      colPivotMeta,
-      isFirstCreate: true,
-      careUndefined: totalData?.length > 0,
-      rowFields: rows,
-      colFields: columns,
-    });
-    paths.push(path);
-    set(indexesData, path, data);
-  }
-
-  return {
-    paths,
-    indexesData,
-    rowPivotMeta,
-    colPivotMeta,
-  };
-}
-
-/**
- * Transform from origin single data to correct dimension values
- * data: {
- *  price: 16,
- *  province: '辽宁省',
- *  city: '芜湖市',
- *  category: '家具',
- *  subCategory: '椅子',
- * }
- * dimensions: [province, city]
- * return [辽宁省, 芜湖市]
- *
- * @param record
- * @param dimensions
- */
-export function transformDimensionsValues(
-  record: DataType,
-  dimensions: string[],
-  sortedDimensionValues: Map<string, Set<string>>
-): string[] {
-  return map(dimensions, (dimension) => {
-    const dimensionValue = record[dimension];
-    if (!sortedDimensionValues.has(dimension)) {
-      sortedDimensionValues.set(dimension, new Set());
-    }
-    const values = sortedDimensionValues.get(dimension);
-    values.add(record[dimension]);
-
-    return dimensionValue;
-  });
-}
-
-
 /**
  * Transform a single data to path
  * {
@@ -103,7 +33,7 @@ export function transformDimensionsValues(
  *
  * @param params
  */
-export function getDataPath(params: DataPathParams) {
+ export function getDataPath(params: DataPathParams) {
   const {
     rowDimensionValues,
     colDimensionValues,
@@ -162,4 +92,73 @@ export function getDataPath(params: DataPathParams) {
   const result = rowPath.concat(...colPath);
 
   return result;
+}
+
+/**
+ * Transform from origin single data to correct dimension values
+ * data: {
+ *  price: 16,
+ *  province: '辽宁省',
+ *  city: '芜湖市',
+ *  category: '家具',
+ *  subCategory: '椅子',
+ * }
+ * dimensions: [province, city]
+ * return [辽宁省, 芜湖市]
+ *
+ * @param record
+ * @param dimensions
+ */
+ export function transformDimensionsValues(
+  record: DataType,
+  dimensions: string[],
+  sortedDimensionValues: Map<string, Set<string>>
+): string[] {
+  return map(dimensions, (dimension) => {
+    const dimensionValue = record[dimension];
+    if (!sortedDimensionValues.has(dimension)) {
+      sortedDimensionValues.set(dimension, new Set());
+    }
+    const values = sortedDimensionValues.get(dimension);
+    values.add(record[dimension]);
+
+    return dimensionValue;
+  });
+}
+
+/**
+ * 转换原始数据为二维数组数据
+ * @param rows 
+ * @param columns 
+ * @param originData
+ * @param totalData
+ */
+export function transformIndexesData(params: Param) {
+  const { rows, columns, originData, totalData, sortedDimensionValues,
+    rowPivotMeta, colPivotMeta } = params;
+  const paths = [];
+  const indexesData = [];
+  for (const data of [...originData, ...totalData]) {
+    const rowDimensionValues = transformDimensionsValues(data, rows, sortedDimensionValues);
+    const colDimensionValues = transformDimensionsValues(data, columns, sortedDimensionValues);
+    const path = getDataPath({
+      rowDimensionValues,
+      colDimensionValues,
+      rowPivotMeta,
+      colPivotMeta,
+      isFirstCreate: true,
+      careUndefined: totalData?.length > 0,
+      rowFields: rows,
+      colFields: columns,
+    });
+    paths.push(path);
+    set(indexesData, path, data);
+  }
+
+  return {
+    paths,
+    indexesData,
+    rowPivotMeta,
+    colPivotMeta,
+  };
 }
