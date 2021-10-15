bb0f1175eb0b51d10bb6e9ddba0ea5473a0d0405
xinhui.zxh <xinhui.zxh@antgroup.com>
2021-10-15 17:33:32 +0800

    fix: use lodash

--- packages/s2-core/src/cell/table-col-cell.ts
+++ packages/s2-core/src/cell/table-col-cell.ts
@@ -111,7 +111,7 @@ export class TableColCell extends ColCell {
       { cursor: 'pointer' },
     );
 
-    if (!spreadsheet.options.headerActionIcons?.length) {
+    if (!isEmpty(spreadsheet.options.headerActionIcons)) {
       renderDetailTypeSortIcon(
         this,
         spreadsheet,
