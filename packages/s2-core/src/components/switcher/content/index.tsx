d31ce63a87d87878f033f51ac2a0e7f4d90c2b31
Wenjun Xu <906626481@qq.com>
2021-10-20 12:55:25 +0800

    feat: add confirm and cancel button for switcher (#495)
    * feat: add confirm and cancel button in switcher
    
    * docs: update switcher api
    
    * refactor: extract content title props
    
    * refactor: replace tag  selector

--- packages/s2-core/src/components/switcher/content/index.tsx
+++ packages/s2-core/src/components/switcher/content/index.tsx
@@ -1,8 +1,8 @@
 import { ReloadOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
-import { isNil } from 'lodash';
+import { isEqual, isNil } from 'lodash';
 import cx from 'classnames';
-import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import React, { FC, forwardRef, useImperativeHandle, useState } from 'react';
 import {
   BeforeCapture,
   DragDropContext,
@@ -30,125 +30,155 @@ export interface SwitcherContentRef {
 }
 
 export interface SwitcherContentProps extends SwitcherFields {
+  contentTitleText?: string;
   resetText?: string;
+  innerContentClassName?: string;
+  onToggleVisible: () => void;
+  onSubmit?: (result: SwitcherResult) => void;
 }
 
-export const SwitcherContent = forwardRef(
-  ({ resetText, ...defaultFields }: SwitcherContentProps, ref) => {
-    const defaultState = getSwitcherState(defaultFields);
+export const SwitcherContent: FC<SwitcherContentProps> = ({
+  innerContentClassName,
+  contentTitleText,
+  resetText,
+  onToggleVisible,
+  onSubmit,
+  ...defaultFields
+}) => {
+  const defaultState = getSwitcherState(defaultFields);
 
-    const [state, setState] = useState<SwitcherState>(defaultState);
-    const [draggingItemId, setDraggingItemId] = useState<string>(null);
+  const [state, setState] = useState<SwitcherState>(defaultState);
+  const [draggingItemId, setDraggingItemId] = useState<string>(null);
 
-    const nonEmptyCount = getNonEmptyFieldCount(defaultFields);
+  const nonEmptyCount = getNonEmptyFieldCount(defaultFields);
 
-    const onBeforeDragStart = (initial: BeforeCapture) => {
-      setDraggingItemId(initial.draggableId);
-    };
+  const onBeforeDragStart = (initial: BeforeCapture) => {
+    setDraggingItemId(initial.draggableId);
+  };
 
-    const onDragEnd = ({ destination, source }: DropResult) => {
-      // reset dragging item id
-      setDraggingItemId(null);
+  const onDragEnd = ({ destination, source }: DropResult) => {
+    // reset dragging item id
+    setDraggingItemId(null);
 
-      // cancelled or drop to where can't drop
-      if (!destination) {
-        return;
-      }
-      // don't change position
-      if (
-        destination.droppableId === source.droppableId &&
-        destination.index === source.index
-      ) {
-        return;
-      }
+    // cancelled or drop to where can't drop
+    if (!destination) {
+      return;
+    }
+    // don't change position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
 
-      const updatedState = moveItem(
-        state[source.droppableId],
-        state[destination.droppableId],
-        source,
-        destination,
-      );
-      setState({ ...state, ...updatedState });
-    };
-
-    const onReset = () => {
-      setState(defaultState);
-    };
-
-    useImperativeHandle(
-      ref,
-      () => ({
-        getResult() {
-          return generateSwitchResult(state);
-        },
-      }),
-      [state],
+    const updatedState = moveItem(
+      state[source.droppableId],
+      state[destination.droppableId],
+      source,
+      destination,
     );
+    setState({ ...state, ...updatedState });
+  };
 
-    const onVisibleItemChange = (
-      fieldType: FieldType,
-      checked: boolean,
-      id: string,
-      parentId?: string,
-    ) => {
-      const updatedState = checkItem(state[fieldType], checked, id, parentId);
-      setState({
-        ...state,
-        [fieldType]: updatedState,
-      });
-    };
+  const onReset = () => {
+    setState(defaultState);
+  };
 
-    return (
-      <DragDropContext
-        onBeforeCapture={onBeforeDragStart}
-        onDragEnd={onDragEnd}
+  const onConfirm = () => {
+    onToggleVisible();
+    onSubmit?.(generateSwitchResult(state));
+  };
+
+  const onVisibleItemChange = (
+    fieldType: FieldType,
+    checked: boolean,
+    id: string,
+    parentId?: string,
+  ) => {
+    const updatedState = checkItem(state[fieldType], checked, id, parentId);
+    setState({
+      ...state,
+      [fieldType]: updatedState,
+    });
+  };
+
+  const isNothingChanged = isEqual(defaultState, state);
+  return (
+    <DragDropContext onBeforeCapture={onBeforeDragStart} onDragEnd={onDragEnd}>
+      <div
+        className={cx(
+          innerContentClassName,
+          getSwitcherClassName(CLASS_NAME_PREFIX),
+        )}
       >
-        <div className={getSwitcherClassName(CLASS_NAME_PREFIX)}>
-          <header className={getSwitcherClassName(CLASS_NAME_PREFIX, 'header')}>
-            {i18n('行列切换')}
-          </header>
-          <main
-            className={cx(
-              getSwitcherClassName(CLASS_NAME_PREFIX, 'main'),
-              getMainLayoutClassName(nonEmptyCount),
+        <header className={getSwitcherClassName(CLASS_NAME_PREFIX, 'header')}>
+          {contentTitleText}
+        </header>
+        <main
+          className={cx(
+            getSwitcherClassName(CLASS_NAME_PREFIX, 'main'),
+            getMainLayoutClassName(nonEmptyCount),
+          )}
+        >
+          {SWITCHER_FIELDS.map(
+            (type) =>
+              isNil(defaultFields[type]) || (
+                <Dimension
+                  {...defaultFields[type]}
+                  key={type}
+                  fieldType={type}
+                  items={state[type]}
+                  crossRows={shouldCrossRows(nonEmptyCount, type)}
+                  droppableType={SWITCHER_CONFIG[type].droppableType}
+                  draggingItemId={draggingItemId}
+                  onVisibleItemChange={onVisibleItemChange}
+                />
+              ),
+          )}
+        </main>
+        <footer className={getSwitcherClassName(CLASS_NAME_PREFIX, 'footer')}>
+          <Button
+            type={'text'}
+            icon={<ReloadOutlined />}
+            className={getSwitcherClassName(
+              CLASS_NAME_PREFIX,
+              'footer',
+              'reset-button',
+            )}
+            disabled={isNothingChanged}
+            onClick={onReset}
+          >
+            {resetText}
+          </Button>
+          <div
+            className={getSwitcherClassName(
+              CLASS_NAME_PREFIX,
+              'footer',
+              'actions',
             )}
           >
-            {SWITCHER_FIELDS.map(
-              (type) =>
-                isNil(defaultFields[type]) || (
-                  <Dimension
-                    {...defaultFields[type]}
-                    fieldType={type}
-                    items={state[type]}
-                    crossRows={shouldCrossRows(nonEmptyCount, type)}
-                    droppableType={SWITCHER_CONFIG[type].droppableType}
-                    draggingItemId={draggingItemId}
-                    onVisibleItemChange={onVisibleItemChange}
-                  />
-                ),
-            )}
-          </main>
-          <footer>
-            <Button
-              type={'text'}
-              icon={<ReloadOutlined />}
-              className={getSwitcherClassName(
-                CLASS_NAME_PREFIX,
-                'reset-button',
-              )}
-              onClick={onReset}
-            >
-              {resetText ?? i18n('恢复默认')}
+            <Button className="action-button" onClick={onToggleVisible}>
+              {i18n('取消')}
             </Button>
-          </footer>
-        </div>
-      </DragDropContext>
-    );
-  },
-);
+            <Button
+              className="action-button"
+              type="primary"
+              disabled={isNothingChanged}
+              onClick={onConfirm}
+            >
+              {i18n('确定')}
+            </Button>
+          </div>
+        </footer>
+      </div>
+    </DragDropContext>
+  );
+};
 
 SwitcherContent.displayName = 'SwitcherContent';
 
 SwitcherContent.defaultProps = {
+  contentTitleText: i18n('行列切换'),
   resetText: i18n('恢复默认'),
 };
