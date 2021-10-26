c0180380000fae8f4cd1dbaf2160e6f1b91bd430
yingying.xwy <yingying.xwy@antgroup.com>
2021-10-26 11:30:28 +0800

    chore: :pencil2: fix the typo

--- packages/s2-core/src/common/interface/store.ts
+++ packages/s2-core/src/common/interface/store.ts
@@ -4,7 +4,7 @@ import { ValueRanges } from './condition';
 import { S2DataConfig } from './s2DataConfig';
 import { Node } from '@/facet/layout/node';
 import { PartDrillDownFieldInLevel } from '@/components/sheets/interface';
-import { GuiIcon } from '@/';
+import { GuiIcon } from '@/common/icons';
 
 export interface Selected {
   type:
