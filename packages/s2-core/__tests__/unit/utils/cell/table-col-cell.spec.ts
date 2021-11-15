043847918ca5f90bceb28dffd31116eeb45494e7
xinhui.zxh <xinhui.zxh@antgroup.com>
2021-11-15 18:56:22 +0800

    feat: update spec

--- packages/s2-core/__tests__/unit/utils/cell/table-col-cell.spec.ts
+++ packages/s2-core/__tests__/unit/utils/cell/table-col-cell.spec.ts
@@ -1,3 +1,4 @@
+import { Node } from 'src/facet/layout/node';
 import { SpreadSheet } from 'src/sheet-type';
 import { getOccupiedWidthForTableCol } from 'src/utils/cell/table-col-cell';
 
@@ -5,7 +6,14 @@ describe('Table col cell util test', () => {
   test('should return right occupied width for table col', () => {
     const mockSS = {
       store: new Map(),
+      options: {
+        showDefaultHeaderActionIcon: true,
+      },
     };
+
+    const mockNode = {
+      field: 'city',
+    } as unknown as Node;
 
     mockSS.store.set('hiddenColumnsDetail', [
       {
@@ -17,7 +25,7 @@ describe('Table col cell util test', () => {
       },
     ]);
     expect(
-      getOccupiedWidthForTableCol(mockSS as unknown as SpreadSheet, 'city', {
+      getOccupiedWidthForTableCol(mockSS as unknown as SpreadSheet, mockNode, {
         cell: {
           padding: {
             left: 10,
@@ -32,6 +40,52 @@ describe('Table col cell util test', () => {
           },
         },
       }),
-    ).toBe(72.5);
+    ).toBe(80);
+  });
+
+  test('should return right occupied width for table col with actionIcons', () => {
+    const mockSS = {
+      store: new Map(),
+      options: {
+        showDefaultHeaderActionIcon: false,
+        headerActionIcons: [
+          {
+            iconNames: ['foo', 'bar'],
+            belongsCell: 'colCell',
+          },
+        ],
+      },
+    };
+
+    const mockNode = {
+      field: 'city',
+    } as unknown as Node;
+
+    mockSS.store.set('hiddenColumnsDetail', [
+      {
+        displaySiblingNode: {
+          prev: {
+            field: 'city',
+          },
+        },
+      },
+    ]);
+    expect(
+      getOccupiedWidthForTableCol(mockSS as unknown as SpreadSheet, mockNode, {
+        cell: {
+          padding: {
+            left: 10,
+            right: 10,
+          },
+        },
+        icon: {
+          size: 15,
+          margin: {
+            left: 10,
+            right: 10,
+          },
+        },
+      }),
+    ).toBe(105);
   });
 });
