d607651641407b7b3bd37f322b4a6187caca60cc
yangzhanmei <15809244435@163.com>
2021-11-19 17:08:36 +0800

    refactor: export TooltipComponent in components

--- packages/s2-react/src/components/index.ts
+++ packages/s2-react/src/components/index.ts
@@ -3,6 +3,7 @@ export { TableSheet } from './sheets/table-sheet';
 export { GridAnalysisSheet } from './sheets/grid-analysis-sheet';
 export { AdvancedSort } from './advanced-sort';
 export { DrillDown } from './drill-down';
+export { TooltipComponent } from './tooltip';
 export { Switcher } from './switcher';
 export * from './sheets';
 export * from './sheets/interface';
