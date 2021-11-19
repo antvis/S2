4ac35f5ab03b6c646826d4b4a0ef21d55b088764
wjgogogo <906626481@qq.com>
2021-11-19 20:30:25 +0800

    chore: add env config

--- packages/s2-react/vite.config.ts
+++ packages/s2-react/vite.config.ts
@@ -6,90 +6,90 @@ import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-const OUT_DIR_NAME_MAP: { [key in LibraryFormats]?: string } = {
+const OUT_DIR_NAME_MAP = {
   es: 'esm',
   cjs: 'lib',
   umd: 'dist',
 };
 
+const format = process.env.FORMAT as LibraryFormats;
+const outDir = OUT_DIR_NAME_MAP[format];
+const isUmdFormat = format === 'umd';
+
+const isAnalysisMode = process.env.ANALYSIS;
+const isDevMode = process.env.PLAYGROUND;
+const root = path.join(__dirname, isDevMode ? 'playground' : '');
+
 // eslint-disable-next-line import/no-default-export
-export default defineConfig(({ mode, command }) => {
-  const format = mode as LibraryFormats;
-  const outDir = OUT_DIR_NAME_MAP[format];
+export default defineConfig({
+  // 开发配置
+  root,
+  server: {
+    port: 3000,
+    hmr: true,
+  },
 
-  const isUmdFormat = format === 'umd';
-  const root = path.join(__dirname, command === 'serve' ? 'playground' : '');
-
-  return {
-    // 开发配置
-    root,
-    server: {
-      port: 3000,
-      hmr: true,
+  // 打包配置
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      lodash: 'lodash-es',
     },
+  },
 
-    // 打包配置
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-        lodash: 'lodash-es',
-      },
-    },
-
-    define: {
-      'process.env.NODE_ENV': JSON.stringify(
-        command === 'serve' ? 'development' : 'production',
-      ),
-    },
-    plugins: [
-      peerDepsExternal(),
-      viteCommonjs(),
-      react(),
-      viteImp({
-        libList: [
-          {
-            libName: 'antd',
-            style: (name) => `antd/es/${name}/style/index.less`,
-          },
-        ],
-      }),
-      visualizer({ gzipSize: true }),
-    ],
-    css: {
-      preprocessorOptions: {
-        less: {
-          javascriptEnabled: true,
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(
+      isDevMode ? 'development' : 'production',
+    ),
+  },
+  plugins: [
+    peerDepsExternal(),
+    viteCommonjs(),
+    react(),
+    viteImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style/index.less`,
         },
-      },
-      modules: {
-        // 样式小驼峰转化
-        // css: goods-list => tsx: goodsList
-        localsConvention: 'camelCase',
+      ],
+    }),
+    isAnalysisMode && visualizer({ gzipSize: true }),
+  ].filter(Boolean),
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
       },
     },
+    modules: {
+      // 样式小驼峰转化
+      // css: goods-list => tsx: goodsList
+      localsConvention: 'camelCase',
+    },
+  },
 
-    build: {
-      minify: isUmdFormat ? 'esbuild' : false,
-      sourcemap: true,
+  build: {
+    minify: isUmdFormat ? 'esbuild' : false,
+    sourcemap: true,
 
-      lib: {
-        name: 'S2-React',
-        entry: './src/index.ts',
-        formats: [format],
-      },
-      outDir: outDir,
+    lib: {
+      name: 'S2-React',
+      entry: './src/index.ts',
+      formats: [format],
+    },
+    outDir: outDir,
 
-      rollupOptions: {
-        output: {
-          entryFileNames: `[name]${isUmdFormat ? '.min' : ''}.js`,
-          assetFileNames: `[name]${isUmdFormat ? '.min' : ''}.[ext]`,
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-            '@antv/s2': 'S2',
-          },
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]${isUmdFormat ? '.min' : ''}.js`,
+        assetFileNames: `[name]${isUmdFormat ? '.min' : ''}.[ext]`,
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@antv/s2': 'S2',
         },
       },
     },
-  };
+  },
 });
