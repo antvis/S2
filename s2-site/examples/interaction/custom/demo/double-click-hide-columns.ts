6259044e4982d56bc608d661c9704267331a8fb4
yingying.xwy <yingying.xwy@antgroup.com>
2021-11-20 22:45:28 +0800

    docs: update the screenshoot

--- s2-site/examples/interaction/custom/demo/double-click-hide-columns.ts
+++ s2-site/examples/interaction/custom/demo/double-click-hide-columns.ts
@@ -34,7 +34,7 @@ class ContextMenuInteraction extends BaseEvent {
 }
 
 fetch(
-  'https://gw.alipayobjects.com/os/bmw-prod/2a5dbbc8-d0a7-4d02-b7c9-34f6ca63cff6.json',
+  'https://gw.alipayobjects.com/os/bmw-prod/d62448ea-1f58-4498-8f76-b025dd53e570.json',
 )
   .then((res) => res.json())
   .then((data) => {
