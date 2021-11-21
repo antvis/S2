1f3c18f47c1dbe1e1805377be44883b26be999b5
Jinke Li <a1231236677287@163.com>
2021-11-21 07:20:14 +0000

    docs: update tooltip demo

--- s2-site/examples/react-component/tooltip/demo/custom-show-tooltip.tsx
+++ s2-site/examples/react-component/tooltip/demo/custom-show-tooltip.tsx
@@ -8,38 +8,7 @@ fetch(
   'https://gw.alipayobjects.com/os/bmw-prod/2a5dbbc8-d0a7-4d02-b7c9-34f6ca63cff6.json',
 )
   .then((res) => res.json())
-  .then((data) => {
-    const s2DataConfig = {
-      fields: {
-        rows: ['province', 'city'],
-        columns: ['type'],
-        values: ['price', 'cost'],
-      },
-      meta: [
-        {
-          field: 'province',
-          name: '省份',
-        },
-        {
-          field: 'city',
-          name: '城市',
-        },
-        {
-          field: 'type',
-          name: '商品类别',
-        },
-        {
-          field: 'price',
-          name: '价格',
-        },
-        {
-          field: 'cost',
-          name: '成本',
-        },
-      ],
-      data,
-    };
-
+  .then((dataCfg) => {
     const s2Options = {
       width: 600,
       height: 480,
@@ -47,7 +16,7 @@ fetch(
 
     ReactDOM.render(
       <SheetComponent
-        dataCfg={s2DataConfig}
+        dataCfg={dataCfg}
         options={s2Options}
         getSpreadSheet={(instance) => {
           instance.showTooltip = (tooltipOptions) => {
