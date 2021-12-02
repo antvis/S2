2b474db70675dfd7ca3d1b9d2ccd6bfa7ec98814
Jinke Li <a1231236677287@163.com>
2021-12-02 16:15:05 +0800

    refactor(components): remove condiation spec

--- packages/s2-react/src/utils/options.ts
+++ packages/s2-react/src/utils/options.ts
@@ -1,8 +1,7 @@
 import React from 'react';
-import { merge } from 'lodash';
-import { DEFAULT_OPTIONS, S2Options } from '@antv/s2';
+import { customMerge, DEFAULT_OPTIONS, S2Options } from '@antv/s2';
 import { SHEET_COMPONENT_DEFAULT_OPTIONS } from '@/common/constant';
 
 export const getSheetComponentOptions = (
   ...options: Partial<S2Options<React.ReactNode>>[]
-) => merge({}, DEFAULT_OPTIONS, SHEET_COMPONENT_DEFAULT_OPTIONS, ...options);
+) => customMerge(DEFAULT_OPTIONS, SHEET_COMPONENT_DEFAULT_OPTIONS, ...options);
