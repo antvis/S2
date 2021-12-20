d6ebc3dc2773977b8a5a5aa4203d513281b6090c
Jinke Li <a1231236677287@163.com>
2021-12-20 15:48:44 +0800

    fix(scroll): 修复滚动表格后, 鼠标移动会重置已选中单元格 close #904 (#914)
    * fix(scroll): 修复滚动表格后, 鼠标移动会重置已选中单元格 close #904
    
    * fix: test
    
    * fix: 修改注释

--- packages/s2-core/src/facet/base-facet.ts
+++ packages/s2-core/src/facet/base-facet.ts
@@ -1151,7 +1151,11 @@ export abstract class BaseFacet {
   }
 
   protected onAfterScroll = debounce(() => {
-    this.spreadsheet.interaction.removeIntercepts([InterceptType.HOVER]);
+    const { interaction } = this.spreadsheet;
+    // 如果是选中单元格状态, 则继续保留 hover 拦截, 避免滚动后 hover 清空已选单元格
+    if (!interaction.isSelectedState()) {
+      this.spreadsheet.interaction.removeIntercepts([InterceptType.HOVER]);
+    }
   }, 300);
 
   protected abstract doLayout(): LayoutResult;
