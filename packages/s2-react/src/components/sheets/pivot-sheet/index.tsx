dfdce2acc63e5e639f798b94c9ef864365412ec6
刘嘉一 <lcx.seima@gmail.com>
2022-01-18 17:09:49 +0800

    fix(drilldown): 变更drillConfig.dataSet未重置下钻结果

--- packages/s2-react/src/components/sheets/pivot-sheet/index.tsx
+++ packages/s2-react/src/components/sheets/pivot-sheet/index.tsx
@@ -12,114 +12,112 @@ import { handleDrillDown, handleDrillDownIcon } from '@/utils';
 export const PivotSheet: React.FC<SheetComponentsProps> = React.memo(
   (props) => {
     const { dataCfg, partDrillDown } = props;
-    const latestPropsRef = useLatest(props);
+    // 记录 headerIcons 中的 下钻 icon
     const drillDownIconRef = React.useRef<HeaderActionIcon>();
+
+    // 一些 props 的 latest ref
+    const latestPropsRef = useLatest(props);
+    const drillConfigRef = useLatest(partDrillDown?.drillConfig);
+    const rowFieldsRef = useLatest(dataCfg.fields.rows);
+    const fetchDataRef = useLatest(partDrillDown?.fetchData);
+    const drillItemsNumRef = useLatest(partDrillDown?.drillItemsNum);
 
     const s2Ref = React.useRef<SpreadSheet>();
 
     const [drillFields, setDrillFields] = React.useState<string[]>([]);
 
-    const onDrillDownIconClick = React.useCallback(
-      (
-        sheetInstance: SpreadSheet,
-        cacheDrillFields?: string[],
-        disabledFields?: string[],
-        event?: GEvent,
-      ) => {
-        const content = (
-          <DrillDown
-            {...partDrillDown?.drillConfig}
-            setDrillFields={setDrillFields}
-            drillFields={cacheDrillFields}
-            disabledFields={disabledFields}
-          />
-        );
+    const onDrillDownIconClick = (
+      sheetInstance: SpreadSheet,
+      cacheDrillFields?: string[],
+      disabledFields?: string[],
+      event?: GEvent,
+    ) => {
+      const content = (
+        <DrillDown
+          {...drillConfigRef.current}
+          setDrillFields={setDrillFields}
+          drillFields={cacheDrillFields}
+          disabledFields={disabledFields}
+        />
+      );
 
-        if (event) {
-          const { showTooltip } = getTooltipOptions(sheetInstance, event);
-          if (!showTooltip) {
-            return;
-          }
-          sheetInstance.showTooltip<React.ReactNode>({
-            position: {
-              x: event.clientX,
-              y: event.clientY,
-            },
-            content,
-          });
+      if (event) {
+        const { showTooltip } = getTooltipOptions(sheetInstance, event);
+        if (!showTooltip) {
+          return;
         }
-      },
-      [partDrillDown?.drillConfig],
-    );
+        sheetInstance.showTooltip<React.ReactNode>({
+          position: {
+            x: event.clientX,
+            y: event.clientY,
+          },
+          content,
+        });
+      }
+    };
 
-    const updateDrillDownOptions = React.useCallback(
-      (sheetProps: SheetComponentsProps = latestPropsRef.current) => {
-        const drillDownOptions = handleDrillDownIcon(
-          sheetProps,
-          s2Ref.current,
-          onDrillDownIconClick,
-          drillDownIconRef,
-        );
-        s2Ref.current.setOptions(drillDownOptions);
-      },
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [onDrillDownIconClick, partDrillDown, latestPropsRef, s2Ref],
-    );
+    const updateDrillDownOptions = (
+      sheetProps: SheetComponentsProps = latestPropsRef.current,
+    ) => {
+      const drillDownOptions = handleDrillDownIcon(
+        sheetProps,
+        s2Ref.current,
+        onDrillDownIconClick,
+        drillDownIconRef,
+      );
+      s2Ref.current.setOptions(drillDownOptions);
+    };
 
     /**
      * 清空下钻信息
      * @param rowId 不传表示全部清空
      */
-    const clearDrillDownInfo = React.useCallback(
-      (rowId?: string) => {
-        s2Ref.current?.clearDrillDownData(rowId);
-      },
-      [s2Ref],
-    );
+    const clearDrillDownInfo = (rowId?: string) => {
+      s2Ref.current?.clearDrillDownData(rowId);
+    };
 
+    /**
+     * 加载或清除下钻数据
+     * 仅由 drillFields 驱动
+     */
     React.useEffect(() => {
       s2Ref.current?.hideTooltip();
-      if (isEmpty(drillFields) || !partDrillDown?.fetchData) {
+      if (isEmpty(drillFields)) {
         clearDrillDownInfo(s2Ref.current.store.get('drillDownNode')?.id);
       } else {
         // TODO 下钻整体流程梳理
         // setLoading(true);
         handleDrillDown({
-          rows: dataCfg.fields.rows,
+          rows: rowFieldsRef.current,
           drillFields: drillFields,
-          fetchData: partDrillDown?.fetchData,
-          drillItemsNum: partDrillDown?.drillItemsNum,
+          fetchData: fetchDataRef.current,
+          drillItemsNum: drillItemsNumRef.current,
           spreadsheet: s2Ref.current,
         });
       }
       updateDrillDownOptions();
-    }, [
-      clearDrillDownInfo,
-      dataCfg.fields.rows,
-      drillFields,
-      partDrillDown,
-      s2Ref,
-      updateDrillDownOptions,
-    ]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [drillFields]);
 
     React.useEffect(() => {
       if (isEmpty(partDrillDown?.clearDrillDown)) {
         return;
       }
       clearDrillDownInfo(partDrillDown?.clearDrillDown?.rowId);
-    }, [clearDrillDownInfo, partDrillDown?.clearDrillDown]);
+    }, [partDrillDown?.clearDrillDown]);
 
     React.useEffect(() => {
       if (!partDrillDown?.drillItemsNum) {
         return;
       }
       clearDrillDownInfo();
-    }, [clearDrillDownInfo, partDrillDown?.drillItemsNum]);
+    }, [partDrillDown?.drillItemsNum]);
 
     React.useEffect(() => {
       updateDrillDownOptions();
       s2Ref.current.render();
-    }, [partDrillDown, s2Ref, updateDrillDownOptions]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [partDrillDown]);
 
     return <BaseSheet {...props} ref={s2Ref} />;
   },
