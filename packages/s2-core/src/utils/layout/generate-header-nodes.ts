4b4aaee6a33a4e89d03ed88a23d6bb1da27e0f12
嘤嘤嘤 <yingying.xwy@antgroup.com>
2022-02-09 14:23:49 +0800

    perf: lodash 使用优化 (#1064)

--- packages/s2-core/src/utils/layout/generate-header-nodes.ts
+++ packages/s2-core/src/utils/layout/generate-header-nodes.ts
@@ -1,4 +1,4 @@
-import { includes, isBoolean, merge } from 'lodash';
+import { includes, isBoolean, memoize, merge } from 'lodash';
 import { HeaderNodesParams } from '@/facet/layout/interface';
 import { TotalClass } from '@/facet/layout/total-class';
 import { TotalMeasure } from '@/facet/layout/total-measure';
@@ -9,7 +9,7 @@ import { Node } from '@/facet/layout/node';
 import { layoutHierarchy } from '@/facet/layout/layout-hooks';
 import { buildGridHierarchy } from '@/facet/layout/build-gird-hierarchy';
 
-export const generateHeaderNodes = (params: HeaderNodesParams) => {
+export const generateHeaderNodes = memoize((params: HeaderNodesParams) => {
   const {
     currentField,
     fields,
@@ -136,4 +136,4 @@ export const generateHeaderNodes = (params: HeaderNodesParams) => {
       });
     }
   }
-};
+});
