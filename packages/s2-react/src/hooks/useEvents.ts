617179ff6145bc6982baa4fa30b1997b0fb2a97d
stone <stone-lyl@outlook.com>
2022-02-13 22:55:54 +0800

    fix: s2-react 事件在 react hooks 使用中事件改变后未及时更新

--- packages/s2-react/src/hooks/useEvents.ts
+++ packages/s2-react/src/hooks/useEvents.ts
@@ -1,122 +1,98 @@
 import {
-  SpreadSheet,
-  S2Event,
-  getBaseCellData,
-  CellScrollPosition,
-  SortParams,
   EmitterType,
-  ViewMeta,
-  LayoutResult,
+  getBaseCellData,
+  S2Event,
+  SpreadSheet,
+  TargetCellInfo,
 } from '@antv/s2';
-import React from 'react';
+import { useEffect } from 'react';
 import { Event as GEvent } from '@antv/g-canvas';
-import { forIn } from 'lodash';
-import { BaseSheetComponentProps } from '../components/sheets/interface';
+import { BaseSheetComponentProps } from '@/components';
 
-export function useEvents(props: BaseSheetComponentProps) {
-  const registerEvent = React.useCallback(
-    (s2: SpreadSheet) => {
-      const EVENT_LISTENER_CONFIG: Record<
-        string,
-        (...args: unknown[]) => void
-      > = {
-        // ============== merged cell ====================
-        [S2Event.MERGED_CELLS_CLICK]: (event: GEvent) => {
-          props.onMergedCellClick?.(getBaseCellData(event));
-        },
-        [S2Event.MERGED_CELLS_HOVER]: (event: GEvent) => {
-          props.onMergedCellHover?.(getBaseCellData(event));
-        },
-        [S2Event.MERGED_CELLS_DOUBLE_CLICK]: (event: GEvent) => {
-          props.onMergedCellsDoubleClick?.(getBaseCellData(event));
-        },
+const useCellEvent = (
+  eventName: keyof EmitterType,
+  handler: (data: TargetCellInfo) => void,
+  s2: SpreadSheet,
+) => {
+  useEffect(() => {
+    const handlerFn = (event: GEvent) => {
+      handler?.(getBaseCellData(event));
+    };
+    s2?.on(eventName, handlerFn);
+    return () => {
+      s2?.off(eventName, handlerFn);
+    };
+  }, [s2, handler]);
+};
 
-        // ============== row cell ====================
-        [S2Event.ROW_CELL_CLICK]: (event: GEvent) => {
-          props.onRowCellClick?.(getBaseCellData(event));
-        },
-        [S2Event.ROW_CELL_HOVER]: (event: GEvent) => {
-          props.onRowCellHover?.(getBaseCellData(event));
-        },
-        [S2Event.ROW_CELL_DOUBLE_CLICK]: (event: GEvent) => {
-          props.onRowCellDoubleClick?.(getBaseCellData(event));
-        },
+const useS2Event = (
+  eventName: keyof EmitterType,
+  handler: (args: any) => void,
+  s2: SpreadSheet,
+) => {
+  useEffect(() => {
+    const handlerFn = (args: any) => {
+      handler?.(args);
+    };
+    s2?.on(eventName, handlerFn);
+    return () => {
+      s2?.off(eventName, handlerFn);
+    };
+  }, [s2, handler]);
+};
 
-        // ============== col cell ====================
-        [S2Event.COL_CELL_CLICK]: (event: GEvent) => {
-          props.onColCellClick?.(getBaseCellData(event));
-        },
-        [S2Event.COL_CELL_HOVER]: (event: GEvent) => {
-          props.onColCellHover?.(getBaseCellData(event));
-        },
-        [S2Event.COL_CELL_DOUBLE_CLICK]: (event: GEvent) => {
-          props.onColCellDoubleClick?.(getBaseCellData(event));
-        },
+export function useEvents(props: BaseSheetComponentProps, s2: SpreadSheet) {
+  // ============== merged cell ====================
+  useCellEvent(S2Event.MERGED_CELLS_CLICK, props.onMergedCellClick, s2);
+  useCellEvent(
+    S2Event.MERGED_CELLS_DOUBLE_CLICK,
+    props.onMergedCellsDoubleClick,
+    s2,
+  );
+  useCellEvent(S2Event.MERGED_CELLS_HOVER, props.onMergedCellHover, s2);
 
-        // ============== data cell ====================
-        [S2Event.DATA_CELL_DOUBLE_CLICK]: (event: GEvent) => {
-          props.onDataCellDoubleClick?.(getBaseCellData(event));
-        },
-        [S2Event.DATA_CELL_CLICK]: (event: GEvent) => {
-          props.onDataCellClick?.(getBaseCellData(event));
-        },
-        [S2Event.DATA_CELL_HOVER]: (event: GEvent) => {
-          props.onDataCellHover?.(getBaseCellData(event));
-        },
-        [S2Event.DATA_CELL_MOUSE_UP]: (event: GEvent) => {
-          props.onDataCellMouseUp?.(getBaseCellData(event));
-        },
-        [S2Event.DATA_CELL_TREND_ICON_CLICK]: (meta: ViewMeta) => {
-          props.onDataCellTrendIconClick?.(meta);
-        },
+  // ============== row cell ====================
+  useCellEvent(S2Event.ROW_CELL_CLICK, props.onRowCellClick, s2);
+  useCellEvent(S2Event.ROW_CELL_DOUBLE_CLICK, props.onRowCellDoubleClick, s2);
+  useCellEvent(S2Event.ROW_CELL_HOVER, props.onRowCellHover, s2);
 
-        // ============== corner cell ====================
-        [S2Event.CORNER_CELL_DOUBLE_CLICK]: (event: GEvent) => {
-          props.onCornerCellDoubleClick?.(getBaseCellData(event));
-        },
-        [S2Event.CORNER_CELL_HOVER]: (event: GEvent) => {
-          props.onCornerCellHover?.(getBaseCellData(event));
-        },
-        [S2Event.CORNER_CELL_CLICK]: (event: GEvent) => {
-          props.onCornerCellClick?.(getBaseCellData(event));
-        },
+  // ============== col cell ====================
+  useCellEvent(S2Event.COL_CELL_CLICK, props.onColCellClick, s2);
+  useCellEvent(S2Event.COL_CELL_DOUBLE_CLICK, props.onColCellDoubleClick, s2);
+  useCellEvent(S2Event.COL_CELL_HOVER, props.onColCellHover, s2);
 
-        // ============== layout ====================
-        [S2Event.LAYOUT_AFTER_HEADER_LAYOUT]: (layoutResult: LayoutResult) => {
-          props.onAfterHeaderLayout?.(layoutResult);
-        },
-        [S2Event.LAYOUT_COLLAPSE_ROWS]: (
-          collapsedRows: Record<string, boolean>,
-        ) => {
-          props.onCollapseRows?.(collapsedRows);
-        },
-        [S2Event.LAYOUT_TREE_ROWS_COLLAPSE_ALL]: (
-          hierarchyCollapse: boolean,
-        ) => {
-          props.onCollapseRowsAll?.(hierarchyCollapse);
-        },
-        [S2Event.LAYOUT_CELL_SCROLL]: (value: CellScrollPosition) => {
-          props.onCellScroll?.(value);
-        },
-        [S2Event.LAYOUT_AFTER_RENDER]: () => {
-          props.onLoad?.();
-        },
-        [S2Event.LAYOUT_DESTROY]: () => {
-          props.onDestroy?.();
-        },
-
-        // ============== sort ====================
-        [S2Event.RANGE_SORT]: (value: SortParams) => {
-          props.onSortChange?.(value);
-        },
-      };
-
-      forIn(EVENT_LISTENER_CONFIG, (handler, event: keyof EmitterType) => {
-        s2?.on(event, handler);
-      });
-    },
-    [props],
+  // ============== data cell ====================
+  useCellEvent(S2Event.DATA_CELL_CLICK, props.onDataCellClick, s2);
+  useCellEvent(S2Event.DATA_CELL_DOUBLE_CLICK, props.onDataCellDoubleClick, s2);
+  useCellEvent(S2Event.DATA_CELL_HOVER, props.onDataCellHover, s2);
+  useCellEvent(S2Event.DATA_CELL_MOUSE_UP, props.onDataCellMouseUp, s2);
+  useS2Event(
+    S2Event.DATA_CELL_TREND_ICON_CLICK,
+    props.onDataCellTrendIconClick,
+    s2,
   );
 
-  return { registerEvent };
+  // ============== corner cell ====================
+  useCellEvent(S2Event.CORNER_CELL_CLICK, props.onCornerCellClick, s2);
+  useCellEvent(
+    S2Event.CORNER_CELL_DOUBLE_CLICK,
+    props.onCornerCellDoubleClick,
+    s2,
+  );
+  useCellEvent(S2Event.CORNER_CELL_HOVER, props.onCornerCellHover, s2);
+
+  // ============== layout ====================
+  useS2Event(S2Event.LAYOUT_AFTER_HEADER_LAYOUT, props.onAfterHeaderLayout, s2);
+  useS2Event(S2Event.LAYOUT_COLLAPSE_ROWS, props.onCollapseRows, s2);
+  useS2Event(
+    S2Event.LAYOUT_TREE_ROWS_COLLAPSE_ALL,
+    props.onCollapseRowsAll,
+    s2,
+  );
+  useS2Event(S2Event.LAYOUT_CELL_SCROLL, props.onCellScroll, s2);
+  useS2Event(S2Event.LAYOUT_AFTER_RENDER, props.onLoad, s2);
+  useS2Event(S2Event.LAYOUT_DESTROY, props.onDestroy, s2);
+
+  // ============== sort ====================
+  useS2Event(S2Event.RANGE_SORT, props.onSortChange, s2);
 }
