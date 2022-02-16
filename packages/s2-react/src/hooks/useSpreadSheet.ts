e4a757e39560edd52048cf6a49824eeb0a0f1dbc
刘嘉一 <lcx.seima@gmail.com>
2022-02-16 17:59:14 +0800

    refactor: 精简 usePagination hooks deps (#1095)
    * refactor: 整理usePagination hooks deps
    
    * refactor: 仅使用ref保存sp实例

--- packages/s2-react/src/hooks/useSpreadSheet.ts
+++ packages/s2-react/src/hooks/useSpreadSheet.ts
@@ -5,6 +5,7 @@ import {
   SpreadSheet,
   TableSheet,
 } from '@antv/s2';
+import { useUpdate } from 'ahooks';
 import React from 'react';
 import type { BaseSheetComponentProps, SheetType } from '../components';
 import { getSheetComponentOptions } from '../utils';
@@ -23,6 +24,7 @@ export function useSpreadSheet(
   props: BaseSheetComponentProps,
   config: UseSpreadSheetConfig,
 ) {
+  const forceUpdate = useUpdate();
   const s2Ref = React.useRef<SpreadSheet>();
   const containerRef = React.useRef<HTMLDivElement>();
 
@@ -56,8 +58,13 @@ export function useSpreadSheet(
     s2Ref.current.setThemeCfg(props.themeCfg);
     s2Ref.current.render();
     setLoading(false);
+
+    // 子 hooks 内使用了 s2Ref.current 作为 dep
+    // forceUpdate 一下保证子 hooks 能 rerender
+    forceUpdate();
+
     props.getSpreadSheet?.(s2Ref.current);
-  }, [props, renderSpreadSheet, setLoading]);
+  }, [props, renderSpreadSheet, setLoading, forceUpdate]);
 
   // init
   React.useEffect(() => {
