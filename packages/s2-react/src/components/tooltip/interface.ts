259955d29ee1ac1395761add9520a78dbe5e6c6f
Jinke Li <a1231236677287@163.com>
2022-02-18 17:59:50 +0800

    feat(tooltip): tooltip 自定义操作项点击事件透出 cell 信息 close #1106 (#1107)
    * feat(tooltip): tooltip 自定义操作项点击事件透出 cell 信息 close #1106
    
    * docs: 完善文档

--- packages/s2-react/src/components/tooltip/interface.ts
+++ packages/s2-react/src/components/tooltip/interface.ts
@@ -1,8 +1,9 @@
-import { TooltipShowOptions } from '@antv/s2';
+import type { S2CellType, TooltipShowOptions } from '@antv/s2';
 
 export interface TooltipRenderProps<T = React.ReactNode>
   extends TooltipShowOptions<T> {
   readonly content?: T;
+  readonly cell: S2CellType;
 }
 
 export type TooltipInfosProps = {
