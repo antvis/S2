b51cddb46147959144fc957d08ceb278337fbc4e
wjgogogo <906626481@qq.com>
2022-03-22 16:49:13 +0800

    chore: remove cypress

--- packages/s2-vue/.eslintrc.cjs
+++ packages/s2-vue/.eslintrc.cjs
@@ -1,25 +1,15 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
+  env: {
+    'vue/setup-compiler-macros': true,
   },
-  "overrides": [
-    {
-      "files": [
-        "cypress/integration/**.spec.{js,ts,jsx,tsx}"
-      ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
-}
+};
