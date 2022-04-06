fba9a499bc21c8229c10ceff311e96dbbc999e4c
刘嘉一 <lcx.seima@gmail.com>
2022-04-06 17:51:37 +0800

    test: 增加测试用例

--- packages/s2-core/__tests__/unit/utils/color-spec.ts
+++ packages/s2-core/__tests__/unit/utils/color-spec.ts
@@ -1,25 +1,77 @@
+import { clone } from 'lodash';
 import { getPalette } from '@/utils';
 import { generatePalette } from '@/utils/color';
+import { Palette } from '@/common';
+
+const expectThemeColor = '#F1535F';
+const expectThemeBgColor = '#F5848E';
+const expectThemePalette = [
+  '#FFFFFF',
+  '#FEF6F6',
+  '#FCE2E4',
+  '#F1535F',
+  '#CF444F',
+  '#CF444F',
+  '#FF0112',
+  '#ED505C',
+  '#FFFFFF',
+  '#FDE5E7',
+  '#F26C77',
+  '#F26C77',
+  '#F1535F',
+  '#000000',
+  '#000000',
+];
 
 describe('color test', () => {
   test('should generate palette', () => {
     const colorfulPalette = getPalette('colorful');
-    const palette = generatePalette(colorfulPalette, '#F1535F');
+    const palette = generatePalette(colorfulPalette, expectThemeColor);
 
-    expect(palette.basicColors[0]).toEqual('#FFFFFF');
-    expect(palette.basicColors[1]).toEqual('#FEF6F6');
-    expect(palette.basicColors[2]).toEqual('#FCE2E4');
-    expect(palette.basicColors[3]).toEqual('#F1535F');
-    expect(palette.basicColors[4]).toEqual('#CF444F');
-    expect(palette.basicColors[5]).toEqual('#CF444F');
-    expect(palette.basicColors[6]).toEqual('#FF0112');
-    expect(palette.basicColors[7]).toEqual('#ED505C');
-    expect(palette.basicColors[8]).toEqual('#FFFFFF');
-    expect(palette.basicColors[9]).toEqual('#FDE5E7');
-    expect(palette.basicColors[10]).toEqual('#F26C77');
-    expect(palette.basicColors[11]).toEqual('#F26C77');
-    expect(palette.basicColors[12]).toEqual('#F1535F');
-    expect(palette.basicColors[13]).toEqual('#000000');
-    expect(palette.basicColors[14]).toEqual('#000000');
+    expect(palette.basicColors).toEqual(expectThemePalette);
+  });
+
+  test('should skip fixed color', () => {
+    const colorfulPalette = clone(getPalette('colorful'));
+
+    colorfulPalette.fontColorBgIndexRelations = {};
+    colorfulPalette.fixedColorIndex = [];
+    for (let index = 0; index < colorfulPalette.basicColors.length; index++) {
+      colorfulPalette.fixedColorIndex.push(index);
+    }
+
+    const palette = generatePalette(colorfulPalette, expectThemeColor);
+    expect(palette.basicColors).toEqual(colorfulPalette.basicColors);
+  });
+
+  test('should skip equal hsl value', () => {
+    const testPalette: Palette = {
+      basicColors: [expectThemeColor, expectThemeColor],
+      brandColorIndex: 0,
+      fixedColorIndex: [0],
+      semanticColors: {},
+    };
+
+    const palette = generatePalette(testPalette, expectThemeColor);
+    expect(palette.basicColors).toEqual(testPalette.basicColors);
+  });
+
+  test('should adapt font color', () => {
+    const testPalette: Palette = {
+      // [brand color, bg color, text color]
+      basicColors: [expectThemeColor, expectThemeBgColor, '#FFFFFF'],
+      brandColorIndex: 0,
+      fixedColorIndex: [0],
+      fontColorBgIndexRelations: {
+        2: 1,
+      },
+      semanticColors: {},
+    };
+
+    const whiteTextPalette = generatePalette(testPalette, '#FFFFFF');
+    expect(whiteTextPalette.basicColors[2]).toEqual('#000000');
+
+    const blackTextPalette = generatePalette(testPalette, '#000000');
+    expect(blackTextPalette.basicColors[2]).toEqual('#FFFFFF');
   });
 });
