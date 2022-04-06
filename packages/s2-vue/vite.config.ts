b07c2ec33100a86e3254ed5fc1585005fda24831
wjgogogo <906626481@qq.com>
2022-04-06 14:32:09 +0800

    feat: complete ts config

--- packages/s2-vue/vite.config.ts
+++ packages/s2-vue/vite.config.ts
@@ -1,27 +1,25 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
-import { defineConfig, LibraryFormats } from 'vite';
+import { defineConfig, type LibraryFormats, type PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-const OUT_DIR_NAME_MAP = {
+const OUT_DIR_NAME_MAP: { [key in LibraryFormats]?: string } = {
   es: 'esm',
   cjs: 'lib',
   umd: 'dist',
 };
 
 const format = process.env.FORMAT as LibraryFormats;
-const outDir = OUT_DIR_NAME_MAP[format];
+const outDir = OUT_DIR_NAME_MAP[format] as string;
 const isUmdFormat = format === 'umd';
 
 const isAnalysisMode = process.env.ANALYSIS;
 const isDevMode = process.env.PLAYGROUND;
 const root = path.join(__dirname, isDevMode ? 'playground' : '');
 
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   // 开发配置
   root,
@@ -33,7 +31,6 @@ export default defineConfig({
   // 打包配置
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
       lodash: 'lodash-es',
     },
   },
@@ -49,19 +46,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     isAnalysisMode && visualizer({ gzipSize: true }),
-  ].filter(Boolean),
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
-    },
-    modules: {
-      // 样式小驼峰转化
-      // css: goods-list => tsx: goodsList
-      localsConvention: 'camelCase',
-    },
-  },
+  ].filter(Boolean) as PluginOption[],
 
   build: {
     minify: isUmdFormat ? 'esbuild' : false,
