4c81fa3d0ac2c9563f022560cae75335c453b218
嘤嘤嘤 <yingying.xwy@antgroup.com>
2022-04-18 21:29:05 +0800

    feat: 增加根据主题色生成对应主题风格色板功能 (#1190)
    * feat: 增加根据主题色生成对应主题风格色板功能
    
    * fix: 修复lint报错
    
    * feat: 固定背景色
    
    * refactor: 改用hsl颜色
    
    * chore: type
    
    * feat: hsl分量作用方式从
    绝对比例改为各自区间相对比例
    
    * test: 色板测试用例
    
    * chore: 函数注释
    
    * docs: 增加主题色生成demo
    
    * docs: 增加自助配色板
    
    * docs: 修改主题章节
    
    * refactor: return
    
    * test: 增加测试用例
    
    * fix: getIndexRangeWithOffsets可视范围计算错误
    
    * fix: 切换pageSize未重置滚动偏移量
    
    * feat: 色成色板时使用标准色板替换HSL方案
    
    * feat: 微调link颜色
    
    * feat: 统一basicColors case
    
    * fix: demo
    
    * fix: 已使用了自定义色板的用户升级后会遇到类型错误
    
    * chore: 注释
    
    * docs: 更新色板介绍文档
    
    Co-authored-by: 刘嘉一 <lcx.seima@gmail.com>

--- packages/s2-core/src/theme/palette/default.ts
+++ packages/s2-core/src/theme/palette/default.ts
@@ -1,26 +1,59 @@
-export const paletteDefault = {
-  // --------- basic colors -----------
-  basicColors: [
-    '#000000',
-    '#f7faff',
-    '#E1EAFE',
-    '#E1EAFE',
-    '#CCDBFD',
-    '#2C60D3',
-    '#0000EE',
-    '#326EF4',
-    '#FFFFFF',
-    '#EBF2FF',
-    '#D6E3FE',
-    '#3471F9',
-    '#3471F9',
-    '#282B33',
-    '#121826',
-  ],
+import { Palette, PaletteMeta } from '@/common';
+import { generatePalette } from '@/utils/color';
 
+const paletteDefaultMeta: PaletteMeta = {
+  brandColor: '#326EF4',
   // ---------- semantic colors ----------
   semanticColors: {
     red: '#FF4D4F',
     green: '#29A294',
   },
+  basicColorRelations: [
+    {
+      basicColorIndex: 1,
+      standardColorIndex: 0,
+    },
+    {
+      basicColorIndex: 2,
+      standardColorIndex: 1,
+    },
+    {
+      basicColorIndex: 3,
+      standardColorIndex: 1,
+    },
+    {
+      basicColorIndex: 4,
+      standardColorIndex: 2,
+    },
+    {
+      basicColorIndex: 5,
+      standardColorIndex: 7,
+    },
+    {
+      basicColorIndex: 6,
+      standardColorIndex: 5,
+    },
+    {
+      basicColorIndex: 7,
+      standardColorIndex: 5,
+    },
+    {
+      basicColorIndex: 9,
+      standardColorIndex: 1,
+    },
+    {
+      basicColorIndex: 10,
+      standardColorIndex: 2,
+    },
+    {
+      basicColorIndex: 11,
+      standardColorIndex: 5,
+    },
+    {
+      basicColorIndex: 12,
+      standardColorIndex: 5,
+    },
+  ],
 };
+
+export const paletteDefault: Palette = generatePalette(paletteDefaultMeta);
