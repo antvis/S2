4b85d9ed9810b0a73303f94af327a550f85b8cff
yingying.xwy <yingying.xwy@antgroup.com>
2022-04-22 11:33:17 +0800

    chore(demo): 增加官网分群下钻表demo注释

--- s2-site/examples/case/proportion/demo/group-drill-down.tsx
+++ s2-site/examples/case/proportion/demo/group-drill-down.tsx
@@ -29,14 +29,16 @@ fetch(
                 text: {
                   field: 'number',
                   mapping: (value, cellInfo) => {
+                    // 添加文本颜色映射逻辑
                     const { colIndex } = cellInfo;
                     if (colIndex <= 1) {
+                      // 主指标为黑色
                       return {
                         fill: '#000',
                       };
                     }
                     return {
-                      fill: isUpDataValue(value) ? '#FF4D4F' : '#29A294',
+                      fill: isUpDataValue(value) ? '#FF4D4F' : '#29A294', // 同环比红张绿跌
                     };
                   },
                 },
