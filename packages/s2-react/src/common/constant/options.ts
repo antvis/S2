199fe74aad7eb0fac09c049ec9e95ac42a0c17de
wjgogogo <906626481@qq.com>
2022-04-26 11:38:03 +0800

    refactor: 抽离getBaseSheetComponentOptions方法

--- packages/s2-react/src/common/constant/options.ts
+++ packages/s2-react/src/common/constant/options.ts
@@ -1,16 +1,8 @@
 import { S2Options, SpreadSheet } from '@antv/s2';
 import { CustomTooltip } from '@/components/tooltip/custom-tooltip';
 
-export const SHEET_COMPONENT_DEFAULT_OPTIONS: Readonly<Partial<S2Options>> = {
+export const RENDER_TOOLTIP_OPTION: Partial<S2Options> = {
   tooltip: {
-    showTooltip: true,
-    autoAdjustBoundary: 'body',
-    operation: {
-      hiddenColumns: true,
-      trend: false,
-      sort: true,
-    },
     renderTooltip: (spreadsheet: SpreadSheet) => new CustomTooltip(spreadsheet),
   },
-  showDefaultHeaderActionIcon: true,
-} as const;
+};
