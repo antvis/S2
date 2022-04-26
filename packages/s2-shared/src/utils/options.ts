573d92df9d2202968081f82b758388c887bcd8d5
wjgogogo <906626481@qq.com>
2022-04-26 11:46:15 +0800

    feat: 抽离公共类型到shared中

---
+++ packages/s2-shared/src/utils/options.ts
@@ -0,0 +1,7 @@
+import { customMerge, DEFAULT_OPTIONS, S2Options } from '@antv/s2';
+import { SHEET_COMPONENT_DEFAULT_OPTIONS } from '../constants';
+
+export const getBaseSheetComponentOptions = (
+  ...options: Partial<S2Options<any>>[]
+): S2Options =>
+  customMerge(DEFAULT_OPTIONS, SHEET_COMPONENT_DEFAULT_OPTIONS, ...options);
