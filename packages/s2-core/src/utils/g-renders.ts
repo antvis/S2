10f43916e884bdebdc35c05734091dff9d8b2ca0
Xinhui <xinhui.zxh@antgroup.com>
2022-04-28 19:13:48 +0800

    fix(interaction): 设置 stateShape 的 visible 默认为 false 来减少绘制调用 (#1295)
    * fix: 设置 stateShape visible 默认为 false 减少绘制调用
    
    * fix: 还原 hideInteractionShape
    
    * fix: 优化 updateByState
    
    * fix: renderRect 类型修复

--- packages/s2-core/src/utils/g-renders.ts
+++ packages/s2-core/src/utils/g-renders.ts
@@ -2,15 +2,26 @@
  * Utils to render all g supported shape
  * https://github.com/antvis/g
  */
-import { Group, IShape, ShapeAttrs, SimpleBBox } from '@antv/g-canvas';
+import {
+  Group,
+  IShape,
+  ShapeAttrs,
+  ShapeCfg,
+  SimpleBBox,
+} from '@antv/g-canvas';
 import { forEach, isEmpty, set, isFunction } from 'lodash';
 import { GuiIcon, GuiIconCfg } from '@/common/icons/gui-icon';
 import { TextTheme } from '@/common/interface/theme';
 
-export function renderRect(group: Group, attrs: ShapeAttrs): IShape {
+export function renderRect(
+  group: Group,
+  attrs: ShapeAttrs,
+  extraParams?: Omit<ShapeCfg, 'attrs'>,
+): IShape {
   return group?.addShape?.('rect', {
     zIndex: 1,
     attrs,
+    ...(extraParams || {}),
   });
 }
 
