a042d514b60315ec78f272a52c7c617e5776689e
wjgogogo <906626481@qq.com>
2022-04-29 11:18:13 +0800

    feat: 构建实例时使用原始数据而非响应式数据

--- packages/s2-vue/src/hooks/useSpreadSheet.ts
+++ packages/s2-vue/src/hooks/useSpreadSheet.ts
@@ -2,11 +2,10 @@ import {
   PivotSheet,
   TableSheet,
   type S2Constructor,
-  type S2Options,
   type SpreadSheet,
 } from '@antv/s2';
 import { getBaseSheetComponentOptions } from '@antv/s2-shared';
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { onBeforeUnmount, onMounted, ref, shallowRef, toRaw } from 'vue';
 import type { BaseSheetInitEmits, EmitFn } from '../interface';
 import type { BaseSheetProps } from '../utils/initPropAndEmits';
 import { useEvents } from './useEvents';
@@ -27,27 +26,31 @@ export function useSpreadSheet(
   } = props;
   const wrapRef = ref<HTMLDivElement>();
   const containerRef = ref<HTMLDivElement>();
-  const s2Ref = ref<SpreadSheet>();
+
+  const s2Ref = shallowRef<SpreadSheet>();
 
   const { loading, setLoading } = useLoading(s2Ref.value!, loadingProps);
 
   // TODO: 如果onSpreadsheet属性变更了怎么办？？？
   const renderSpreadSheet = (container: HTMLDivElement) => {
-    const s2Options = getBaseSheetComponentOptions(options as S2Options);
-    const s2Constructor: S2Constructor = [container, dataCfg!, s2Options];
+    const rawDataCfg = toRaw(dataCfg!);
+    const rawOptions = toRaw(options);
+
+    const s2Options = getBaseSheetComponentOptions(rawOptions);
+    const s2Constructor: S2Constructor = [container, rawDataCfg, s2Options];
     if (onSpreadsheet) {
       return onSpreadsheet(...s2Constructor);
     }
     if (sheetType === 'table') {
-      return new TableSheet(container, dataCfg!, s2Options);
+      return new TableSheet(container, rawDataCfg, s2Options);
     }
-    return new PivotSheet(container, dataCfg!, s2Options);
+    return new PivotSheet(container, rawDataCfg, s2Options);
   };
 
   const buildSpreadSheet = () => {
     setLoading(true);
     s2Ref.value = renderSpreadSheet(containerRef.value!);
-    s2Ref.value.setThemeCfg(themeCfg);
+    s2Ref.value.setThemeCfg(toRaw(themeCfg));
     s2Ref.value.render();
     setLoading(false);
     onGetSpreadSheet?.(s2Ref.value);
