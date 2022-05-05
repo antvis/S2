3378f7d37a931b76ab2a38a40cdffe05d7dc4a7d
wjgogogo <906626481@qq.com>
2022-05-05 13:10:49 +0800

    refactor: 抽离工具函数traverse

--- packages/s2-vue/src/hooks/useSheetUpdate.ts
+++ packages/s2-vue/src/hooks/useSheetUpdate.ts
@@ -1,41 +1,7 @@
 import type { SpreadSheet } from '@antv/s2';
-import { isArray, isMap, isObject, isPlainObject, isSet } from 'lodash';
-import { isProxy, isRef, reactive, watch, type ShallowRef } from 'vue';
+import { reactive, watch, type ShallowRef } from 'vue';
 import type { BaseSheetProps } from '../utils/initPropAndEmits';
-
-// 属性遍历工具函数
-const traverse = <T>(value: T, seen?: Set<T>) => {
-  // eslint-disable-next-line dot-notation
-  if (!isObject(value) || (value as any)['__v_skip']) {
-    return value;
-  }
-
-  if (!isProxy(value) && !isRef(value)) {
-    return value;
-  }
-
-  seen = seen || new Set();
-  if (seen.has(value)) {
-    return value;
-  }
-  seen.add(value);
-  if (isRef(value)) {
-    traverse(value.value, seen);
-  } else if (isArray(value)) {
-    for (let i = 0; i < value.length; i++) {
-      traverse(value[i], seen);
-    }
-  } else if (isSet(value) || isMap(value)) {
-    value.forEach((v: any) => {
-      traverse(v, seen);
-    });
-  } else if (isPlainObject(value)) {
-    for (const key in value as any) {
-      traverse((value as any)[key], seen);
-    }
-  }
-  return value;
-};
+import { traverse } from '../utils/traverse';
 
 // props会将所有属性用shallowReactive包裹起来：
 // 1. 如果是对dataCfg或者options直接替换， 那么只需要简单的通过 ()=>props.dataCfg 这种getter就能监听到
