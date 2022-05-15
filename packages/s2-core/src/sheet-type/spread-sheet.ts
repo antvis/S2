4c77cfdb2f6dbafcead419eb3033db80a6e400a0
刘嘉一 <lcx.seima@gmail.com>
2022-05-15 23:54:58 +0800

    fix: total 配置项取用错误

--- packages/s2-core/src/sheet-type/spread-sheet.ts
+++ packages/s2-core/src/sheet-type/spread-sheet.ts
@@ -516,7 +516,8 @@ export abstract class SpreadSheet extends EE {
    */
   public getTotalsConfig(dimension: string): Partial<Totals['row']> {
     const { totals } = this.options;
-    const { rows } = this.dataCfg.fields;
+    const { rows } = this.dataSet.fields;
+
     const totalConfig = get(
       totals,
       includes(rows, dimension) ? 'row' : 'col',
