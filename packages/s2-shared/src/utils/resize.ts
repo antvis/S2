2c13437e93d24e87109663bd1bd61dbb2894c7b0
xiaochong44 <277324181@qq.com>
2022-06-10 23:07:58 +0800

    fix: 修改resize缩进

--- packages/s2-shared/src/utils/resize.ts
+++ packages/s2-shared/src/utils/resize.ts
@@ -38,9 +38,8 @@ export const createResizeObserver = (params: ResizeEffectParams) => {
   const debounceRender = debounce(render, RESIZE_RENDER_DELAY);
 
   const onResize = () => {
-    //取dom的大小，解决scale问题
-    const { clientWidth: nodeWidth, clientHeight: nodeHeight } =
-      container;
+    // 取dom的大小，解决scale问题
+    const { clientWidth: nodeWidth, clientHeight: nodeHeight } = container;
 
     const width = adaptiveWidth
       ? Math.floor(nodeWidth ?? s2.options.width)
