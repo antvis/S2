b39742e3a7276836c504f2a0d5343ff201a65bba
Jinke Li <a1231236677287@163.com>
2022-06-22 10:11:03 +0800

    fix(strategysheet): 修复趋势分析表多列头切换为单列头后, 隐藏列头功能失效 (#1470)

--- packages/s2-react/__tests__/unit/hooks/useSpreadSheet-spec.ts
+++ packages/s2-react/__tests__/unit/hooks/useSpreadSheet-spec.ts
@@ -1,7 +1,8 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import { PivotSheet, type S2Options } from '@antv/s2';
+import { PivotSheet, type S2DataConfig, type S2Options } from '@antv/s2';
 import { getContainer } from 'tests/util/helpers';
 import * as mockDataConfig from 'tests/data/simple-data.json';
+import { cloneDeep } from 'lodash';
 import { useSpreadSheet } from '@/hooks';
 import type { SheetComponentsProps } from '@/components';
 
@@ -13,22 +14,29 @@ const s2Options: S2Options = {
 
 describe('useSpreadSheet tests', () => {
   const container = getContainer();
-  const props: SheetComponentsProps = {
-    spreadsheet: () => new PivotSheet(container, mockDataConfig, s2Options),
-    options: s2Options,
-    dataCfg: mockDataConfig,
+  const getConfig = (
+    fields: S2DataConfig['fields'] = mockDataConfig.fields,
+  ): SheetComponentsProps => {
+    return {
+      spreadsheet: () => new PivotSheet(container, mockDataConfig, s2Options),
+      options: s2Options,
+      dataCfg: {
+        fields,
+        data: mockDataConfig.data,
+      },
+    };
   };
 
   test('should build spreadSheet', () => {
     const { result } = renderHook(() =>
-      useSpreadSheet({ ...props, sheetType: 'pivot' }),
+      useSpreadSheet({ ...getConfig(), sheetType: 'pivot' }),
     );
     expect(result.current.s2Ref).toBeDefined();
   });
 
   test('should cannot change table size when width or height updated and disable adaptive', () => {
     const { result } = renderHook(() =>
-      useSpreadSheet({ ...props, sheetType: 'pivot', adaptive: false }),
+      useSpreadSheet({ ...getConfig(), sheetType: 'pivot', adaptive: false }),
     );
     const s2 = result.current.s2Ref.current;
 
@@ -44,5 +52,40 @@ describe('useSpreadSheet tests', () => {
 
     expect(canvas.style.width).toEqual(`200px`);
     expect(canvas.style.height).toEqual(`200px`);
+  });
+
+  test('should clear init column leaf nodes when column fields changed', () => {
+    const defaultFields: S2DataConfig['fields'] = {
+      rows: ['province'],
+      columns: ['type', 'city'],
+      values: ['price'],
+      valueInCols: true,
+    };
+
+    const { result, rerender } = renderHook(() =>
+      useSpreadSheet(
+        cloneDeep({
+          ...getConfig(defaultFields),
+          sheetType: 'strategy',
+        }),
+      ),
+    );
+    const s2 = result.current.s2Ref.current;
+
+    expect(s2.getInitColumnLeafNodes()).toHaveLength(2);
+
+    // 很奇怪, rerender 之后始终拿到的两次 dataCfg 是一样的, 暂时先注释了
+    // act(() => {
+    //   const fields: S2DataConfig['fields'] = {
+    //     rows: ['province', 'city'],
+    //     columns: ['type'],
+    //     values: ['price'],
+    //     valueInCols: false,
+    //   };
+
+    //   rerender(getConfig(fields));
+    // });
+
+    // expect(s2.store.get('initColumnLeafNodes')).toEqual([]);
   });
 });
