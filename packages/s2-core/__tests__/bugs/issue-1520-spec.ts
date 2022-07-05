fb06ce0fadb7cc8f2b42c5f78b85f22d91a2640a
Wenjun Xu <906626481@qq.com>
2022-07-05 18:50:32 +0800

    fix: 移除字段标记中多余的 fieldValue判断 (#1525)
    * fix: 移除字段标记中多余的 fieldValue判断
    
    * test: 添加单测

---
+++ packages/s2-core/__tests__/bugs/issue-1520-spec.ts
@@ -0,0 +1,31 @@
+/**
+ * @description spec for issue #1520
+ * https://github.com/antvis/S2/issues/1520
+ * 数据为0时，图标标记中的图标不显示
+ */
+import { getContainer } from '../util/helpers';
+import * as mockDataConfig from '../data/data-issue-1520.json';
+import { PivotSheet } from '@/sheet-type';
+import { GuiIcon } from '@/common';
+
+const s2Options = {
+  width: 800,
+  height: 600,
+  conditions: {
+    icon: [{ field: 'cost', mapping: () => ({ fill: 'red', icon: 'CellUp' }) }],
+  },
+};
+
+describe('Conditions Test', () => {
+  const s2 = new PivotSheet(getContainer(), mockDataConfig, s2Options);
+  s2.render();
+  test('should render icon for cell with field value 0', () => {
+    const dataNodes = s2.interaction.getPanelGroupAllDataCells();
+    dataNodes.forEach((node) => {
+      const drawIcon = node
+        .getChildren()
+        .some((child) => child instanceof GuiIcon);
+      expect(drawIcon).toBeTrue();
+    });
+  });
+});
