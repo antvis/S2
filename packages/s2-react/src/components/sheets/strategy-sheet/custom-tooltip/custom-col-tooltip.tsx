b44ffe3dd03bc23dfbf8ba8828dc5e1b90a50310
Jinke Li <a1231236677287@163.com>
2022-07-08 12:40:37 +0800

    feat(strategysheet): 趋势分析表 Tooltip 增加 label 属性用于自定义标题 (#1540)
    * feat(strategysheet): 趋势分析表 Tooltip 增加 label 属性用于自定义标题
    
    * feat: update
    
    * test: 增加测试

--- packages/s2-react/src/components/sheets/strategy-sheet/custom-tooltip/custom-col-tooltip.tsx
+++ packages/s2-react/src/components/sheets/strategy-sheet/custom-tooltip/custom-col-tooltip.tsx
@@ -1,11 +1,15 @@
 import cls from 'classnames';
 import React from 'react';
 import { getStrategySheetTooltipClsName as tooltipCls } from '@antv/s2-shared';
+import { isFunction } from 'lodash';
 import type { CustomTooltipProps } from './interface';
 
 import './index.less';
 
-export const ColTooltip: React.FC<CustomTooltipProps> = ({ cell }) => {
+export const StrategySheetColTooltip: React.FC<CustomTooltipProps> = ({
+  cell,
+  label,
+}) => {
   const meta = cell.getMeta();
 
   // 趋势分析表叶子节点显示是指标标题, tooltip 中没必要再显示了
@@ -14,10 +18,12 @@ export const ColTooltip: React.FC<CustomTooltipProps> = ({ cell }) => {
   }
 
   const cellName = meta.spreadsheet.dataSet.getFieldName(meta.field);
+  const customLabel = isFunction(label) ? label(cell, cellName) : label;
+  const name = customLabel ?? cellName;
 
   return (
     <div className={cls(tooltipCls(), tooltipCls('col'))}>
-      <span className={tooltipCls('name')}>{cellName}</span>
+      <span className={tooltipCls('name')}>{name}</span>
       <span className={tooltipCls('value')}>{meta.value}</span>
     </div>
   );
--- packages/s2-react/src/components/sheets/strategy-sheet/custom-tooltip/interface.ts
+++ packages/s2-react/src/components/sheets/strategy-sheet/custom-tooltip/interface.ts
@@ -3,4 +3,10 @@ import type { Node, S2CellType, TooltipShowOptions, ViewMeta } from '@antv/s2';
 export interface CustomTooltipProps {
   cell: S2CellType<Node | ViewMeta>;
   defaultTooltipShowOptions?: TooltipShowOptions<React.ReactNode>;
+  label?:
+    | React.ReactNode
+    | ((
+        cell: S2CellType<Node | ViewMeta>,
+        defaultLabel: React.ReactNode,
+      ) => React.ReactNode);
 }
