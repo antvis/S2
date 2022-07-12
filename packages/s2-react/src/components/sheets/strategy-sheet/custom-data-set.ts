457c378ae346eb19a3d7822fd887eafecced420c
Wenjun Xu <906626481@qq.com>
2022-07-12 12:43:58 +0800

    fix: 修复趋势分析表导出问题 (#1553)
    * fix: 修复趋势分析表导出问题
    
    * test: 添加单测
    
    * refactor: 重构获取maxDepth逻辑
    
    * test: 添加趋势表导出单测

--- packages/s2-react/src/components/sheets/strategy-sheet/custom-data-set.ts
+++ packages/s2-react/src/components/sheets/strategy-sheet/custom-data-set.ts
@@ -4,13 +4,15 @@ import {
   type S2DataConfig,
   EXTRA_FIELD,
   VALUE_FIELD,
+  type Meta,
+  i18n,
 } from '@antv/s2';
 
 export class StrategyDataSet extends CustomTreePivotDataSet {
   processDataCfg(dataCfg: S2DataConfig): S2DataConfig {
     dataCfg.fields.rows = [EXTRA_FIELD];
     dataCfg.fields.valueInCols = false;
-    const { data, ...restCfg } = dataCfg;
+    const { data, meta, ...restCfg } = dataCfg;
     const transformedData = [];
     forEach(data, (dataItem) => {
       let isPushed = false;
@@ -28,8 +30,19 @@ export class StrategyDataSet extends CustomTreePivotDataSet {
         transformedData.push(dataItem);
       }
     });
+
+    const extraFieldName =
+      this.spreadsheet?.options?.cornerExtraFieldText || i18n('数值');
+
+    const extraFieldMeta: Meta = {
+      field: EXTRA_FIELD,
+      name: extraFieldName,
+    };
+    const newMeta: Meta[] = [...meta, extraFieldMeta];
+
     return {
       data: uniq(transformedData),
+      meta: newMeta,
       ...restCfg,
     };
   }
