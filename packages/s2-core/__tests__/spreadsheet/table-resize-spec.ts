d8a01c9d3dec9fe826ef34b7334fdf495d45f1a9
wjgogogo <906626481@qq.com>
2022-08-08 15:12:37 +0800

    test: 添加单测

---
+++ packages/s2-core/__tests__/spreadsheet/table-resize-spec.ts
@@ -0,0 +1,37 @@
+import type { IGroup } from '@antv/g-canvas';
+import { getContainer } from '../util/helpers';
+import * as mockDataConfig from '../data/simple-table-data.json';
+import { TableSheet } from '@/sheet-type';
+import { KEY_GROUP_ROW_RESIZE_AREA } from '@/common/constant';
+
+describe('Table Sheet Resize Test', () => {
+  test('should draw resize area in series cell when show series', () => {
+    const s2 = new TableSheet(getContainer(), mockDataConfig, {
+      width: 800,
+      height: 600,
+      showSeriesNumber: true,
+    });
+    s2.render();
+
+    const resizeGroup = s2.foregroundGroup.findById(
+      KEY_GROUP_ROW_RESIZE_AREA,
+    ) as IGroup;
+
+    expect(resizeGroup.getChildren().length).toBeGreaterThan(0);
+  });
+
+  test('should draw resize area in data cell when hide series', () => {
+    const s2 = new TableSheet(getContainer(), mockDataConfig, {
+      width: 800,
+      height: 600,
+      showSeriesNumber: false,
+    });
+    s2.render();
+
+    const resizeGroup = s2.foregroundGroup.findById(
+      KEY_GROUP_ROW_RESIZE_AREA,
+    ) as IGroup;
+
+    expect(resizeGroup.getChildren().length).toBeGreaterThan(0);
+  });
+});
