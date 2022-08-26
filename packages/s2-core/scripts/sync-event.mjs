8a06deab51e01e62f9cbd651fa4e3e9e88c24561
zishang <lyl275911@antgroup.com>
2022-08-26 18:19:01 +0800

    chore: 修复评审问题

--- packages/s2-core/scripts/sync-event.mjs
+++ packages/s2-core/scripts/sync-event.mjs
@@ -2,51 +2,43 @@
  * 将在core 层 S2Event 已经定义的事件，同步到 vue 和 react 中
  * ！注意自己检查生成结果和格式化一下
  */
-
+import { readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
 import { default as inquirer } from 'inquirer';
-import { readFileSync, writeFileSync } from "fs";
-import{ default as _ } from "lodash";
-import { basename, resolve } from 'path';
-export const symbol = '// ============== Auto 自动生成的 ================';
+import { default as _ } from 'lodash';
+
+const symbol = '// ============== Auto 自动生成的 ================';
+
 const insertEventIntoFile = (filePath, template) => {
   const content = readFileSync(filePath, { encoding: 'utf-8' });
   const lastBracket = content.lastIndexOf(symbol) + symbol.length;
+  const newContent =
+    content.slice(0, lastBracket) +
+    `\n  ${template}` +
+    content.slice(lastBracket);
 
-  const newContent = content.slice(0, lastBracket) + `\n  ${template}` + content.slice(lastBracket);
-  console.log(newContent, 'newContent');
-  writeFileSync(filePath, newContent );
-}
-
-// _  to camelCase
-/**
- * @param {string} str
- * @return {string} value
- */
-const camelcaseName = (str) => {
-  return _.chain(str)
-    .split('_')
-    .map(word => {
-      const w = _.trim(word);
-      return w[0].toUpperCase() + w.slice(1).toLowerCase();
-    })
-    .join('')
-    .value();
-}
+  writeFileSync(filePath, newContent);
+};
 
 const getVueEventName = (eventName) => {
-  let vueEventName = camelcaseName(eventName);
-  return vueEventName[0].toLowerCase() + vueEventName.slice(1);
-}
+  return _.camelCase(eventName);
+};
+
+const getReactEventName = (eventName) => {
+  return (
+    'on' + eventName.charAt(0).toUpperCase() + _.camelCase(eventName.slice(1))
+  );
+};
 
 const vueEventTemplate = (eventName, eventHookName) => {
   const vueEventName = getVueEventName(eventName);
   return `${eventHookName}(s2Ref, emit, S2Event.${eventName}, '${vueEventName}');`;
-}
+};
 
 const reactEventTemplate = (eventName, eventHookName) => {
-  const reactEventName = 'on' + camelcaseName(eventName);
-  return `${eventHookName}(S2Event.${eventName}, props.${reactEventName}, s2);`
-}
+  const reactEventName = getReactEventName(eventName);
+  return `${eventHookName}(S2Event.${eventName}, props.${reactEventName}, s2);`;
+};
 
 function getCommonInterfaceTemplate(eventHookName, commonEventName) {
   return eventHookName === 'useCellEvent'
@@ -55,7 +47,7 @@ function getCommonInterfaceTemplate(eventHookName, commonEventName) {
 }
 
 /**
- * read terminal input
+ * 读取 eventName
  * @param {string} question
  * @return {Promise<string>}
  */
@@ -65,21 +57,25 @@ const readInputLine = (question) => {
     type: 'input',
     message: question,
     name: 'eventName',
-    default: 'COL_CELL_BRUSH_SELECTION'
+    default: 'COL_CELL_BRUSH_SELECTION',
   });
-}
+};
 
-const readListLine = async (question) => {
+/**
+ * 读取 eventHookName
+ * @param {string} question
+ * @return {Promise<string>}
+ */
+const readListLine = (question) => {
   return inquirer.prompt({
     name: 'eventHookName',
     type: 'list',
     message: question,
     default: 'useS2Event',
-    choices: [ 'useCellEvent', 'useS2Event' ],
+    choices: ['useCellEvent', 'useS2Event'],
   });
-}
-// packages/s2-vue/src/utils/initPropAndEmits.ts
-// packages/s2-shared/src/interface.ts
+};
+
 const VUE_USE_EVENTS_PATH = 'packages/s2-vue/src/hooks/useEvents.ts';
 const REACT_USE_EVENTS_PATH = 'packages/s2-react/src/hooks/useEvents.ts';
 const VUE_INTERFACE_PATH = 'packages/s2-vue/src/utils/initPropAndEmits.ts';
@@ -94,7 +90,6 @@ function insertVueUseEvent(eventName, eventHookName) {
 function insertReactUseEvent(eventName, eventHookName) {
   const reactStr = reactEventTemplate(eventName, eventHookName);
   const reactPath = resolve(process.cwd(), `../../${REACT_USE_EVENTS_PATH}`);
-
   insertEventIntoFile(reactPath, reactStr);
 }
 
@@ -104,10 +99,12 @@ function insertVueInterface(eventName) {
   insertEventIntoFile(vuePath, `'${vueEventName}',`);
 }
 
-
 function insertCommonInterface(eventName, eventHookName) {
-  const commonEventName = 'on' + camelcaseName(eventName);
-  const commonInterfaceTemplate = getCommonInterfaceTemplate(eventHookName, commonEventName);
+  const commonEventName = getReactEventName(eventName);
+  const commonInterfaceTemplate = getCommonInterfaceTemplate(
+    eventHookName,
+    commonEventName,
+  );
   const reactPath = resolve(process.cwd(), `../../${COMMON_INTERFACE_PATH}`);
   insertEventIntoFile(reactPath, `${commonInterfaceTemplate}`);
 }
@@ -116,14 +113,16 @@ const syncEvent = async () => {
   const { eventName } = await readInputLine('请输入事件名称：');
   const { eventHookName } = await readListLine(
     '请选择使用 useCellEvent （将事件中传输的event 转为cell返回）\n' +
-    '或 useS2Event （直接返回原始参数）？');
-  console.log(eventHookName, 'eventName');
+      '或 useS2Event （直接返回原始参数）？',
+  );
+
   insertVueUseEvent(eventName, eventHookName);
   insertVueInterface(eventName);
   insertReactUseEvent(eventName, eventHookName);
   insertCommonInterface(eventName, eventHookName);
 
+  // eslint-disable-next-line no-console
   console.warn(`✅${eventName}插入完成!  ⚠️ 注意自己检查生成结果和格式化一下`);
-}
+};
 
-await syncEvent()
+await syncEvent();
