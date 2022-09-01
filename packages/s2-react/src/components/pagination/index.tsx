287753d9b960031ee49b08747144a71056a5e503
Wenjun Xu <906626481@qq.com>
2022-09-01 17:19:48 +0800

    fix: 修复 pagination 组件属性透传问题, 简化 usePagination 逻辑 (#1722)

--- packages/s2-react/src/components/pagination/index.tsx
+++ packages/s2-react/src/components/pagination/index.tsx
@@ -6,56 +6,36 @@ import type { SheetComponentsProps } from '../sheets/interface';
 
 export interface S2PaginationProps {
   pagination: SheetComponentsProps['options']['pagination'];
-  current: number;
-  total: number;
-  pageSize?: number;
-  setCurrent?: (current: number) => void;
-  setPageSize?: (pageSize: number) => void;
-  onShowSizeChange?: (pageSize: number) => void;
-  onChange?: (current: number) => void;
+  onShowSizeChange: (current: number, pageSize: number) => void;
+  onChange: (current: number, pageSize: number) => void;
 }
 
 const PRE_CLASS = `${S2_PREFIX_CLS}-pagination`;
 
 export const S2Pagination: React.FC<S2PaginationProps> = ({
   pagination,
-  current,
-  total,
-  pageSize,
-  setCurrent,
-  setPageSize = () => {},
-  onShowSizeChange = () => {},
-  onChange = () => {},
+  onShowSizeChange,
+  onChange,
 }) => {
   // not show the pagination
   if (isEmpty(pagination)) {
     return null;
   }
 
+  const { total, pageSize } = pagination;
   // only show the pagination when the pageSize > 5
   const showQuickJumper = total / pageSize > 5;
 
   return (
     <div className={PRE_CLASS}>
       <AntdPagination
-        defaultCurrent={1}
-        current={current}
-        total={total}
-        pageSize={pageSize}
-        showSizeChanger
         size="small"
+        defaultCurrent={1}
+        showSizeChanger
         showQuickJumper={showQuickJumper}
         {...pagination}
-        onShowSizeChange={(current, size) => {
-          onShowSizeChange(size);
-          setPageSize(size);
-          pagination.onShowSizeChange?.(current, size);
-        }}
-        onChange={(page, pageSize) => {
-          onChange(page);
-          setCurrent(page);
-          pagination.onChange?.(page, pageSize);
-        }}
+        onShowSizeChange={onShowSizeChange}
+        onChange={onChange}
       />
       <span
         className={`${PRE_CLASS}-count`}
