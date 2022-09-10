ce61e7798ff852630f3158eaffc673ba2cf6b912
wjgogogo <906626481@qq.com>
2022-09-10 20:34:41 +0800

    refactor: 优化自定义目录树逻辑

--- packages/s2-core/src/data-set/custom-tree-pivot-data-set.ts
+++ packages/s2-core/src/data-set/custom-tree-pivot-data-set.ts
@@ -1,10 +1,9 @@
-import { forEach, get, has, intersection, isEmpty, keys, uniq } from 'lodash';
-import { EXTRA_FIELD, VALUE_FIELD } from '../common/constant';
-import type { Data, S2DataConfig } from '../common/interface';
+import { get } from 'lodash';
+import { EXTRA_FIELD } from '../common/constant';
+import type { S2DataConfig } from '../common/interface';
 import {
   getDataPath,
   transformDimensionsValues,
-  transformIndexesData,
 } from '../utils/dataset/pivot-data-set';
 import { CellData } from './cell-data';
 import type { CellDataParams } from './interface';
@@ -29,67 +28,18 @@ export class CustomTreePivotDataSet extends PivotDataSet {
     }
   }
 
-  setDataCfg(dataCfg: S2DataConfig) {
-    super.setDataCfg(dataCfg);
-    this.sortedDimensionValues = {};
-    this.rowPivotMeta = new Map();
-    this.colPivotMeta = new Map();
-
-    const { rows, columns, values } = this.fields;
-    const { indexesData } = transformIndexesData({
-      rows,
-      columns,
-      values,
-      originData: this.originData,
-      totalData: [], // 自定义目录树没有 totalData 概念
-      indexesData: this.indexesData,
-      sortedDimensionValues: this.sortedDimensionValues,
-      rowPivotMeta: this.rowPivotMeta,
-      colPivotMeta: this.colPivotMeta,
-    });
-    this.indexesData = indexesData;
-
-    this.handleDimensionValuesSort();
-  }
-
   processDataCfg(dataCfg: S2DataConfig): S2DataConfig {
     // 自定义行头有如下几个特点
     // 1、rows配置必须是空，需要额外添加 $$extra$$ 定位数据（标记指标的id）
     // 2、要有配置 fields.rowCustomTree(行头结构)
     // 3、values 不需要参与计算，默认就在行头结构中
-    dataCfg.fields.rows = [EXTRA_FIELD];
-    dataCfg.fields.valueInCols = false;
-    const { data, ...restCfg } = dataCfg;
-    const { values } = dataCfg.fields;
-    // 将源数据中的value值，映射为 $$extra$$,$$value$$
-    // {
-    // province: '四川',    province: '四川',
-    // city: '成都',   =>   city: '成都',
-    // price='11'           price='11'
-    //                      $$extra$$=price
-    //                      $$value$$=11
-    // 此时 province, city 均配置在columns里面
-    // }
-    const transformedData = [];
-    forEach(data, (dataItem) => {
-      if (isEmpty(intersection(keys(dataItem), values))) {
-        transformedData.push(dataItem);
-      } else {
-        forEach(values, (value) => {
-          if (has(dataItem, value)) {
-            transformedData.push({
-              ...dataItem,
-              [EXTRA_FIELD]: value,
-              [VALUE_FIELD]: dataItem[value],
-            });
-          }
-        });
-      }
-    });
-
     return {
-      data: uniq(transformedData),
-      ...restCfg,
+      ...dataCfg,
+      fields: {
+        ...dataCfg.fields,
+        rows: [EXTRA_FIELD],
+        valueInCols: false,
+      },
     };
   }
 }
