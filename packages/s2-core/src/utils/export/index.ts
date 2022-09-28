d9b4cfc932c0a33ecd05d3dece8cf1d6428649a2
Wenjun Xu <906626481@qq.com>
2022-09-28 15:24:53 +0800

    refactor: update packages/s2-core/src/utils/export/index.ts

--- packages/s2-core/src/utils/export/index.ts
+++ packages/s2-core/src/utils/export/index.ts
@@ -92,7 +92,7 @@ export const copyToClipboard = (
     copyableItem = data;
   }
 
-  if (!navigator.clipboard || !window.ClipboardItem || sync)  {
+  if (!navigator.clipboard || !window.ClipboardItem || sync) {
     return copyToClipboardByExecCommand(copyableItem);
   }
   return copyToClipboardByClipboard(copyableItem);
