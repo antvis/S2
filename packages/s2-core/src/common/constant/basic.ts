06877794be701608e5a68e6cab43e56927d59967
Wenjun Xu <906626481@qq.com>
2022-10-19 10:38:50 +0800

    feat: 在生成 id 时，对 undefined, null 做区分 (#1828)
    feat: 在生成 id 时对 null 和 undefined 做区分

--- packages/s2-core/src/common/constant/basic.ts
+++ packages/s2-core/src/common/constant/basic.ts
@@ -8,6 +8,9 @@ export const SERIES_NUMBER_FIELD = '$$series_number$$';
 export const TOTAL_VALUE = '$$total$$';
 export const MULTI_VALUE = '$$multi$$';
 
+export const NULL_SYMBOL_ID = '$$null$$';
+export const UNDEFINED_SYMBOL_ID = '$$undefined$$';
+
 export const BACK_GROUND_GROUP_CONTAINER_Z_INDEX = 0;
 
 // foregroundGroup 上的 children 层叠顺序
--- packages/s2-core/src/utils/layout/index.ts
+++ packages/s2-core/src/utils/layout/index.ts
@@ -1,2 +1,2 @@
-export { generateId } from './generate-id';
+export { generateId, resolveId } from './generate-id';
 export * from './frozen';
