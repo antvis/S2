830341456a5bb6e0a5cc6fadeb8b2fd2d30bb300
stone <stone-lyl@outlook.com>
2022-10-26 15:28:56 +0800

    test: 移动端 Mobile Sheet 单测

---
+++ packages/s2-react/__tests__/unit/components/sheets/mobile-sheet/index-spec.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DEFAULT_MOBILE_OPTIONS, DeviceType } from '@antv/s2/src';
+import { pick } from 'lodash';
+import * as mockDataConfig from '../../../../data/simple-data.json';
+import { MobileSheetComponent } from '../../../../../src/components/sheets/mobile-sheet';
+
+describe('MobileSheet Tests', () => {
+  test('get mobile default option', () => {
+    let s2;
+    render(
+      <MobileSheetComponent
+        dataCfg={mockDataConfig}
+        options={{ height: 300 }}
+        onMounted={(s) => {
+          s2 = s;
+        }}
+      />,
+    );
+
+    const interactionOptions = pick(
+      s2.options.interaction,
+      Object.keys(DEFAULT_MOBILE_OPTIONS.interaction),
+    );
+    const { height, device } = s2.options;
+    expect(interactionOptions).toEqual(DEFAULT_MOBILE_OPTIONS.interaction);
+    expect(height).toEqual(300);
+    expect(device).toEqual(DeviceType.MOBILE);
+  });
+
+  test('get mobile default fragment', () => {
+    const { asFragment } = render(
+      <MobileSheetComponent
+        dataCfg={mockDataConfig}
+        options={{ height: 300, width: 300 }}
+      />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('Mobile Component Tests', () => {});
