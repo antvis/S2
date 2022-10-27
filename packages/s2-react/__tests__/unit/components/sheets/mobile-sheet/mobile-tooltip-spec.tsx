45339543dde9153cecf1c03518f4391533a56e8a
stone <stone-lyl@outlook.com>
2022-10-27 14:30:16 +0800

    test: 添加 mobile tooltip 单测

--- packages/s2-react/__tests__/unit/components/sheets/mobile-sheet/mobile-tooltip-spec.tsx
+++ packages/s2-react/__tests__/unit/components/sheets/mobile-sheet/mobile-tooltip-spec.tsx
@@ -1,21 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { BaseTooltip } from '@antv/s2/src';
 import { MobileSheetComponent } from '../../../../../src/components/sheets/mobile-sheet';
+import { SheetComponent } from '../../../../../src/components/sheets';
 import { CustomTooltip } from '../../../../../src';
-import { TooltipOperator } from '../../../../../src/components/tooltip/components/operator';
 import * as mockDataConfig from '../../../../data/simple-data.json';
-import { TooltipDetail } from '../../../../../src/components/tooltip/components/detail';
-import { TooltipHead } from '../../../../../src/components/tooltip/components/head-info';
-import { TooltipSummary } from '../../../../../src/components/tooltip/components/summary';
 
-describe('Tooltip Common Tests', () => {
-  test('custom tooltip instance of CustomTooltip', () => {
+describe('Mobile Tooltip Different Tests', () => {
+  test('SheetComponent hide tooltip do not trigger renderContent', () => {
     let s2;
+    let customTooltipInstance;
     render(
-      <MobileSheetComponent
+      <SheetComponent
         dataCfg={mockDataConfig}
-        options={{ height: 300 }}
+        options={{
+          tooltip: {
+            renderTooltip: (s) => {
+              customTooltipInstance = new CustomTooltip(s);
+              customTooltipInstance.renderContent = jest.fn();
+              return customTooltipInstance;
+            },
+          },
+        }}
         onMounted={(s) => {
           s2 = s;
         }}
@@ -24,109 +30,63 @@ describe('Tooltip Common Tests', () => {
 
     s2.showTooltip({ position: { x: 0, y: 0 }, content: '111' });
     expect(s2.tooltip).toBeInstanceOf(CustomTooltip);
-    expect(s2.tooltip).toBeInstanceOf(BaseTooltip);
+    expect(s2.tooltip.renderContent).toHaveBeenCalledTimes(1);
+
+    s2.hideTooltip();
+    expect(s2.tooltip.renderContent).toHaveBeenCalledTimes(1);
   });
 
-  test('render sort tooltip: TooltipOperator', () => {
-    const mockCell = jest.fn();
-    const mockMenuClick = jest.fn();
-    const menus = [
-      { key: 'asc', icon: 'groupAsc', text: '组内升序' },
-      { key: 'desc', icon: 'groupDesc', text: '组内降序' },
-      { key: 'none', text: '不排序' },
-    ];
-
-    const { asFragment, getByText } = render(
-      <TooltipOperator
-        menus={menus}
-        key={'tooltipOperator'}
-        cell={mockCell}
-        onlyMenu={true}
-        onClick={mockMenuClick}
-        defaultSelectedKeys={menus[0].key}
+  test('hide tooltip trigger renderContent', () => {
+    let s2;
+    let customTooltipInstance;
+    render(
+      <MobileSheetComponent
+        dataCfg={mockDataConfig}
+        options={{
+          tooltip: {
+            renderTooltip: (s) => {
+              customTooltipInstance = new CustomTooltip(s);
+              customTooltipInstance.renderContent = jest.fn();
+              return customTooltipInstance;
+            },
+          },
+        }}
+        onMounted={(s) => {
+          s2 = s;
+        }}
       />,
     );
 
-    expect(asFragment()).toMatchSnapshot();
-    expect(getByText('组内升序')).toBeTruthy();
-    expect(getByText('不排序').className).toContain(
-      'antv-s2-tooltip-operator-text',
-    );
+    s2.showTooltip({ position: { x: 0, y: 0 }, content: '111' });
+    expect(s2.tooltip).toBeInstanceOf(CustomTooltip);
+    expect(s2.tooltip.renderContent).toHaveBeenCalledTimes(1);
+
+    s2.hideTooltip();
+    expect(s2.tooltip.renderContent).toHaveBeenCalledTimes(2);
   });
 
-  test('render hide icon: TooltipOperator', () => {
-    const hiddenMenus = [
-      {
-        key: 'hiddenColumns',
-        text: '隐藏',
-        icon: 'EyeOutlined',
-        onClick: 'ƒ onClick() {}',
-      },
-    ];
-
-    const { asFragment, getByText, container } = render(
-      <TooltipOperator onlyMenu={false} menus={hiddenMenus} cell={jest.fn()} />,
+  test('show tooltip when visible is true', () => {
+    let s2;
+    let customTooltipInstance;
+    render(
+      <MobileSheetComponent
+        dataCfg={mockDataConfig}
+        options={{
+          tooltip: {
+            renderTooltip: (s) => {
+              customTooltipInstance = new CustomTooltip(s);
+              customTooltipInstance.renderContent = jest.fn();
+              return customTooltipInstance;
+            },
+          },
+        }}
+        onMounted={(s) => {
+          s2 = s;
+        }}
+      />,
     );
 
-    expect(asFragment()).toMatchSnapshot();
-    expect(getByText('隐藏').className).toContain(
-      'antv-s2-tooltip-operator-text',
-    );
-    expect(container.querySelector('svg').getAttribute('data-icon')).toBe(
-      'eye',
-    );
-  });
-
-  test('render TooltipDetail', () => {
-    const list = [
-      {
-        name: '20岁以下',
-        value: '20.5%',
-      },
-    ];
-
-    const { asFragment, getByText } = render(<TooltipDetail list={list} />);
-
-    expect(asFragment()).toMatchSnapshot();
-    expect(getByText('20岁以下')).toBeTruthy();
-    expect(getByText('20.5%').className).toContain('antv-s2-tooltip-highlight');
-  });
-
-  test('render TooltipHead', () => {
-    const cols = [{ name: '所在城市', value: '一二线城市' }];
-    const rows = [
-      { name: '类别', value: '有信用卡' },
-      { name: '职业', value: '学生' },
-      { name: '年龄分布', value: '20岁以下' },
-    ];
-
-    const { asFragment, getByText } = render(
-      <TooltipHead cols={cols} rows={rows} />,
-    );
-
-    expect(asFragment()).toMatchSnapshot();
-    expect(getByText('一二线城市，有信用卡 / 学生 / 20岁以下')).toBeTruthy();
-  });
-
-  test('render TooltipSummary', () => {
-    const summaries = [
-      { name: 'A人群', selectedData: Array(30), value: '495.48 %' },
-      { name: 'B人群', selectedData: Array(30), value: '494.52%' },
-      { name: '差值', selectedData: Array(30), value: '+381%' },
-    ];
-
-    const { asFragment, getByText } = render(
-      <TooltipSummary summaries={summaries} />,
-    );
-
-    expect(asFragment()).toMatchSnapshot();
-    expect(getByText('90 项')).toBeTruthy();
-    expect(getByText('495.48 %').className).toContain(
-      'antv-s2-tooltip-summary-val antv-s2-tooltip-bold',
-    );
+    s2.showTooltip({ position: { x: 0, y: 0 }, content: '111' });
+    expect(s2.tooltip.visible).toEqual(true);
   });
 });
-
-// describe('Mobile Tooltip Tests', function () {
-//
-// });
--- packages/s2-react/__tests__/unit/components/tooltip/index-spec.tsx
+++ packages/s2-react/__tests__/unit/components/tooltip/index-spec.tsx
@@ -1,7 +1,13 @@
-import { getTooltipOperatorSortMenus, S2CellType } from '@antv/s2';
+import { BaseTooltip, getTooltipOperatorSortMenus, S2CellType } from '@antv/s2';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { TooltipComponent } from '../../../../src';
+import { CustomTooltip, TooltipComponent } from '../../../../src';
+import { MobileSheetComponent } from '../../../../src/components/sheets/mobile-sheet';
+import { TooltipOperator } from '../../../../src/components/tooltip/components/operator';
+import { TooltipDetail } from '../../../../src/components/tooltip/components/detail';
+import { TooltipHead } from '../../../../src/components/tooltip/components/head-info';
+import { TooltipSummary } from '../../../../src/components/tooltip/components/summary';
+import * as mockDataConfig from '../../../data/simple-data.json';
 
 describe('Tooltip Component Tests', () => {
   // https://github.com/antvis/S2/issues/1716
@@ -33,4 +39,122 @@ describe('Tooltip Component Tests', () => {
       expect(selectedMenu[0]?.textContent).toContain(text);
     },
   );
+});
+
+describe('Tooltip Common Components Tests', () => {
+  test('custom tooltip instance of CustomTooltip', () => {
+    let s2;
+    render(
+      <MobileSheetComponent
+        dataCfg={mockDataConfig}
+        options={{ height: 300 }}
+        onMounted={(s) => {
+          s2 = s;
+        }}
+      />,
+    );
+
+    s2.showTooltip({ position: { x: 0, y: 0 }, content: '111' });
+    expect(s2.tooltip).toBeInstanceOf(CustomTooltip);
+    expect(s2.tooltip).toBeInstanceOf(BaseTooltip);
+  });
+
+  test('render sort tooltip: TooltipOperator', () => {
+    const mockCell = jest.fn();
+    const mockMenuClick = jest.fn();
+    const menus = [
+      { key: 'asc', icon: 'groupAsc', text: '组内升序' },
+      { key: 'desc', icon: 'groupDesc', text: '组内降序' },
+      { key: 'none', text: '不排序' },
+    ];
+
+    const { asFragment, getByText } = render(
+      <TooltipOperator
+        menus={menus}
+        key={'tooltipOperator'}
+        cell={mockCell}
+        onlyMenu={true}
+        onClick={mockMenuClick}
+        defaultSelectedKeys={menus[0].key}
+      />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText('组内升序')).toBeTruthy();
+    expect(getByText('不排序').className).toContain(
+      'antv-s2-tooltip-operator-text',
+    );
+  });
+
+  test('render hide icon: TooltipOperator', () => {
+    const hiddenMenus = [
+      {
+        key: 'hiddenColumns',
+        text: '隐藏',
+        icon: 'EyeOutlined',
+        onClick: 'ƒ onClick() {}',
+      },
+    ];
+
+    const { asFragment, getByText, container } = render(
+      <TooltipOperator onlyMenu={false} menus={hiddenMenus} cell={jest.fn()} />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText('隐藏').className).toContain(
+      'antv-s2-tooltip-operator-text',
+    );
+    expect(container.querySelector('svg').getAttribute('data-icon')).toBe(
+      'eye',
+    );
+  });
+
+  test('render TooltipDetail', () => {
+    const list = [
+      {
+        name: '20岁以下',
+        value: '20.5%',
+      },
+    ];
+
+    const { asFragment, getByText } = render(<TooltipDetail list={list} />);
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText('20岁以下')).toBeTruthy();
+    expect(getByText('20.5%').className).toContain('antv-s2-tooltip-highlight');
+  });
+
+  test('render TooltipHead', () => {
+    const cols = [{ name: '所在城市', value: '一二线城市' }];
+    const rows = [
+      { name: '类别', value: '有信用卡' },
+      { name: '职业', value: '学生' },
+      { name: '年龄分布', value: '20岁以下' },
+    ];
+
+    const { asFragment, getByText } = render(
+      <TooltipHead cols={cols} rows={rows} />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText('一二线城市，有信用卡 / 学生 / 20岁以下')).toBeTruthy();
+  });
+
+  test('render TooltipSummary', () => {
+    const summaries = [
+      { name: 'A人群', selectedData: Array(30), value: '495.48 %' },
+      { name: 'B人群', selectedData: Array(30), value: '494.52%' },
+      { name: '差值', selectedData: Array(30), value: '+381%' },
+    ];
+
+    const { asFragment, getByText } = render(
+      <TooltipSummary summaries={summaries} />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText('90 项')).toBeTruthy();
+    expect(getByText('495.48 %').className).toContain(
+      'antv-s2-tooltip-summary-val antv-s2-tooltip-bold',
+    );
+  });
 });
