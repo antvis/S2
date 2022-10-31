b4b422c687e799592cce85714d427c0b6e513b4a
wjgogogo <906626481@qq.com>
2022-10-31 16:22:59 +0800

    fix: 修复行列冻结序号列主题

--- packages/s2-core/src/cell/table-series-cell.ts
+++ packages/s2-core/src/cell/table-series-cell.ts
@@ -4,7 +4,9 @@ import { TableDataCell } from './table-data-cell';
 
 export class TableSeriesCell extends TableDataCell {
   public get cellType() {
-    return CellTypes.ROW_CELL;
+    // 在行列冻结并且开启序号时
+    // 那么在如果行头冻结2列，并且CellTypes设置成以前的RowCell时，【FrozenRowGroup的分割线】和【左上角和左下角的边框样式】样式会混乱
+    return CellTypes.DATA_CELL;
   }
 
   protected getTextStyle(): TextTheme {
