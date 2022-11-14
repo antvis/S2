5dae6dab49c424010d3e003d9918d3d778081a26
stone <stone-lyl@outlook.com>
2022-11-14 15:30:22 +0800

    fix: 解决单个IP地址请求过多，导致Google API 报 14 错误问题

--- s2-site/script/translation.mjs
+++ s2-site/script/translation.mjs
@@ -21,7 +21,7 @@ const targetLanguageCode = 'en';
 // 文件信息
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const mdFile = path.join(__dirname, '../docs/api/basic-class');
+const mdFile = path.join(__dirname, '../docs/api');
 const allFilesName = glob.sync("*.zh.md", { cwd: mdFile, realpath: true });
 
 // markdown 和 AST 的转换方法
@@ -30,23 +30,34 @@ const toMarkdown = (ast) => {
   return unified().use(stringify).stringify(ast);
 };
 
+const isValidHttpUrl = (string) => {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
 /**
- * 中翻英，翻译方法
- * @param {string} originalText
+ * google translate API 请求, 中翻英
+ * @param {string[]} originalText
  * @return {Promise<*[]>}
  */
-const translateText = async (originalText) => {
+const getTranslatedText = async (originalText) => {
   const translationClient = new TranslationServiceClient();
   const request = {
     parent: `projects/${projectId}/locations/${location}`,
-    contents: [ originalText ],
+    contents: originalText,
     mimeType: 'text/plain', // mime types: text/plain, text/html
     sourceLanguageCode: 'zh',
     targetLanguageCode,
   };
-  const result = [];
-  // Run request
+  let result = [];
   try {
+    // Run request
+    console.log('request 11');
     const [ response ] = await translationClient.translateText(request);
     for (const translation of response.translations) {
       result.push(translation.translatedText);
@@ -57,23 +68,70 @@ const translateText = async (originalText) => {
   return result;
 }
 
-// 请求 google 翻译，将翻译结果写入 AST
-const getOriginalAllText = (mdAST) => {
-  const originalText = [];
-  visit(mdAST, async (node) => {
-    if (node.type === "text" || node.type === "code") {
-      originalText.push(node.value);
-      const translatedText = await translateText(node.value);
-      node.value = translatedText[0];
+const isLinkType = (node) => {
+  return node.type === 'link';
+}
+const formatUrl = (url) => url?.replace(/^#/, '');
+
+/**
+ * 请求 google 翻译，将翻译结果写入 AST
+ * @param mdAST
+ * @return {{textMap: Map<string, number>, originalTextArr: string[]}}
+ */
+const getOriginalTextInfo = (mdAST) => {
+  const originalTextArr = [];
+  const textMap = new Map();
+  let index = 0;
+
+  visit(mdAST, (node) => {
+    if (isTranslateText(node)) {
+      if (isLinkType(node) && isValidHttpUrl(node.url)) {
+        return;
+      }
+      const nodeVal = node.type === 'link' ? formatUrl(node.url) : node.value;
+      if (!textMap.has(nodeVal)) {
+        originalTextArr.push(nodeVal);
+        textMap.set(nodeVal, index);
+        index++;
+      }
     }
   });
-  return originalText;
+  return { originalTextArr, textMap };
 }
 
-const translatedTextArray = (originalTextList) => originalTextList.map(async (text) => {
-  return await translateText(text);
-});
+// 判断是否为需要翻译的文本
+function isTranslateText(node) {
+  return node.type === "text" || node.type === "code" || node.type === "link";
+}
 
+/**
+ * 将翻译后的文本写入 AST
+ * @param mdAST 翻译前的 markdown AST
+ * @param translatedTextList 翻译后的文本
+ * @param textMap 原文本和翻译后文本的对应关系
+ * @return mdAST 翻译后的 markdown AST
+ */
+const writeValueToAST = (mdAST, translatedTextList, textMap) => {
+  visit(mdAST, (node) => {
+    if (isTranslateText(node)) {
+      if (isLinkType(node) && !isValidHttpUrl(node.url)) {
+        const valueIndex = textMap.get(formatUrl(node.url));
+        node.url = `#${translatedTextList[valueIndex]}`;
+      } else {
+        const valueIndex = textMap.get(node.value);
+        node.value = translatedTextList[valueIndex];
+      }
+    }
+  });
+  return mdAST;
+}
+
+/**
+ * 将 AST 转换为 markdown 写入文件
+ * @param pathName
+ * @param mdAST
+ * @return {Promise<void>}
+ */
 const writeToFile = async (pathName, mdAST) => {
   const writePath = pathName.replace('.zh.md', '.en.md');
   fs.writeFileSync(
@@ -84,12 +142,19 @@ const writeToFile = async (pathName, mdAST) => {
   console.log(`${writePath} written`);
 }
 
+/**
+ * 将中文 markdown 转换为英文 markdown
+ * eg: api/README.zh.md -> api/README.en.md
+ * 获取文件内容 -> 转换为 AST -> 提取文字 -> 翻译 -> 写入 AST -> 转换为 markdown -> 写入文件
+ */
 allFilesName.forEach(async (pathName) => {
-  // 获取文件内容 -> 转换为 AST -> 提取文字 -> 翻译 -> 写入 AST -> 转换为 markdown -> 写入文件
   const mdContent = fs.readFileSync(pathName, 'utf8');
   const mdAST = toMdAST(mdContent);
-  const originalTextList = getOriginalAllText(mdAST);
-  await Promise.all(translatedTextArray(originalTextList));
+  const { originalTextArr, textMap } = getOriginalTextInfo(mdAST);
+  console.log(originalTextArr, 'originalTextList', originalTextArr.length);
+  const translatedAllText = await getTranslatedText(originalTextArr);
+  writeValueToAST(mdAST, translatedAllText, textMap);
+  console.log(translatedAllText, 'translatedAllText', translatedAllText.length);
   await writeToFile(pathName, mdAST);
 });
 
