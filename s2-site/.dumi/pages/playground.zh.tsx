a4541dad1fcf8220bc3c473af282e889edf41d5b
yingying.xwy <yingying.xwy@antgroup.com>
2022-11-17 16:36:17 +0800

    chore: 更新playground配置

--- s2-site/.dumi/pages/playground.zh.tsx
+++ s2-site/.dumi/pages/playground.zh.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Page from '../../playground/layouts';
+import { Header } from '@antv/dumi-theme-antv/dist/slots/Header';
+import { Footer } from '@antv/dumi-theme-antv/dist/slots/Footer';
 
 const Playground: React.FC = () => {
-  return <Page />;
+  return (
+    <>
+    <Header/>
+    <Page />
+    <Footer />
+    </>
+  )
 };
 
 export default Playground;
