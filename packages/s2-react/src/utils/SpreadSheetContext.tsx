f0c0b6e6c9485dd92f2f358274a11e60a52bcaea
wengyidong.wyd <wengyidong.wyd@alibaba-inc.com>
2022-11-18 19:28:50 +0800

    fix: test

--- packages/s2-react/src/utils/SpreadSheetContext.tsx
+++ packages/s2-react/src/utils/SpreadSheetContext.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import type { SpreadSheet } from '@antv/s2';
+import type { TableFacet } from 's2-core/src/facet';
+
+export const facet: TableFacet | null = null;
 
 export const SpreadSheetContext = React.createContext<SpreadSheet>(null as any);
 
