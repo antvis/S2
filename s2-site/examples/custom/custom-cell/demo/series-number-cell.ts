0081e9646610a82d3e41784731d2bb10ca1124d4
wjgogogo <906626481@qq.com>
2022-12-02 15:02:12 +0800

    docs: 新增自定义序号单元格实例

---
+++ s2-site/examples/custom/custom-cell/demo/series-number-cell.ts
@@ -0,0 +1,73 @@
+import {
+  PivotSheet,
+  SeriesNumberCell,
+  Node,
+  SERIES_NUMBER_FIELD,
+} from '@antv/s2';
+
+/**
+ * 自定义 RowCell，给行头添加背景图
+ * 查看更多方法 https://github.com/antvis/S2/blob/master/packages/s2-core/src/cell/row-cell.ts
+ */
+class CustomSeriesCell extends SeriesNumberCell {
+  // 覆盖背景绘制，可覆盖或者增加绘制方法
+  getBackgroundColor() {
+    return { backgroundColor: 'cyan', backgroundColorOpacity: 0.5 };
+  }
+}
+
+const layoutSeriesNumberNodes = (
+  rowsHierarchy,
+  seriesNumberWidth,
+  spreadsheet,
+) => {
+  const isHierarchyTreeType = spreadsheet.isHierarchyTreeType();
+  const leaves = isHierarchyTreeType
+    ? rowsHierarchy.getNodes()
+    : rowsHierarchy.getLeaves();
+
+  return leaves.map((node, idx) => {
+    const sNode = new Node({
+      id: '',
+      key: SERIES_NUMBER_FIELD,
+      rowIndex: idx,
+      value: `${String.fromCharCode(97 + idx)}`, // 序号从 a 开始递增，在大量数据下，需要更完善的处理
+    });
+    sNode.x = 0;
+    sNode.y = node.y;
+    sNode.width = seriesNumberWidth;
+    sNode.height = node.height;
+    return sNode;
+  });
+};
+
+fetch(
+  'https://gw.alipayobjects.com/os/bmw-prod/cd9814d0-6dfa-42a6-8455-5a6bd0ff93ca.json',
+)
+  .then((res) => res.json())
+  .then((res) => {
+    const container = document.getElementById('container');
+    const s2DataConfig = {
+      fields: {
+        rows: ['province', 'city'],
+        columns: ['type', 'sub_type'],
+        values: ['number'],
+      },
+      meta: res.meta,
+      data: res.data,
+    };
+    const s2Options = {
+      width: 600,
+      height: 480,
+      hierarchyType: 'tree', // 切换到 grid 模式试试看
+      showSeriesNumber: true,
+      layoutSeriesNumberNodes,
+      seriesNumberCell: (node, s2, headConfig) => {
+        return new CustomSeriesCell(node, s2, headConfig);
+      },
+    };
+    const s2 = new PivotSheet(container, s2DataConfig, s2Options);
+
+    // 使用
+    s2.render();
+  });
