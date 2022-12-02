f3be27042206ae398f88a55c6b8d5e3fb8544667
Jinke Li <a1231236677287@163.com>
2022-12-02 17:10:37 +0800

    refactor: 替换成函数组件

--- packages/s2-react/src/common/react-element.tsx
+++ packages/s2-react/src/common/react-element.tsx
@@ -2,35 +2,33 @@ import React from 'react';
 import cls from 'classnames';
 import { S2_PREFIX_CLS } from '@antv/s2';
 
-interface Props {
+interface ReactElementProps {
   content: React.ReactNode;
   style?: React.CSSProperties;
   className?: string;
 }
 
-export class ReactElement extends React.PureComponent<Props> {
-  render() {
-    const { style = {}, className, content } = this.props;
-    const commonProps: JSX.IntrinsicElements['span'] = {
-      style,
-      className: cls(`${S2_PREFIX_CLS}-react-element`, className),
-    };
+export const ReactElement: React.FC<ReactElementProps> = (props) => {
+  const { style = {}, className, content } = props;
+  const commonProps: JSX.IntrinsicElements['span'] = {
+    style,
+    className: cls(`${S2_PREFIX_CLS}-react-element`, className),
+  };
 
-    // React 组件
-    if (React.isValidElement(content)) {
-      return (
-        <span {...commonProps}>
-          <>{content}</>
-        </span>
-      );
-    }
-
-    // DOM/字符串
-    const htmlNode =
-      typeof content !== 'string' ? (content as Element)?.innerHTML : content;
-
+  // React 组件
+  if (React.isValidElement(content)) {
     return (
-      <span {...commonProps} dangerouslySetInnerHTML={{ __html: htmlNode }} />
+      <span {...commonProps}>
+        <>{content}</>
+      </span>
     );
   }
-}
+
+  // DOM/字符串
+  const htmlNode =
+    typeof content !== 'string' ? (content as Element)?.innerHTML : content;
+
+  return (
+    <span {...commonProps} dangerouslySetInnerHTML={{ __html: htmlNode }} />
+  );
+};
