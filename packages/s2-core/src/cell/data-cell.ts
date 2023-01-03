84558caba4bd0f5f520bee6bc42982e042f8e427
stone <stone-lyl@users.noreply.github.com>
2023-01-03 11:14:43 +0800

    fix : 修复点击行列头无法整行高亮的问题 (#2025)
    * fix: 修复点击行列头无法整行高亮的问题
    
    * test: 添加修复点击行列头无法整行高亮的问题的单测
    
    * docs: 完善 selectedCellHighlight 相关文档
    
    * docs: 完善 selectedCellHighlight 相关文档
    
    Co-authored-by: zishang <lyl275911@antgroup.com>

--- packages/s2-core/src/cell/data-cell.ts
+++ packages/s2-core/src/cell/data-cell.ts
@@ -141,7 +141,8 @@ export class DataCell extends BaseCell<ViewMeta> {
 
   public update() {
     const stateName = this.spreadsheet.interaction.getCurrentStateName();
-    const cells = this.spreadsheet.interaction.getCells([CellTypes.DATA_CELL]);
+    // 获取当前 interaction 记录的 Cells 元信息列表，不仅仅是数据单元格，也可能是行头或者列头。
+    const cells = this.spreadsheet.interaction.getCells();
 
     if (stateName === InteractionStateName.ALL_SELECTED) {
       this.updateByState(InteractionStateName.SELECTED);
