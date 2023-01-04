c073578dc008ef83a2877041830be18f827c7341
Jinke Li <a1231236677287@163.com>
2023-01-04 11:38:52 +0800

    fix(layout): 修复无列头时行头对应的角头不显示 close #1929 (#2026)
    * fix(layout): 修复无列头时行头对应的角头不显示 close #1929
    
    * test: 修复测试

---
+++ packages/s2-core/__tests__/spreadsheet/corner-spec.ts
@@ -0,0 +1,114 @@
+import * as simpleDataConfig from 'tests/data/simple-data.json';
+import { DEFAULT_STYLE, type SpreadSheet } from '../../src';
+import { createPivotSheet } from '../util/helpers';
+
+describe('PivotSheet Corner Tests', () => {
+  let s2: SpreadSheet;
+
+  beforeEach(() => {
+    s2 = createPivotSheet({
+      width: 300,
+      height: 300,
+    });
+    s2.render();
+  });
+
+  afterEach(() => {
+    s2.destroy();
+  });
+
+  // https://github.com/antvis/S2/issues/1929
+  test('should render row corner when columns is empty and measure hidden', () => {
+    s2.setDataCfg({
+      ...simpleDataConfig,
+      fields: {
+        ...simpleDataConfig.fields,
+        columns: [],
+      },
+    });
+    s2.setOptions({
+      style: {
+        colCfg: {
+          hideMeasureColumn: true,
+        },
+      },
+    });
+    s2.render();
+
+    const cornerNodes = s2.facet.getCornerNodes();
+    const { colsHierarchy } = s2.facet.layoutResult;
+
+    expect(colsHierarchy.width).toEqual(0);
+    expect(colsHierarchy.height).toEqual(DEFAULT_STYLE.colCfg.height);
+    expect(colsHierarchy.sampleNodeForLastLevel.y).toEqual(0);
+    expect(colsHierarchy.sampleNodeForLastLevel.height).toEqual(
+      DEFAULT_STYLE.colCfg.height,
+    );
+    expect(cornerNodes).toHaveLength(2);
+    expect(s2.facet.getCornerNodes()).toMatchSnapshot();
+  });
+
+  test('should render row corner when measure hidden', () => {
+    s2.setOptions({
+      style: {
+        colCfg: {
+          hideMeasureColumn: true,
+        },
+      },
+    });
+    s2.render();
+
+    const cornerNodes = s2.facet.getCornerNodes();
+    const { colsHierarchy } = s2.facet.layoutResult;
+
+    expect(colsHierarchy.width).toEqual(100);
+    expect(colsHierarchy.height).toEqual(DEFAULT_STYLE.colCfg.height);
+    expect(colsHierarchy.sampleNodeForLastLevel.y).toEqual(0);
+    expect(colsHierarchy.sampleNodeForLastLevel.height).toEqual(
+      DEFAULT_STYLE.colCfg.height,
+    );
+    expect(cornerNodes).toHaveLength(2);
+    expect(s2.facet.getCornerNodes()).toMatchSnapshot();
+  });
+
+  test('should render row corner when columns and values is empty', () => {
+    s2.setDataCfg({
+      ...simpleDataConfig,
+      fields: {
+        ...simpleDataConfig.fields,
+        columns: [],
+        values: [],
+      },
+    });
+    s2.render();
+
+    const cornerNodes = s2.facet.getCornerNodes();
+    const { colsHierarchy } = s2.facet.layoutResult;
+
+    expect(colsHierarchy.width).toEqual(0);
+    expect(colsHierarchy.height).toEqual(0);
+    expect(colsHierarchy.sampleNodeForLastLevel).toBeNull();
+    expect(cornerNodes).toHaveLength(2);
+    expect(cornerNodes).toMatchSnapshot();
+  });
+
+  test('should not render row corner when fields is empty', () => {
+    s2.setDataCfg({
+      ...simpleDataConfig,
+      fields: {
+        rows: [],
+        columns: [],
+        values: [],
+      },
+    });
+    s2.render();
+
+    const cornerNodes = s2.facet.getCornerNodes();
+    const { colsHierarchy } = s2.facet.layoutResult;
+
+    expect(colsHierarchy.width).toEqual(0);
+    expect(colsHierarchy.height).toEqual(0);
+    expect(colsHierarchy.sampleNodeForLastLevel).toBeNull();
+    expect(cornerNodes).toHaveLength(0);
+  });
+});
