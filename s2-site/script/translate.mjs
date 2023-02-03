c32cc67abb1bc46773724661c5146b361343a94b
stone <stone-lyl@outlook.com>
2023-02-03 14:53:14 +0800

    chore: 添加测试文件

--- s2-site/script/translate.mjs
+++ s2-site/script/translate.mjs
@@ -2,10 +2,11 @@ import translateDoc from "@stone-lyl/google-translate-script/src/translateDoc.mj
 import { fileURLToPath } from "url";
 
 translateDoc({
-  fileName: '../../docs/manual/basic',
+  fileName: '../../docs/manual/basic/sheet-type',
   dirname: fileURLToPath(import.meta.url),
   sourceSuffix: '.zh.md',
   projectInfo: {
     projectId: 'crested-sunup-368006',
-  }
+  },
+  isKeepHtml: true,
 });
