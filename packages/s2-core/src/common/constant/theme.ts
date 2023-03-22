6dcc55cb2a80f29f516cd9d438b64380e86376de
刘嘉一 <lcx.seima@gmail.com>
2023-03-22 16:05:07 +0800

    feat: 增加暗黑主题

--- packages/s2-core/src/common/constant/theme.ts
+++ packages/s2-core/src/common/constant/theme.ts
@@ -2,12 +2,14 @@ import type { Palette } from '../interface';
 import { paletteColorful } from '../../theme/palette/colorful';
 import { paletteDefault } from '../../theme/palette/default';
 import { paletteGray } from '../../theme/palette/gray';
+import { paletteDark } from '../../theme/palette/dark';
 
 // Map of the theme
 export const PALETTE_MAP: Record<string, Palette> = {
   default: paletteDefault,
   colorful: paletteColorful,
   gray: paletteGray,
+  dark: paletteDark,
 };
 
 /**
---
+++ packages/s2-core/src/theme/palette/dark.ts
@@ -0,0 +1,24 @@
+import type { Palette } from '../../common';
+
+export const paletteDark: Palette = {
+  brandColor: '#255dff',
+  basicColorRelations: [],
+  basicColors: [
+    '#ffffff',
+    '#151a27',
+    '#213f94',
+    '#133aad',
+    '#213f94',
+    '#255dff',
+    '#4b91ff',
+    '#4b91ff',
+    '#191919',
+    '#1e2436',
+    '#0647b1',
+    '#7899ff',
+    '#7899ff',
+    '#f0f0f0',
+    '#ffffff',
+  ],
+  semanticColors: { red: '#FF4D4F', green: '#29A294', yellow: '#FAAD14' },
+};
