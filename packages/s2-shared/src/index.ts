21377fd49d87a9c074434544c61a6a2a1da95073
刘嘉一 <lcx.seima@gmail.com>
2023-03-27 20:21:19 +0800

    feat: 适配switcher&tooltip样式

--- packages/s2-shared/src/index.ts
+++ packages/s2-shared/src/index.ts
@@ -3,4 +3,5 @@ export * from './utils/classnames';
 export * from './utils/options';
 export * from './utils/resize';
 export * from './utils/drill-down';
+export * from './utils/theme';
 export * from './constant';
