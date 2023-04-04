6ccd1b9431241b4224e7574a3c625f33123021a6
stone <stone-lyl@users.noreply.github.com>
2023-04-04 10:44:31 +0800

    [WIP] feat: 统一导出和复制逻辑，优化导出和复制性能 (#2134)
    * test: 完善导出方法的单测
    
    * feat: 抽离 copy 文件的方法，将选择行列的方法，都集成到 processPivotSelected 中
    
    * chore: 修复推送的 lint 报错
    
    * refactor: 统一数据返回格式
    
    * refactor: 重构 table 表格复制逻辑
    
    * refactor: 分离不同模块的代码
    
    * chore: 添加注释和删除无用的代码
    
    * chore: 修复 lint 问题
    
    * fix: 修复循环引用导致单测错误
    
    * fix: 修复因换行符不一致和序号不展示导致的单测错误
    
    * test: 简化单测
    
    * chore: 移动常量到 constant 文件，修改文件名，导出方法统一到 index 中
    
    * feat: 使用复制全选代替导出全选
    
    * fix: 解决树状模式下，全部折叠行头，全量导出错误的问题
    
    * refactor: 使用类重构 pivot-data-cell-copy.ts 文件逻辑
    
    * fix: 解决复制和导出格式化数据单元格格式化错误的问题
    
    * fix: 按整列，整行和全量导出时，行头格式化正确
    
    * test: 新增获取刷选后格式化内容单测
    
    * test: 修复树状模式下带行头刷选错误
    
    * feat: 实现明细表的全量导出功能
    
    * feat: 实现明细表在导出时，展示序号的功能
    
    * feat: 明细表选择某行某列时也需要添加序号
    
    * refactor: 使用新的 copyData 方法来复制和导出全量数据
    
    * refactor: 统一交叉表行列矩阵的生成
    
    * refactor: 简化格式化方法
    
    * refactor: getDataMatrixByDataCell 分别放置到 table 和 pivot 中
    
    * refactor: 重命名 copy 相关的函数名称，同时重构 table copy 相关冗余的业务逻辑
    
    * refactor: 删除 pivot 中一些冗余的代码，将函数放置于合适的文件中
    
    * refactor: getFormatter 重构由外部控制
    
    * refactor: 对不需要进行格式化的数据，使用 getHeaderList 生成，提高复制导出的速度
    
    * fix: 修复合并后单测失败问题
    
    * fix: 修复升级后的单测问题
    
    * fix: 修复 getCornerMatrix 时，修改 columns 的问题
    
    * feat: 完成趋势行列头和数值单元导出逻辑
    
    * feat: 完成趋势分析表角头展示
    
    * test: 修复趋势分析表导出单测问题
    
    * refactor: 统一导出内容，删除冗余代码

--- packages/s2-core/__tests__/unit/utils/export/export-spec.ts
+++ packages/s2-core/__tests__/unit/utils/export/export-spec.ts
@@ -4,6 +4,7 @@ import { assembleDataCfg, assembleOptions } from '../../../util';
 import { getContainer } from '../../../util/helpers';
 import { PivotSheet, TableSheet } from '@/sheet-type';
 import { copyData } from '@/utils';
+import { NewTab, NewLine } from '@/common';
 
 describe('TableSheet Export Test', () => {
   it('should export correct data with series number', () => {
@@ -28,33 +29,28 @@ describe('TableSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t', {
+    const data = copyData(s2, NewTab, {
       isFormatHeader: true,
     });
-    const rows = data.split('\n');
-    const headers = rows[0].split('\t');
+    const rows = data.split(NewLine);
+    const headers = rows[0].split(NewTab);
 
-    // 33行数据 包括一行列头
-    expect(rows).toHaveLength(33);
     expect(slice(rows, 0, 5)).toMatchInlineSnapshot(`
       Array [
-        "\\"序号\\"	\\"province\\"	\\"city\\"	\\"产品类型\\"	\\"sub_type\\"	\\"number\\"
-      ",
-        "\\"1\\"	\\"浙江省\\"	\\"杭州市\\"	\\"家具\\"	\\"桌子\\"	\\"7789\\"
-      ",
-        "\\"2\\"	\\"浙江省\\"	\\"绍兴市\\"	\\"家具\\"	\\"桌子\\"	\\"2367\\"
-      ",
-        "\\"3\\"	\\"浙江省\\"	\\"宁波市\\"	\\"家具\\"	\\"桌子\\"	\\"3877\\"
-      ",
-        "\\"4\\"	\\"浙江省\\"	\\"舟山市\\"	\\"家具\\"	\\"桌子\\"	\\"4342\\"
-      ",
+        "序号	province	city	产品类型	sub_type	number",
+        "1	浙江省	杭州市	家具	桌子	7789",
+        "2	浙江省	绍兴市	家具	桌子	2367",
+        "3	浙江省	宁波市	家具	桌子	3877",
+        "4	浙江省	舟山市	家具	桌子	4342",
       ]
     `);
+    // 33行数据 包括一行列头
+    expect(rows).toHaveLength(33);
     // 6列数据 包括序列号
     rows.forEach((e) => {
-      expect(e.split('\t')).toHaveLength(6);
+      expect(e.split(NewTab)).toHaveLength(6);
     });
-    expect(headers.map((e) => JSON.parse(e))).toEqual([
+    expect(headers).toEqual([
       '序号',
       'province',
       'city',
@@ -79,23 +75,17 @@ describe('TableSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
-    const headers = rows[0].split('\t');
+    const data = copyData(s2, NewTab);
+    const rows = data.split(NewLine);
+    const headers = rows[0].split(NewTab);
 
     // 33行数据 包括一行列头
     expect(rows).toHaveLength(33);
     // 5列数据 包括序列号
     rows.forEach((e) => {
-      expect(e.split('\t')).toHaveLength(5);
+      expect(e.split(NewTab)).toHaveLength(5);
     });
-    expect(headers.map((e) => JSON.parse(e))).toEqual([
-      'province',
-      'city',
-      'type',
-      'sub_type',
-      'number',
-    ]);
+    expect(headers).toEqual(['province', 'city', 'type', 'sub_type', 'number']);
   });
 
   it('should export correct data with totals', () => {
@@ -123,21 +113,21 @@ describe('TableSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t', true);
+    const data = copyData(s2, NewTab, true);
 
     expect(data).toMatchInlineSnapshot(`
-      "\\"province\\"	\\"type\\"	\\"sub_type\\"	\\"number\\"
-      \\"浙江省-province\\"	\\"家具-type\\"	\\"桌子\\"	\\"7789\\"
-      \\"浙江省-province\\"	\\"家具-type\\"	\\"桌子\\"	\\"2367\\"
-      \\"浙江省-province\\"	\\"家具-type\\"	\\"桌子\\"	\\"3877\\"
-      \\"浙江省-province\\"	\\"家具-type\\"	\\"桌子\\"	\\"4342\\"
-      \\"浙江省-province\\"	\\"家具-type\\"	\\"沙发\\"	\\"5343\\""
+      "province	type	sub_type	number
+      浙江省-province	家具-type	桌子	7789
+      浙江省-province	家具-type	桌子	2367
+      浙江省-province	家具-type	桌子	3877
+      浙江省-province	家具-type	桌子	4342
+      浙江省-province	家具-type	沙发	5343"
     `);
   });
 });
 
 describe('PivotSheet Export Test', () => {
-  it('should export correct data in grid mode', () => {
+  it('should export correct data in grid mode - get', () => {
     const s2 = new PivotSheet(
       getContainer(),
       assembleDataCfg({
@@ -151,18 +141,18 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
+    const data = copyData(s2, NewTab);
+    const rows = data.split(NewLine);
 
     expect(rows).toHaveLength(14);
-    expect(rows[0].split('\t')[1]).toEqual('"province"');
-    expect(rows[1].split('\t')[1]).toEqual('"city"');
+    expect(rows[0].split(NewTab)[1]).toEqual('province');
+    expect(rows[1].split(NewTab)[1]).toEqual('city');
 
     rows.forEach((e) => {
-      expect(e.split('\t')).toHaveLength(34);
+      expect(e.split(NewTab)).toHaveLength(34);
     });
   });
-  it('should export correct data in tree mode', () => {
+  it('should export correct data in tree mode - get', () => {
     const s2 = new PivotSheet(
       getContainer(),
       assembleDataCfg({
@@ -178,14 +168,14 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
+    const data = copyData(s2, NewTab);
+    const rows = data.split(NewLine);
 
     expect(rows).toHaveLength(16);
-    expect(rows[0].split('\t')[1]).toEqual('"province"');
-    expect(rows[1].split('\t')[1]).toEqual('"city"');
+    expect(rows[0].split(NewTab)[1]).toEqual('province');
+    expect(rows[1].split(NewTab)[1]).toEqual('city');
     rows.forEach((e) => {
-      expect(e.split('\t')).toHaveLength(34);
+      expect(e.split(NewTab)).toHaveLength(34);
     });
   });
 
@@ -193,12 +183,17 @@ describe('PivotSheet Export Test', () => {
    * 因为导出的数据单测，很难看出问题，所以提供图片 + 代码的模式查看：
    * https://gw.alipayobjects.com/zos/antfincdn/AU83KF1Sq/6fb3f3e6-0064-4ef8-a5c3-b1333fb59adf.png
    */
-  it('should export correct data in tree mode and collapseAll is true', () => {
+  it('should export correct data in tree mode and collapseAll is true -get', () => {
     const s2 = new PivotSheet(
       getContainer(),
       assembleDataCfg(),
       assembleOptions({
         hierarchyType: 'tree',
+        totals: {
+          row: {
+            showSubTotals: true,
+          },
+        },
         style: {
           rowCell: {
             collapseAll: true,
@@ -208,21 +203,28 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
+    const data = copyData(s2, NewTab, true);
+    const rows = data.split(NewLine);
 
+    expect(rows).toMatchInlineSnapshot(`
+      Array [
+        "类别	家具	家具	办公用品	办公用品",
+        "子类别	桌子	沙发	笔	纸张",
+        "省份	数量	数量	数量	数量",
+        "浙江省	18375	14043	4826	5854",
+        "四川省	7818	9473	7495	10984",
+      ]
+    `);
     expect(rows).toHaveLength(5);
-    expect(rows[0].split('\t').length).toEqual(5);
-    expect(rows[0].split('\t')[0]).toEqual('"类别"');
-    expect(rows[0].split('\t')[1]).toEqual('"家具"');
-    expect(rows[1].split('\t')[0]).toEqual('"子类别"');
-    expect(rows[1].split('\t')[1]).toEqual('"桌子"');
-    expect(rows[2].split('\t')[0]).toEqual('"省份"');
-    expect(rows[2].split('\t')[1]).toEqual('"数量"');
+    expect(rows[0].split(NewTab).length).toEqual(5);
+    expect(rows[1].split(NewTab)[0]).toEqual('子类别');
+    expect(rows[1].split(NewTab)[1]).toEqual('桌子');
+    expect(rows[2].split(NewTab)[0]).toEqual('省份');
+    expect(rows[2].split(NewTab)[1]).toEqual('数量');
   });
 
   // https://gw.alipayobjects.com/zos/antfincdn/PyrWwocNf/56d0914b-159a-4293-8615-6c1308bf4b3a.png
-  it('should export correct data in tree mode and collapseAll is false', () => {
+  it('should export correct data in tree mode and collapseAll is false -get', () => {
     const s2 = new PivotSheet(
       getContainer(),
       assembleDataCfg(),
@@ -237,21 +239,34 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
+    const data = copyData(s2, NewTab, true);
+    const rows = data.split(NewLine);
 
+    expect(rows).toMatchInlineSnapshot(`
+      Array [
+        "	类别	家具	家具	办公用品	办公用品",
+        "	子类别	桌子	沙发	笔	纸张",
+        "省份	城市	数量	数量	数量	数量",
+        "浙江省		18375	14043	4826	5854",
+        "浙江省	杭州市	7789	5343	945	1343",
+        "浙江省	绍兴市	2367	632	1304	1354",
+        "浙江省	宁波市	3877	7234	1145	1523",
+        "浙江省	舟山市	4342	834	1432	1634",
+        "四川省		7818	9473	7495	10984",
+        "四川省	成都市	1723	2451	2335	4004",
+        "四川省	绵阳市	1822	2244	245	3077",
+        "四川省	南充市	1943	2333	2457	3551",
+        "四川省	乐山市	2330	2445	2458	352",
+      ]
+    `);
     expect(rows).toHaveLength(13);
-    expect(rows[0].split('\t').length).toEqual(6);
-    expect(rows[0].split('\t')[1]).toEqual('"类别"');
-    expect(rows[0].split('\t')[2]).toEqual('"家具"');
-    expect(rows[1].split('\t')[1]).toEqual('"子类别"');
-    expect(rows[1].split('\t')[2]).toEqual('"桌子"');
-    expect(rows[2].split('\t')[0]).toEqual('"省份"');
-    expect(rows[2].split('\t')[1]).toEqual('"城市"');
-    expect(rows[2].split('\t')[2]).toEqual('"数量"');
+    expect(rows[0].split(NewTab).length).toEqual(6);
+    expect(rows[1].split(NewTab)[1]).toEqual('子类别');
+    expect(rows[2].split(NewTab)[1]).toEqual('城市');
+    expect(rows[2].split(NewTab)[2]).toEqual('数量');
   });
 
-  it('should export correct data in grid mode with valueInCols is false', () => {
+  it('should export correct data in grid mode with valueInCols is false - get', () => {
     const s2 = new PivotSheet(
       getContainer(),
       assembleDataCfg({
@@ -267,16 +282,16 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
+    const data = copyData(s2, NewTab);
+    const rows = data.split(NewLine);
 
     expect(rows).toHaveLength(13);
     rows.forEach((e) => {
-      expect(e.split('\t')).toHaveLength(35);
+      expect(e.split(NewTab)).toHaveLength(35);
     });
   });
 
-  it('should export correct data in grid mode with totals in col', () => {
+  it('should export correct data in grid mode with totals in col - get', () => {
     const s2 = new PivotSheet(
       getContainer(),
       assembleDataCfg({
@@ -303,16 +318,16 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
+    const data = copyData(s2, NewTab);
+    const rows = data.split(NewLine);
 
     expect(rows).toHaveLength(17);
     rows.forEach((e) => {
-      expect(e.split('\t')).toHaveLength(53);
+      expect(e.split(NewTab)).toHaveLength(53);
     });
   });
 
-  it('should export correct data in grid mode with totals in row', () => {
+  it('should export correct data in grid mode with totals in row - get', () => {
     const s2 = new PivotSheet(
       getContainer(),
       assembleDataCfg({
@@ -339,12 +354,12 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
-    const rows = data.split('\n');
+    const data = copyData(s2, NewTab);
+    const rows = data.split(NewLine);
 
     expect(rows).toHaveLength(16);
     rows.forEach((e) => {
-      expect(e.split('\t')).toHaveLength(54);
+      expect(e.split(NewTab)).toHaveLength(54);
     });
   });
 
@@ -369,19 +384,62 @@ describe('PivotSheet Export Test', () => {
           values: ['number'],
         },
       }),
-      assembleOptions({}),
+      assembleOptions({
+        interaction: {
+          enableCopy: true,
+          copyWithHeader: true,
+          copyWithFormat: true,
+        },
+      }),
     );
 
     s2.render();
     const data = copyData(s2, '\t', { isFormatHeader: true });
-
     const rows = data.split('\n');
 
     expect(rows).toHaveLength(7);
-    expect(rows[0].split('\t')[1]).toEqual('"province"');
-    expect(rows[0].split('\t')[2]).toEqual('"浙江省-province"');
-    expect(rows[1].split('\t')[1]).toEqual('"city"');
-    expect(rows[3].split('\t')[0]).toEqual('"家具-type"');
+    expect(rows[0].split(NewTab)[1]).toEqual('province');
+    expect(rows[0].split(NewTab)[2]).toEqual('浙江省-province');
+    expect(rows[1].split(NewTab)[1]).toEqual('city');
+    expect(rows[3].split(NewTab)[0]).toEqual('家具-type');
+  });
+
+  it('should export correct data when isFormat: {isFormatData: true}', () => {
+    const s2 = new PivotSheet(
+      getContainer(),
+      assembleDataCfg({
+        meta: [
+          {
+            field: 'number',
+            formatter: (value) => `${value}%`,
+          },
+        ],
+      }),
+      assembleOptions({
+        interaction: {
+          enableCopy: true,
+          copyWithFormat: true,
+        },
+      }),
+    );
+
+    s2.render();
+
+    const data = copyData(s2, NewTab, { isFormatData: true });
+
+    expect(data).toMatchInlineSnapshot(`
+      "	type	家具	家具	办公用品	办公用品
+      	sub_type	桌子	沙发	笔	纸张
+      province	city	number	number	number	number
+      浙江省	杭州市	7789%	5343%	945%	1343%
+      浙江省	绍兴市	2367%	632%	1304%	1354%
+      浙江省	宁波市	3877%	7234%	1145%	1523%
+      浙江省	舟山市	4342%	834%	1432%	1634%
+      四川省	成都市	1723%	2451%	2335%	4004%
+      四川省	绵阳市	1822%	2244%	245%	3077%
+      四川省	南充市	1943%	2333%	2457%	3551%
+      四川省	乐山市	2330%	2445%	2458%	352%"
+    `);
   });
 
   it('should export correct data when data is incomplete', () => {
@@ -413,18 +471,18 @@ describe('PivotSheet Export Test', () => {
     );
 
     s2.render();
-    const data = copyData(s2, '\t');
+    const data = copyData(s2, NewTab);
 
     expect(data).toMatchInlineSnapshot(`
-      "	\\"province\\"	\\"浙江省\\"	\\"浙江省\\"	\\"浙江省\\"	\\"浙江省\\"	\\"四川省\\"	\\"四川省\\"	\\"四川省\\"	\\"四川省\\"
-      	\\"city\\"	\\"杭州市\\"	\\"绍兴市\\"	\\"宁波市\\"	\\"舟山市\\"	\\"成都市\\"	\\"绵阳市\\"	\\"南充市\\"	\\"乐山市\\"
-      \\"type\\"	\\"sub_type\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"
-      \\"家具\\"		\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"
-      \\"家具\\"	\\"桌子\\"	\\"null\\"	\\"2367\\"	\\"3877\\"	\\"4342\\"	\\"1723\\"	\\"1822\\"	\\"1943\\"	\\"2330\\"
-      \\"家具\\"	\\"沙发\\"	\\"null\\"	\\"632\\"	\\"7234\\"	\\"834\\"	\\"2451\\"	\\"2244\\"	\\"2333\\"	\\"2445\\"
-      \\"办公用品\\"		\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"
-      \\"办公用品\\"	\\"笔\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"	\\"null\\"
-      \\"办公用品\\"	\\"纸张\\"	\\"null\\"	\\"1354\\"	\\"1523\\"	\\"1634\\"	\\"4004\\"	\\"3077\\"	\\"3551\\"	\\"352\\""
+      "	province	浙江省	浙江省	浙江省	浙江省	四川省	四川省	四川省	四川省
+      	city	杭州市	绍兴市	宁波市	舟山市	成都市	绵阳市	南充市	乐山市
+      type	sub_type	number	number	number	number	number	number	number	number
+      家具									
+      家具	桌子		2367	3877	4342	1723	1822	1943	2330
+      家具	沙发		632	7234	834	2451	2244	2333	2445
+      办公用品									
+      办公用品	笔								
+      办公用品	纸张		1354	1523	1634	4004	3077	3551	352"
     `);
   });
 
@@ -440,25 +498,30 @@ describe('PivotSheet Export Test', () => {
           values: ['number'],
         },
       }),
-      assembleOptions({ hierarchyType: 'grid', showSeriesNumber: true }),
+      assembleOptions({
+        hierarchyType: 'grid',
+        showSeriesNumber: true,
+        interaction: { enableCopy: true, copyWithHeader: true },
+      }),
     );
 
     s2.render();
 
-    const data = copyData(s2, '\t');
+    const data = copyData(s2, NewTab);
 
     expect(data).toMatchInlineSnapshot(`
-      "	\\"province\\"	\\"浙江省\\"	\\"浙江省\\"	\\"浙江省\\"	\\"浙江省\\"	\\"四川省\\"	\\"四川省\\"	\\"四川省\\"	\\"四川省\\"
-      	\\"city\\"	\\"杭州市\\"	\\"绍兴市\\"	\\"宁波市\\"	\\"舟山市\\"	\\"成都市\\"	\\"绵阳市\\"	\\"南充市\\"	\\"乐山市\\"
-      \\"type\\"	\\"sub_type\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"	\\"number\\"
-      \\"家具\\"	\\"桌子\\"	\\"7789\\"	\\"2367\\"	\\"3877\\"	\\"4342\\"	\\"1723\\"	\\"1822\\"	\\"1943\\"	\\"2330\\"
-      \\"家具\\"	\\"沙发\\"	\\"5343\\"	\\"632\\"	\\"7234\\"	\\"834\\"	\\"2451\\"	\\"2244\\"	\\"2333\\"	\\"2445\\"
-      \\"办公用品\\"	\\"笔\\"	\\"945\\"	\\"1304\\"	\\"1145\\"	\\"1432\\"	\\"2335\\"	\\"245\\"	\\"2457\\"	\\"2458\\"
-      \\"办公用品\\"	\\"纸张\\"	\\"1343\\"	\\"1354\\"	\\"1523\\"	\\"1634\\"	\\"4004\\"	\\"3077\\"	\\"3551\\"	\\"352\\""
+      "	province	浙江省	浙江省	浙江省	浙江省	四川省	四川省	四川省	四川省
+      	city	杭州市	绍兴市	宁波市	舟山市	成都市	绵阳市	南充市	乐山市
+      type	sub_type	number	number	number	number	number	number	number	number
+      家具	桌子	7789	2367	3877	4342	1723	1822	1943	2330
+      家具	沙发	5343	632	7234	834	2451	2244	2333	2445
+      办公用品	笔	945	1304	1145	1432	2335	245	2457	2458
+      办公用品	纸张	1343	1354	1523	1634	4004	3077	3551	352"
     `);
-    const rows = data.split('\n');
 
-    expect(rows[0].split('\t')[1]).toEqual('"province"');
-    expect(rows[1].split('\t')[1]).toEqual('"city"');
+    const rows = data.split(NewLine);
+
+    expect(rows[0].split(NewTab)[1]).toEqual('province');
+    expect(rows[1].split(NewTab)[1]).toEqual('city');
   });
 });
--- packages/s2-core/src/utils/export/interface.ts
+++ packages/s2-core/src/utils/export/interface.ts
@@ -1,4 +1,4 @@
-import type { DataItem } from '../../common';
+import type { DataItem, CellMeta } from '../../common';
 
 export type MatrixTransformer = (data: DataItem[][]) => CopyableItem;
 
@@ -25,3 +25,25 @@ export type CopyableHTML = {
 export type CopyableList = [CopyablePlain, CopyableHTML];
 
 export type Copyable = CopyableItem | CopyableItem[];
+
+export type FormatOptions =
+  | boolean
+  | {
+      isFormatHeader?: boolean;
+      isFormatData?: boolean;
+    };
+
+// 使用 类 替代 函数，重构下面的代码
+
+export interface CopyOrExportConfig {
+  selectedCells?: CellMeta[];
+  formatOptions?: FormatOptions;
+  separator?: string;
+}
+
+export interface CopyAndExportUnifyConfig {
+  separator: string;
+  isFormatHeader: boolean;
+  isFormatData: boolean;
+  selectedCells: CellMeta[];
+}
