0d8f08996ae9fef12b8d5ff1017f98ad9598ff82
刘嘉一 <lcx.seima@gmail.com>
2023-06-27 19:52:39 +0800

    test: 修复测试用例

--- packages/s2-react/__tests__/unit/hooks/useSpreadSheet-spec.ts
+++ packages/s2-react/__tests__/unit/hooks/useSpreadSheet-spec.ts
@@ -9,6 +9,7 @@ import {
 import { getContainer } from 'tests/util/helpers';
 import * as mockDataConfig from 'tests/data/simple-data.json';
 import { cloneDeep } from 'lodash';
+import { waitFor } from '@testing-library/react';
 import { useSpreadSheet } from '@/hooks';
 import type { SheetComponentsProps } from '@/components';
 
@@ -19,13 +20,12 @@ const s2Options: SheetComponentsProps['options'] = {
 };
 
 describe('useSpreadSheet tests', () => {
-  const container = getContainer();
   const getConfig = (
     fields: S2DataConfig['fields'] = mockDataConfig.fields,
   ): SheetComponentsProps => {
     return {
       spreadsheet: () =>
-        new PivotSheet(container, mockDataConfig, s2Options as S2Options),
+        new PivotSheet(getContainer(), mockDataConfig, s2Options as S2Options),
       options: s2Options,
       dataCfg: {
         fields,
@@ -34,36 +34,47 @@ describe('useSpreadSheet tests', () => {
     };
   };
 
-  test('should build spreadSheet', () => {
+  test('should build spreadSheet', async () => {
+    const props = getConfig();
     const { result } = renderHook(() =>
-      useSpreadSheet({ ...getConfig(), sheetType: 'pivot' }),
+      useSpreadSheet({ ...props, sheetType: 'pivot' }),
     );
 
-    expect(result.current.s2Ref).toBeDefined();
+    await waitFor(() => {
+      expect(result.current.s2Ref).toBeDefined();
+    });
   });
 
-  test('should cannot change table size when width or height updated and disable adaptive', () => {
-    const { result } = renderHook(() =>
-      useSpreadSheet({ ...getConfig(), sheetType: 'pivot', adaptive: false }),
-    );
+  test('should cannot change table size when width or height updated and disable adaptive', async () => {
+    const props = {
+      ...getConfig(),
+      sheetType: 'pivot' as const,
+      adaptive: false,
+    };
+    const { result } = renderHook(() => useSpreadSheet(props));
     const s2: SpreadSheet = result.current.s2Ref.current!;
 
-    expect(s2.options.width).toEqual(s2Options.width);
-    expect(s2.options.height).toEqual(s2Options.height);
+    await waitFor(() => {
+      expect(s2.options.width).toEqual(s2Options.width);
+      expect(s2.options.height).toEqual(s2Options.height);
+    });
+
     act(() => {
       s2.setOptions({ width: 300, height: 400 });
     });
 
     const canvas = s2.getCanvasElement();
 
-    expect(s2.options.width).toEqual(300);
-    expect(s2.options.height).toEqual(400);
+    await waitFor(() => {
+      expect(s2.options.width).toEqual(300);
+      expect(s2.options.height).toEqual(400);
 
-    expect(canvas.style.width).toEqual(`200px`);
-    expect(canvas.style.height).toEqual(`200px`);
+      expect(canvas.style.width).toEqual(`200px`);
+      expect(canvas.style.height).toEqual(`200px`);
+    });
   });
 
-  test('should clear init column leaf nodes when column fields changed', () => {
+  test('should clear init column leaf nodes when column fields changed', async () => {
     const defaultFields: S2DataConfig['fields'] = {
       rows: ['province'],
       columns: ['type', 'city'],
@@ -71,17 +82,16 @@ describe('useSpreadSheet tests', () => {
       valueInCols: true,
     };
 
-    const { result } = renderHook(() =>
-      useSpreadSheet(
-        cloneDeep({
-          ...getConfig(defaultFields),
-          sheetType: 'strategy',
-        }),
-      ),
-    );
+    const props = cloneDeep({
+      ...getConfig(defaultFields),
+      sheetType: 'strategy' as const,
+    });
+    const { result } = renderHook(() => useSpreadSheet(props));
     const s2 = result.current.s2Ref.current;
 
-    expect(s2!.facet.getInitColLeafNodes()).toHaveLength(2);
+    await waitFor(() => {
+      expect(s2!.facet.getInitColLeafNodes()).toHaveLength(2);
+    });
 
     /*
      * 很奇怪, rerender 之后始终拿到的两次 dataCfg 是一样的, 暂时先注释了
@@ -102,17 +112,16 @@ describe('useSpreadSheet tests', () => {
     // expect(s2.store.get('initColLeafNodes')).toEqual([]);
   });
 
-  test('should destroy sheet after unmount component', () => {
+  test('should destroy sheet after unmount component', async () => {
     const onDestroyFromProps = jest.fn();
     const onDestroyFromS2Event = jest.fn();
 
-    const { result, unmount } = renderHook(() =>
-      useSpreadSheet({
-        ...getConfig(),
-        sheetType: 'pivot',
-        onDestroy: onDestroyFromProps,
-      }),
-    );
+    const props = {
+      ...getConfig(),
+      sheetType: 'pivot' as const,
+      onDestroy: onDestroyFromProps,
+    };
+    const { result, unmount } = renderHook(() => useSpreadSheet(props));
 
     const s2 = result.current.s2Ref.current!;
 
@@ -126,24 +135,27 @@ describe('useSpreadSheet tests', () => {
       unmount();
     });
 
-    expect(destroySpy).toHaveBeenCalledTimes(1);
-    expect(onDestroyFromProps).toHaveBeenCalledTimes(1);
-    expect(onDestroyFromS2Event).toHaveBeenCalledTimes(1);
+    await waitFor(() => {
+      expect(destroySpy).toHaveBeenCalledTimes(1);
+      expect(onDestroyFromProps).toHaveBeenCalledTimes(1);
+      expect(onDestroyFromS2Event).toHaveBeenCalledTimes(1);
+    });
   });
 
-  test('should call onMounted when sheet mounted', () => {
+  test('should call onMounted when sheet mounted', async () => {
     const onMounted = jest.fn();
 
-    const { result } = renderHook(() =>
-      useSpreadSheet({
-        ...getConfig(),
-        sheetType: 'pivot',
-        onMounted,
-      }),
-    );
+    const props = {
+      ...getConfig(),
+      sheetType: 'pivot' as const,
+      onMounted,
+    };
+    const { result } = renderHook(() => useSpreadSheet(props));
 
     const s2 = result.current.s2Ref.current;
 
-    expect(onMounted).toHaveBeenCalledWith(s2);
+    await waitFor(() => {
+      expect(onMounted).toHaveBeenCalledWith(s2);
+    });
   });
 });
