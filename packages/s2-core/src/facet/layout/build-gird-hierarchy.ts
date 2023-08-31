68e98fea3f0231d7f0bd84db37737e6fef90d197
JuZe <niuwenze.nwz@antgroup.com>
2023-08-31 14:36:04 +0800

    fix: 删除了一个莫名其妙的函数

--- packages/s2-core/src/facet/layout/build-gird-hierarchy.ts
+++ packages/s2-core/src/facet/layout/build-gird-hierarchy.ts
@@ -1,26 +1,11 @@
 import { isEmpty, isUndefined } from 'lodash';
 import { EXTRA_FIELD } from '../../common/constant';
-import type { SpreadSheetFacetCfg } from '../../common/interface';
 import { addTotals } from '../../utils/layout/add-totals';
 import { generateHeaderNodes } from '../../utils/layout/generate-header-nodes';
 import { getDimsCondition } from '../../utils/layout/get-dims-condition-by-node';
 import type { FieldValue, GridHeaderParams } from '../layout/interface';
 import { layoutArrange } from '../layout/layout-hooks';
 import { TotalMeasure } from '../layout/total-measure';
-
-const hideMeasureColumn = (
-  fieldValues: FieldValue[],
-  field: string,
-  cfg: SpreadSheetFacetCfg,
-) => {
-  const hideMeasure = cfg.colCfg?.hideMeasureColumn ?? false;
-  const valueInCol = cfg.dataSet.fields.valueInCols;
-  for (const value of fieldValues) {
-    if (hideMeasure && valueInCol && field === EXTRA_FIELD) {
-      fieldValues.splice(fieldValues.indexOf(value), 1);
-    }
-  }
-};
 
 /**
  * Build grid hierarchy in rows or columns
@@ -76,8 +61,7 @@ export const buildGridHierarchy = (params: GridHeaderParams) => {
         fieldValues.push(fieldName);
       }
     }
-    // hide measure in columns
-    hideMeasureColumn(fieldValues, currentField, facetCfg);
+
     // add totals if needed
     addTotals({
       currentField,
