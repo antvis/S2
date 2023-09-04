16388888accebd445ba553ee5a9e7b036614e14d
JuZe <niuwenze.nwz@antgroup.com>
2023-09-04 15:10:47 +0800

    chore: 版本号更新

--- packages/s2-react/scripts/test-live.mjs
+++ packages/s2-react/scripts/test-live.mjs
@@ -10,7 +10,7 @@ async function main() {
   const spinner = ora('读取测试文件中...').start();
   const paths = glob.sync(`!(node_modules)/**/*-spec.ts?(x)`);
 
-  const defaultPath = '__tests__/spreadsheet/spread-sheet-spec.ts';
+  const defaultPath = '__tests__/spreadsheet/spread-sheet-spec.tsx';
   spinner.stop();
 
   const selectedPath = await inquirer.prompt([
