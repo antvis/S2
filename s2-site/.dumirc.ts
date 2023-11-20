47d0665d3ef3ad600cc354dd3e4593e1d9833c23
ai-qing-hai <wb-xcf804241@antgroup.com>
2023-11-20 15:03:49 +0800

    chore: update dumi-theme-antv 0.4.0

--- s2-site/.dumirc.ts
+++ s2-site/.dumirc.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     description: '多维交叉分析表格',
     defaultLanguage: 'zh', // 默认语言
     isAntVSite: false, // 是否是 AntV 的大官网
+    footerTheme: 'light', // 白色 底部主题
     siteUrl: 'https://antv.antgroup.com', // 官网地址
     githubUrl: repository.url, // GitHub 地址
     showSearch: true, // 是否显示搜索框
