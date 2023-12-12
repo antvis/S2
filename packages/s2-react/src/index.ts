2f934bdd7449b2892da8599d9da02772bbb657a5
lijinke666 <a1231236677287@163.com>
2023-12-12 16:07:48 +0800

    ci: 解决打包问题

--- packages/s2-react/src/index.ts
+++ packages/s2-react/src/index.ts
@@ -1,3 +1,5 @@
+// https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1270716220
+import type {} from 'rc-pagination';
 import './utils/extendLocale';
 
 export * from './components';
