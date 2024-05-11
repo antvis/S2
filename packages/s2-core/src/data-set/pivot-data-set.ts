0c3210a9d0078768bfbc279f3f0e1e42acefd16c
lijinke666 <a1231236677287@163.com>
2024-05-11 11:34:42 +0800

    fix: 配置了 subTotalsDimensions 时不兜底

--- packages/s2-core/src/data-set/pivot-data-set.ts
+++ packages/s2-core/src/data-set/pivot-data-set.ts
@@ -372,17 +372,17 @@ export class PivotDataSet extends BaseDataSet {
   }
 
   getTotalValue(query: Query, totalStatus?: TotalStatus) {
+    const { options } = this.spreadsheet;
     const effectiveStatus = some(totalStatus);
     const status = effectiveStatus ? totalStatus : this.getTotalStatus(query);
     const { aggregation, calcFunc } =
-      getAggregationAndCalcFuncByQuery(
-        status,
-        this.spreadsheet.options?.totals,
-      ) || {};
+      getAggregationAndCalcFuncByQuery(status, options?.totals) || {};
+
     // 聚合方式从用户配置的 s2Options.totals 取, 在触发前端兜底计算汇总逻辑时, 如果没有汇总的配置, 默认按 [求和] 计算,避免排序失效.
-    const defaultAggregation = !this.spreadsheet.isHierarchyTreeType()
-      ? Aggregation.SUM
-      : '';
+    const defaultAggregation =
+      isEmpty(options?.totals) && !this.spreadsheet.isHierarchyTreeType()
+        ? Aggregation.SUM
+        : '';
     const calcAction = calcActionByType[aggregation || defaultAggregation];
 
     // 前端计算汇总值
