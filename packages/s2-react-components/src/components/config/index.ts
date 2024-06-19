cc4e7785b73447aa5623f65e9653973a18175748
lijinke666 <a1231236677287@163.com>
2024-06-19 16:55:28 +0800

    chore: 完善测试和国际化

--- packages/s2-react-components/src/components/config/index.ts
+++ packages/s2-react-components/src/components/config/index.ts
@@ -3,6 +3,8 @@ export { ColorBox } from './theme-panel/color-box';
 export { ColorPickerPanel } from './theme-panel/color-picker-panel';
 
 export { FrozenPanel } from './frozen-panel';
+export { FrozenInputNumber } from './frozen-panel/frozen-input-number';
+
 export { TextAlignPanel } from './text-align-panel';
 
 export * from './theme-panel/interface';
