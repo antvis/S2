4e02f6cfe9bf465d2b345cbf59883948a5a8306a
lijinke666 <a1231236677287@163.com>
2024-06-21 09:50:15 +0800

    chore: 移除别名配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'father';
-import path from 'path';
 
 export default (name: string) => {
   return defineConfig({
     sourcemap: true,
-    alias: {
-      lodash: 'lodash-es',
-      '@antv/s2': path.resolve(__dirname, './packages/s2-core'),
-      '@antv/s2-shared': path.resolve(__dirname, './packages/s2-shared'),
-      '@/*': 's2-core/src/*',
-      'tests/*': 's2-core/__tests__/*',
-    },
+    // alias: {
+    //   lodash: 'lodash-es',
+    //   '@antv/s2': path.resolve(__dirname, './packages/s2-core'),
+    //   '@antv/s2-shared': path.resolve(__dirname, './packages/s2-shared'),
+    // },
     define: {
       'process.env.NODE_ENV': JSON.stringify('production'),
     },
