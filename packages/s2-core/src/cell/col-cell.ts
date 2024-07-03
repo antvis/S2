5cb324388cc2590f627a3de7031fac0bbc4fdc48
wjgogogo <906626481@qq.com>
2024-07-03 23:11:56 +0800

    fix: 修复列头 resize 区域冻结滚动位置偏移问题

--- packages/s2-core/src/cell/col-cell.ts
+++ packages/s2-core/src/cell/col-cell.ts
@@ -121,7 +121,6 @@ export class ColCell extends HeaderCell<ColHeaderConfig> {
       viewportWidth,
       cornerWidth = 0,
       scrollX = 0,
-      position,
     } = this.getHeaderConfig();
 
     const frozenGroupAreas = (this.spreadsheet.facet as FrozenFacet)
@@ -138,18 +137,13 @@ export class ColCell extends HeaderCell<ColHeaderConfig> {
       };
     }
 
-    const scrollXUntilColStickToLeft = frozenColGroupWidth
-      ? cornerWidth
-      : position.x;
-
     return {
-      start:
-        frozenColGroupWidth + Math.max(0, scrollX - scrollXUntilColStickToLeft),
+      start: frozenColGroupWidth + Math.max(0, scrollX - cornerWidth),
       size:
         viewportWidth -
         frozenColGroupWidth -
         frozenTrailingColGroupWidth +
-        Math.min(scrollX, scrollXUntilColStickToLeft),
+        Math.min(scrollX, cornerWidth),
     };
   }
 
@@ -324,16 +318,19 @@ export class ColCell extends HeaderCell<ColHeaderConfig> {
   }
 
   protected shouldAddVerticalResizeArea() {
+    if (this.getMeta().isFrozen) {
+      return true;
+    }
+
     const { x, y, width, height } = this.meta;
     const {
-      scrollX,
+      scrollX = 0,
       scrollY,
       cornerWidth = 0,
       height: headerHeight,
       width: headerWidth,
     } = this.getHeaderConfig();
 
-    const scrollContainsRowHeader = !this.spreadsheet.isFrozenRowHeader();
     const resizeStyle = this.getResizeAreaStyle();
 
     const resizeAreaBBox: SimpleBBox = {
@@ -343,12 +340,29 @@ export class ColCell extends HeaderCell<ColHeaderConfig> {
       height,
     };
 
-    const resizeClipAreaBBox: SimpleBBox = {
-      x: scrollContainsRowHeader ? -cornerWidth : 0,
-      y: 0,
-      width: scrollContainsRowHeader ? cornerWidth + headerWidth : headerWidth,
-      height: headerHeight,
-    };
+    const frozenGroupAreas = (this.spreadsheet.facet as FrozenFacet)
+      .frozenGroupAreas;
+    const colWidth = frozenGroupAreas[FrozenGroupArea.Col].width;
+    const trailingColWidth =
+      frozenGroupAreas[FrozenGroupArea.TrailingCol].width;
+
+    let resizeClipAreaBBox: SimpleBBox;
+
+    if (this.spreadsheet.isFrozenRowHeader()) {
+      resizeClipAreaBBox = {
+        x: colWidth,
+        y: 0,
+        width: headerWidth - colWidth - trailingColWidth,
+        height: headerHeight,
+      };
+    } else {
+      resizeClipAreaBBox = {
+        x: colWidth - cornerWidth,
+        y: 0,
+        width: headerWidth - colWidth - trailingColWidth + cornerWidth,
+        height: headerHeight,
+      };
+    }
 
     return shouldAddResizeArea(resizeAreaBBox, resizeClipAreaBBox, {
       scrollX,
@@ -358,10 +372,41 @@ export class ColCell extends HeaderCell<ColHeaderConfig> {
 
   protected getVerticalResizeAreaOffset() {
     const { x, y } = this.meta;
-    const { scrollX = 0, position } = this.getHeaderConfig();
+    const {
+      scrollX = 0,
+      position,
+      cornerWidth = 0,
+      viewportWidth,
+    } = this.getHeaderConfig();
+
+    const isFrozenRowHeader = this.spreadsheet.isFrozenRowHeader();
+
+    const frozenGroupAreas = (this.spreadsheet.facet as FrozenFacet)
+      .frozenGroupAreas;
+
+    const frozenColGroup = frozenGroupAreas[FrozenGroupArea.Col];
+    const frozenTrailingColGroup =
+      frozenGroupAreas[FrozenGroupArea.TrailingCol];
+
+    let offsetX = position?.x;
+
+    if (this.getMeta().isFrozenHead) {
+      offsetX +=
+        x -
+        frozenColGroup.x -
+        (isFrozenRowHeader ? 0 : Math.min(scrollX, cornerWidth));
+    } else if (this.getMeta().isFrozenTrailing) {
+      offsetX +=
+        x -
+        frozenTrailingColGroup.x +
+        viewportWidth -
+        frozenTrailingColGroup.width;
+    } else {
+      offsetX += x - scrollX;
+    }
 
     return {
-      x: position?.x + x - scrollX,
+      x: offsetX,
       y: position?.y + y,
     };
   }
--- packages/s2-core/src/cell/table-col-cell.ts
+++ packages/s2-core/src/cell/table-col-cell.ts
@@ -1,16 +1,10 @@
 import { find } from 'lodash';
 import { ColCell } from '../cell/col-cell';
-import {
-  FrozenGroupArea,
-  HORIZONTAL_RESIZE_AREA_KEY_PRE,
-} from '../common/constant';
+import { HORIZONTAL_RESIZE_AREA_KEY_PRE } from '../common/constant';
 import type { FormatResult } from '../common/interface';
-import type { SimpleBBox } from '../engine';
-import type { FrozenFacet } from '../facet/frozen-facet';
 import type { BaseHeaderConfig } from '../facet/header';
 import { formattedFieldValue } from '../utils/cell/header-cell';
 import { renderRect } from '../utils/g-renders';
-import { shouldAddResizeArea } from '../utils/interaction/resize';
 import { getSortTypeIcon } from '../utils/sort-action';
 
 export class TableColCell extends ColCell {
@@ -33,82 +27,6 @@ export class TableColCell extends ColCell {
       this.meta,
       this.spreadsheet.dataSet.getFieldName(this.meta.field),
     );
-  }
-
-  protected shouldAddVerticalResizeArea() {
-    if (this.getMeta().isFrozen) {
-      return true;
-    }
-
-    const {
-      scrollX,
-      scrollY,
-      width: headerWidth,
-      height: headerHeight,
-      spreadsheet,
-    } = this.getHeaderConfig();
-    const { x, y, width, height } = this.getBBoxByType();
-    const resizeStyle = this.getResizeAreaStyle();
-
-    const resizeAreaBBox: SimpleBBox = {
-      x: x + width - resizeStyle.size!,
-      y,
-      width: resizeStyle.size!,
-      height,
-    };
-
-    const frozenGroupAreas = (spreadsheet.facet as FrozenFacet)
-      .frozenGroupAreas;
-    const colWidth = frozenGroupAreas[FrozenGroupArea.Col].width;
-    const trailingColWidth =
-      frozenGroupAreas[FrozenGroupArea.TrailingCol].width;
-
-    const resizeClipAreaBBox: SimpleBBox = {
-      x: colWidth,
-      y: 0,
-      width: headerWidth - colWidth - trailingColWidth,
-      height: headerHeight,
-    };
-
-    return shouldAddResizeArea(resizeAreaBBox, resizeClipAreaBBox, {
-      scrollX,
-      scrollY,
-    });
-  }
-
-  protected getVerticalResizeAreaOffset() {
-    const { x, y } = this.meta;
-    const {
-      scrollX = 0,
-      position,
-      spreadsheet,
-      viewportWidth,
-    } = this.getHeaderConfig();
-
-    const frozenGroupAreas = (spreadsheet.facet as FrozenFacet)
-      .frozenGroupAreas;
-
-    const frozenColGroup = frozenGroupAreas[FrozenGroupArea.Col];
-    const frozenTrailingColGroup =
-      frozenGroupAreas[FrozenGroupArea.TrailingCol];
-    let offsetX = position?.x;
-
-    if (this.getMeta().isFrozenHead) {
-      offsetX += x - frozenColGroup.x;
-    } else if (this.getMeta().isFrozenTrailing) {
-      offsetX +=
-        x -
-        frozenTrailingColGroup.x +
-        viewportWidth -
-        frozenTrailingColGroup.width;
-    } else {
-      offsetX += x - scrollX;
-    }
-
-    return {
-      x: offsetX,
-      y: position?.y + y,
-    };
   }
 
   protected isSortCell() {
