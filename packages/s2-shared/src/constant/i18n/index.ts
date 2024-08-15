21f4226095fe55e0b23bb6a0a0598dc56ac8b5ce
Artem Gusev <corocoto.art@gmail.com>
2024-08-15 06:10:29 +0300

    feat(i18n): add support of the russian language (#2853)
    * test(s2-core): add test scenarios for ru lang
    
    * feat(i18n): add support ru lang
    
    * docs(i18n.en): update description

--- packages/s2-shared/src/constant/i18n/index.ts
+++ packages/s2-shared/src/constant/i18n/index.ts
@@ -1,8 +1,10 @@
 import type { LocaleType } from '@antv/s2';
 import { EN_US } from './en_US';
 import { ZH_CN } from './zh_CN';
+import { RU } from './ru';
 
 export const Locale: LocaleType = {
   zh_CN: ZH_CN,
   en_US: EN_US,
+  ru: RU,
 };
