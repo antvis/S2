82c15002bd12b5c21cfae581711abf51553d8000
Jinke Li <a1231236677287@163.com>
2024-10-25 16:21:08 +0800

    fix: 修复通过修改引用的方式修改菜单配置时, tooltip 未重新渲染 (#2933)
    * fix: 修复通过修改引用的方式修改菜单配置时, tooltip 未重新渲染
    
    * test: 简化代码

--- packages/s2-react/src/components/tooltip/components/operator.tsx
+++ packages/s2-react/src/components/tooltip/components/operator.tsx
@@ -12,71 +12,72 @@ import { TooltipIcon } from './icon';
 
 import '@antv/s2-shared/src/styles/tooltip/operator.less';
 
-export const TooltipOperator: React.FC<Required<TooltipOperatorProps>> =
-  React.memo((props) => {
-    const {
-      onlyShowOperator,
-      cell,
-      menu: {
-        className,
-        items: menus = [],
-        onClick,
-        selectedKeys,
-        ...otherMenuProps
-      },
-    } = props;
+export const TooltipOperator: React.FC<Required<TooltipOperatorProps>> = (
+  props,
+) => {
+  const {
+    onlyShowOperator,
+    cell,
+    menu: {
+      className,
+      items: menus = [],
+      onClick,
+      selectedKeys,
+      ...otherMenuProps
+    },
+  } = props;
 
-    if (isEmpty(menus)) {
-      return null;
-    }
+  if (isEmpty(menus)) {
+    return null;
+  }
 
-    const onMenuClick = (info: TooltipOperatorMenuInfo) => {
-      onClick?.(info, cell);
-    };
+  const onMenuClick = (info: TooltipOperatorMenuInfo) => {
+    onClick?.(info, cell);
+  };
 
-    const renderMenu = (
-      menu: TooltipOperatorMenuItem,
-    ): GetProp<MenuProps, 'items'>[number] => {
-      const { key, label, children, onClick: onTitleClick } = menu;
-      const subMenus = map(children, renderMenu);
+  const renderMenu = (
+    menu: TooltipOperatorMenuItem,
+  ): GetProp<MenuProps, 'items'>[number] => {
+    const { key, label, children, onClick: onTitleClick } = menu;
+    const subMenus = map(children, renderMenu);
 
-      return {
-        key,
-        label,
-        icon: (
-          <TooltipIcon
-            icon={menu.icon!}
-            className={`${TOOLTIP_PREFIX_CLS}-operator-icon`}
-          />
-        ),
-        popupClassName: `${TOOLTIP_PREFIX_CLS}-operator-submenu-popup`,
-        onTitleClick: (info) => {
-          onTitleClick?.(info as any, cell);
-          onMenuClick?.(info);
-        },
-        children: !isEmpty(subMenus) ? subMenus : undefined,
-      };
-    };
-
-    const renderMenus = () => {
-      const items = map(menus, renderMenu);
-
-      return (
-        <Menu
-          mode={onlyShowOperator ? 'vertical' : 'horizontal'}
-          className={cls(`${TOOLTIP_PREFIX_CLS}-operator-menus`, className)}
-          onClick={onMenuClick}
-          selectedKeys={selectedKeys}
-          items={items}
-          selectable={onlyShowOperator}
-          {...otherMenuProps}
+    return {
+      key,
+      label,
+      icon: (
+        <TooltipIcon
+          icon={menu.icon!}
+          className={`${TOOLTIP_PREFIX_CLS}-operator-icon`}
         />
-      );
+      ),
+      popupClassName: `${TOOLTIP_PREFIX_CLS}-operator-submenu-popup`,
+      onTitleClick: (info) => {
+        onTitleClick?.(info as any, cell);
+        onMenuClick?.(info);
+      },
+      children: !isEmpty(subMenus) ? subMenus : undefined,
     };
+  };
+
+  const renderMenus = () => {
+    const items = map(menus, renderMenu);
 
     return (
-      <div className={`${TOOLTIP_PREFIX_CLS}-operator`}>{renderMenus()}</div>
+      <Menu
+        mode={onlyShowOperator ? 'vertical' : 'horizontal'}
+        className={cls(`${TOOLTIP_PREFIX_CLS}-operator-menus`, className)}
+        onClick={onMenuClick}
+        selectedKeys={selectedKeys}
+        items={items}
+        selectable={onlyShowOperator}
+        {...otherMenuProps}
+      />
     );
-  });
+  };
+
+  return (
+    <div className={`${TOOLTIP_PREFIX_CLS}-operator`}>{renderMenus()}</div>
+  );
+};
 
 TooltipOperator.displayName = 'TooltipOperator';
