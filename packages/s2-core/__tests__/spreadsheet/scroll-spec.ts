00314cc5f6e5f7c7fc1d27e11a4405971c6083ef
lijinke666 <a1231236677287@163.com>
2024-11-01 17:50:34 +0800

    test: 修复单测

--- packages/s2-core/__tests__/spreadsheet/scroll-spec.ts
+++ packages/s2-core/__tests__/spreadsheet/scroll-spec.ts
@@ -480,7 +480,7 @@ describe('Scroll Tests', () => {
     s2.changeSheetSize(1000, 150); // 纵向滚动条
     await s2.render(false);
 
-    expect(Math.floor(s2.facet.vScrollBar.getBBox().x)).toEqual(195);
+    expect(Math.floor(s2.facet.vScrollBar.getBBox().x)).toEqual(201);
 
     s2.setOptions({
       interaction: {
