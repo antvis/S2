1f6e0192ffd5f874c8e2ce804eefdd1d1b018ab5
wjgogogo <906626481@qq.com>
2024-11-19 15:03:25 +0800

    refactor: icon 位置迁移

--- packages/s2-react-components/src/components/common/icons/index.tsx
+++ packages/s2-react-components/src/components/common/icons/index.tsx
@@ -1,9 +1,9 @@
-import { ReactComponent as CalendarIcon } from '@antv/s2/esm/shared/icons/calendar-icon.svg';
-import { ReactComponent as ColIcon } from '@antv/s2/esm/shared/icons/col-icon.svg';
-import { ReactComponent as LocationIcon } from '@antv/s2/esm/shared/icons/location-icon.svg';
-import { ReactComponent as RowIcon } from '@antv/s2/esm/shared/icons/row-icon.svg';
-import { ReactComponent as TextIcon } from '@antv/s2/esm/shared/icons/text-icon.svg';
-import { ReactComponent as ValueIcon } from '@antv/s2/esm/shared/icons/value-icon.svg';
 import './index.less';
+import { ReactComponent as CalendarIcon } from './svg/calendar-icon.svg';
+import { ReactComponent as ColIcon } from './svg/col-icon.svg';
+import { ReactComponent as LocationIcon } from './svg/location-icon.svg';
+import { ReactComponent as RowIcon } from './svg/row-icon.svg';
+import { ReactComponent as TextIcon } from './svg/text-icon.svg';
+import { ReactComponent as ValueIcon } from './svg/value-icon.svg';
 
 export { CalendarIcon, ColIcon, LocationIcon, RowIcon, TextIcon, ValueIcon };
