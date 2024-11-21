11df5d511515817047b09d6e3dd37000f7b189f2
lijinke666 <a1231236677287@163.com>
2024-11-21 17:08:43 +0800

    fix: 修改包 peerDependencies 版本和样式依赖问题

--- s2-site/examples/react-component/sheet/demo/chart.tsx
+++ s2-site/examples/react-component/sheet/demo/chart.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { S2DataConfig, S2Options } from '@antv/s2';
 import { SheetComponent } from '@antv/s2-react';
-import '@antv/s2-react/dist/style.min.css';
+import '@antv/s2-react/dist/s2-react.min.css';
 
 const s2Options: S2Options = {
   width: 1000,
