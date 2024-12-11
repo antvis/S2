cfc30367eca0434e83b56c163478a2c1e7902869
lijinke666 <a1231236677287@163.com>
2024-12-11 17:30:58 +0800

    fix: 修复行头圈选复制时部分场景下数据重复 close #2975

--- packages/s2-core/src/interaction/brush-selection/base-brush-selection.ts
+++ packages/s2-core/src/interaction/brush-selection/base-brush-selection.ts
@@ -158,8 +158,11 @@ export class BaseBrushSelection
 
     let newX = this.endBrushPoint?.x + x;
     let newY = this.endBrushPoint?.y + y;
-    let needScrollForX = true;
-    let needScrollForY = true;
+    // 有滚动条才需要滚动
+    let needScrollForX = isRowHeader
+      ? !!facet.hRowScrollBar
+      : !!facet.hScrollBar;
+    let needScrollForY = !!facet.vScrollBar;
     const vScrollBarWidth = facet.vScrollBar?.getBBox()?.width;
     // 额外加缩进，保证 getShape 在 panelBox 内
     const extraPixel = 2;
@@ -769,18 +772,18 @@ export class BaseBrushSelection
   };
 
   public autoBrushScroll(point: PointLike, isRowHeader = false) {
-    this.clearAutoScroll();
-
-    if (!this.isPointInCanvas(point)) {
-      const deltaX = point?.x - this.endBrushPoint?.x;
-      const deltaY = point?.y - this.endBrushPoint?.y;
-
-      this.handleScroll(deltaX, deltaY, isRowHeader);
-
-      return true;
+    if (this.isPointInCanvas(point)) {
+      return false;
     }
 
-    return false;
+    this.clearAutoScroll();
+
+    const deltaX = point?.x - this.endBrushPoint?.x;
+    const deltaY = point?.y - this.endBrushPoint?.y;
+
+    this.handleScroll(deltaX, deltaY, isRowHeader);
+
+    return true;
   }
 
   public emitBrushSelectionEvent(
