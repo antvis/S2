986bdc254902162ec27803375377e585bbbd9dfd
huiyu.zjt <Alexzjt@users.noreply.github.com>
2025-04-03 17:01:34 +0800

    fix: 明细表自定义列头prefix重复展示异常 (#3142)

--- packages/s2-core/src/facet/base-facet.ts
+++ packages/s2-core/src/facet/base-facet.ts
@@ -1974,9 +1974,14 @@ export abstract class BaseFacet {
       return;
     }
 
+    const levelIds =
+      colsHierarchy.sampleNodeForLastLevel?.id.split(NODE_ID_SEPARATOR) || [];
     // 每一列层级不定, 用层级最深的那一列采样高度
     const nodes = colsHierarchy.getNodes().filter((node) => {
-      return colsHierarchy.sampleNodeForLastLevel?.id.includes(node.id);
+      return (
+        colsHierarchy.sampleNodeForLastLevel?.id.includes(node.id) &&
+        levelIds.includes(node.field)
+      );
     });
 
     colsHierarchy.sampleNodesForAllLevels = nodes;
