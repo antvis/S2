04086bdcbcaa83183a1854e7da651695ef7c968b
Alexzjt <alex_zjt@163.com>
2025-04-30 17:18:01 +0800

    feat: 外部可以重写getContainer

--- packages/s2-core/src/ui/tooltip/index.ts
+++ packages/s2-core/src/ui/tooltip/index.ts
@@ -161,7 +161,7 @@ export class BaseTooltip<
     this.position = { x: 0, y: 0 };
   }
 
-  private getContainer(): HTMLElement {
+  protected getContainer(): HTMLElement {
     if (!this.container) {
       const { tooltip } = this.spreadsheet.options;
       const rootContainer = tooltip?.getContainer?.() || document.body;
