193f77a87e0d54ebb98b8709fda256dd221f2cae
Alexzjt <alex_zjt@163.com>
2025-05-22 17:14:36 +0800

    docs: 去掉SEO

--- s2-site/.dumirc.ts
+++ s2-site/.dumirc.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'dumi';
 import { repository } from './package.json';
 
 export default defineConfig({
-  ...(process.env.NODE_ENV === 'production' ? { ssr: { builder: 'webpack', mako: false } } : { ssr: false, mako: {} }),
   locales: [
     { id: 'zh', name: '中文' },
     { id: 'en', name: 'English' },
