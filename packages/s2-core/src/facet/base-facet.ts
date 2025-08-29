433e34753c0eb32999c6394d173c02ef11c8b185
Alexzjt <1543042497@qq.com>
2025-08-29 14:55:05 +0800

    perf: updateGrid过程中复用line

--- packages/s2-core/src/facet/base-facet.ts
+++ packages/s2-core/src/facet/base-facet.ts
@@ -726,7 +726,7 @@ export abstract class BaseFacet {
   onContainerWheelForPc = () => {
     const canvas = this.spreadsheet.getCanvasElement();
 
-    canvas?.addEventListener('wheel', this.onWheel);
+    canvas?.addEventListener('wheel', this.onWheel, { passive: true });
   };
 
   onContainerWheelForMobile = () => {
