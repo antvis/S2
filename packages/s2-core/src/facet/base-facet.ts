b9684c9c64ed7afd992c68510a2a33a379531cdb
Alexzjt <1543042497@qq.com>
2025-09-19 18:03:58 +0800

    fix: fontSize变化时需要自动适配

--- packages/s2-core/src/facet/base-facet.ts
+++ packages/s2-core/src/facet/base-facet.ts
@@ -91,6 +91,7 @@ import type {
 } from '../common/interface/scroll';
 import { PanelScrollGroup } from '../group/panel-scroll-group';
 import type { SpreadSheet } from '../sheet-type';
+import { DEFAULT_FONTSIZE } from '../theme';
 import { ScrollBar, ScrollType } from '../ui/scrollbar';
 import type { SelectedIds } from '../utils';
 import { getAdjustedRowScrollX, getAdjustedScrollOffset } from '../utils/facet';
@@ -417,9 +418,13 @@ export abstract class BaseFacet {
     }
 
     const isEnableColNodeHeightAdaptive =
-      colCellStyle?.maxLines! > 1 && colCellStyle?.wordWrap;
+      (colCellStyle?.maxLines! > 1 && colCellStyle?.wordWrap) ||
+      this.spreadsheet.theme.colCell.text?.fontSize > DEFAULT_FONTSIZE ||
+      this.spreadsheet.theme.colCell.bolderText.fontSize > DEFAULT_FONTSIZE;
     const isEnableCornerNodeHeightAdaptive =
-      cornerCellStyle?.maxLines! > 1 && cornerCellStyle?.wordWrap;
+      (cornerCellStyle?.maxLines! > 1 && cornerCellStyle?.wordWrap) ||
+      this.spreadsheet.theme.cornerCell.text?.fontSize > DEFAULT_FONTSIZE ||
+      this.spreadsheet.theme.cornerCell.bolderText?.fontSize > DEFAULT_FONTSIZE;
     const defaultHeight = this.getDefaultColNodeHeight(colNode, colsHierarchy);
 
     let colAdaptiveHeight = defaultHeight;
--- packages/s2-core/src/facet/pivot-facet.ts
+++ packages/s2-core/src/facet/pivot-facet.ts
@@ -34,6 +34,7 @@ import type {
 } from '../common/interface';
 import type { Query } from '../data-set/interface';
 import type { PivotDataSet } from '../data-set/pivot-data-set';
+import { DEFAULT_FONTSIZE } from '../theme';
 import { getValidFrozenOptionsForPivot, safeJsonParse } from '../utils';
 import { getDataCellId } from '../utils/cell/data-cell';
 import { getActionIconConfig } from '../utils/cell/header-cell';
@@ -426,7 +427,7 @@ export class PivotFacet extends FrozenFacet {
     // 文本超过 1 行时再自适应单元格高度, 不然会频繁触发 GC, 导致性能降低: https://github.com/antvis/S2/issues/2693
     const isEnableHeightAdaptive =
       (rowCellStyle?.maxLines! > 1 && rowCellStyle?.wordWrap) ||
-      this.spreadsheet.theme.rowCell!.text.fontSize > 12;
+      this.spreadsheet.theme.rowCell!.text.fontSize > DEFAULT_FONTSIZE;
 
     if (this.isCustomRowCellHeight(rowNode) || !isEnableHeightAdaptive) {
       rowNode.extra.isCustomHeight = true;
