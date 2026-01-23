ec5106f2c5a234a8388f90e06903294bdf303a32
Alexzjt <1543042497@qq.com>
2026-01-23 18:00:44 +0800

    docs: 删除多余的注释

--- packages/s2-core/src/sheet-type/pivot-sheet.ts
+++ packages/s2-core/src/sheet-type/pivot-sheet.ts
@@ -50,8 +50,6 @@ export class PivotSheet extends SpreadSheet {
   }
 
   public isHierarchyTreeType(): boolean {
-    // 注意：grid-tree 模式使用 grid 布局，不在此处返回 true
-    // 需要折叠逻辑时请使用 isHierarchyCollapseType()
     return this.options.hierarchyType === 'tree';
   }
 
