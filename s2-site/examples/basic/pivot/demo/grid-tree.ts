33e2b39fa3e60447180165107b7ee49a26c87973
Alexzjt <1543042497@qq.com>
2026-01-26 11:44:51 +0800

    docs: 更新树状平铺的demo

--- s2-site/examples/basic/pivot/demo/grid-tree.ts
+++ s2-site/examples/basic/pivot/demo/grid-tree.ts
@@ -1,15 +1,15 @@
-import { PivotSheet, S2Options } from '@antv/s2';
+import { PivotSheet, S2Options, setLang } from '@antv/s2';
 
 fetch(
-    'https://gw.alipayobjects.com/os/bmw-prod/2a5dbbc8-d0a7-4d02-b7c9-34f6ca63cff6.json',
+    'https://assets.antv.antgroup.com/s2/expanded-en-data-config.json',
 )
     .then((res) => res.json())
     .then(async (dataCfg) => {
         const container = document.getElementById('container');
 
         const s2Options: S2Options = {
-            width: 600,
-            height: 480,
+            width: 1200,
+            height: 800,
             // grid-tree 模式：平铺布局 + 展开折叠
             // 每个维度层级有独立的列，同时支持展开/折叠子节点
             hierarchyType: 'grid-tree',
@@ -17,14 +17,6 @@ fetch(
                 rowCell: {
                     // 默认展开第 1 层 (从 0 开始)
                     expandDepth: 1,
-
-                    // 折叠全部
-                    // collapseAll: true,
-
-                    // 折叠指定节点
-                    // collapseFields: {
-                    //   'root[&]浙江省': true,
-                    // },
                 },
             },
             // 配置总计和小计
@@ -42,9 +34,12 @@ fetch(
                     calcGrandTotals: {
                         aggregation: 'SUM',
                     },
+                    reverseSubTotalsLayout: true,
                 },
             },
         };
+
+        setLang('en_US');
 
         const s2 = new PivotSheet(container!, dataCfg, s2Options);
 
